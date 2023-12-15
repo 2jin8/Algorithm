93f99cf750d8db918cebad904ece8dcc0da091f1
2jin8 <78083320+2jin8@users.noreply.github.com>
2023-12-15 13:30:01 +0900

    [Silver II] Title: 약수의 합 2, Time: 196 ms, Memory: 18512 KB -BaekjoonHub

--- 백준/Silver/17427. 약수의 합 2/약수의 합 2.java
+++ 백준/Silver/17427. 약수의 합 2/약수의 합 2.java
@@ -1,13 +1,22 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        long sum = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] fx = new int[n + 1];
 
-        for (int i = 1; i <= x; i++) {
-            sum += (long) (x / i) * i;
+        Arrays.fill(fx, 1);
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; i * j <= n; j++) {
+                fx[i * j] += i;
+            }
+        }
+        
+        long sum = 0L;
+        for (int i = 1; i <= n; i++) {
+            sum += fx[i];
         }
         System.out.println(sum);
     }
