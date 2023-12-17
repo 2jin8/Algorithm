34a1fc8e85ff1685cc51714e828dc0913ad39fbb
2jin8 <78083320+2jin8@users.noreply.github.com>
2023-12-17 22:27:16 +0900

    [Silver I] Title: 양, Time: 204 ms, Memory: 21676 KB -BaekjoonHub

--- 백준/Silver/3184. 양/양.java
+++ 백준/Silver/3184. 양/양.java
@@ -3,9 +3,10 @@ import java.io.*;
 
 public class Main {
     private static int R, C;
-    private static int sheep, fox;
+    private static int sheep, fox, sheepNum, foxNum;
     private static char[][] board;
     private static boolean[][] visited;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -25,56 +26,37 @@ public class Main {
         for (int i = 0; i < R; i++) {
             for (int j = 0; j < C; j++) {
                 if (board[i][j] != '#' && !visited[i][j]) { // 울타리가 아니고 방문하지 않은 경우
-                    bfs(i, j); // 탐색 시작
+                    sheepNum = 0;
+                    foxNum = 0;
+                    dfs(i, j);
+                    if (sheepNum > foxNum) fox -= foxNum;
+                    else sheep -= sheepNum;
                 }
             }
         }
         System.out.println(sheep + " " + fox);
     }
 
-    public static void bfs(int r, int c) {
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(r, c));
+    public static void dfs(int r, int c) {
         visited[r][c] = true;
+        if (board[r][c] == 'o') sheepNum++;
+        else if (board[r][c] == 'v') foxNum++;
 
         int[] dr = {1, -1, 0, 0};
         int[] dc = {0, 0, 1, -1};
-        int foxNum = 0, sheepNum = 0;
-        while (!queue.isEmpty()) {
-            Point point = queue.poll();
-            r = point.r;
-            c = point.c;
-            if (board[r][c] == 'v') foxNum++; // 늑대 수 세기
-            else if (board[r][c] == 'o') sheepNum++; // 양의 수 세기
+        for (int i = 0; i < 4; i++) {
+            int tr = r + dr[i];
+            int tc = c + dc[i];
 
-            for (int i = 0; i < 4; i++) {
-                int tr = r + dr[i];
-                int tc = c + dc[i];
+            if (tr < 0 || tc < 0 || tr >= R || tc >= C) // 범위 벗어나는 경우
+                continue;
 
-                if (tr < 0 || tc < 0 || tr >= R || tc >= C) // 범위 벗어나는 경우
-                    continue;
+            if (board[tr][tc] == '#') // 울타리인 경우
+                continue;
 
-                if (board[tr][tc] == '#') // 울타리인 경우
-                    continue;
-
-                if (!visited[tr][tc]) {
-                    queue.offer(new Point(tr, tc));
-                    visited[tr][tc] = true;
-                }
+            if (!visited[tr][tc]) {
+                dfs(tr, tc);
             }
         }
-        // 양의 수 > 늑대의 수 -> 양이 이김
-        if (sheepNum > foxNum) fox -= foxNum;
-        else sheep -= sheepNum;
-    }
-}
-
-class Point {
-    int r;
-    int c;
-
-    public Point(int r, int c) {
-        this.r = r;
-        this.c = c;
     }
 }
