19ede9a8351d149fdd372ec2980c1d2dcbcc8913
2jin8 <78083320+2jin8@users.noreply.github.com>
2023-12-17 22:56:50 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 156 ms, Memory: 16948 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.java
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Main {
     private static int N, M;
-    private static int wNum = 0, bNum = 0;
+    private static int wNum = 0, bNum = 0, num;
     private static char[][] board;
     private static boolean[][] visited;
     public static void main(String[] args) throws IOException {
@@ -24,50 +24,32 @@ public class Main {
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
                 if (!visited[i][j]) {
-                    if (board[i][j] == 'W') wNum += Math.pow(bfs(i, j), 2);
-                    else bNum += Math.pow(bfs(i, j), 2);
+                    num = 0;
+                    dfs(i, j, board[i][j]);
+                    if (board[i][j] == 'W') wNum += Math.pow(num, 2);
+                    else bNum += Math.pow(num, 2);
                 }
             }
         }
         System.out.println(wNum + " " + bNum);
     }
 
-    public static int bfs(int x, int y) {
-        Queue<Point> queue = new LinkedList<>();
-        char team = board[x][y];
-        queue.offer(new Point(x, y));
+    public static void dfs(int x, int y, char team) {
         visited[x][y] = true;
+        num++;
 
         int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        int num = 0;
-        while (!queue.isEmpty()) {
-            Point point = queue.poll();
-            num++;
+        int[] dy = {0, 0, 1, -1};
+        for (int i = 0; i < 4; i++) {
+            int tx = x + dx[i];
+            int ty = y + dy[i];
 
-            for (int i = 0; i < 4; i++) {
-                int tx = point.x + dx[i];
-                int ty = point.y + dy[i];
+            if (tx < 0 || ty < 0 || tx >= M || ty >= N)
+                continue;
 
-                if (tx < 0 || ty < 0 || tx >= M || ty >= N)
-                    continue;
-
-                if (board[tx][ty] == team && !visited[tx][ty]) { // 같은 팀이고 방문하지 않은 경우
-                    queue.offer(new Point(tx, ty));
-                    visited[tx][ty] = true;
-                }
+            if (board[tx][ty] == team && !visited[tx][ty]) {
+                dfs(tx, ty, team);
             }
         }
-        return num;
-    }
-}
-
-class Point {
-    int x;
-    int y;
-
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
     }
 }
