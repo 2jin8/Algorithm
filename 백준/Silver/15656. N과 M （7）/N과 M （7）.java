b7021d12cd0c3d4ec6044388c3e6311020e84160
2jin8 <78083320+2jin8@users.noreply.github.com>
2023-12-19 23:48:15 +0900

    [Silver III] Title: N과 M (7), Time: 772 ms, Memory: 127336 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int[] result, nList; // 결과 저장 배열
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        nList = new int[n];
+        for (int i = 0; i < n; i++) {
+            nList[i] = sc.nextInt();
+        }
+        Arrays.sort(nList); // 증가하는 순서대로 출력
+
+        result = new int[m];
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int cnt) {
+        if (cnt == m) {
+            for (int r : result) {
+                sb.append(r).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            // 같은 수 여러 번 골라도 됨
+            result[cnt] = nList[i];
+            dfs(cnt + 1);
+        }
+    }
+}
