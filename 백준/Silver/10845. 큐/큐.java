88ba66f90fa7561e3c377f93411c9d94c0e71672
2jin8 <78083320+2jin8@users.noreply.github.com>
2023-12-20 21:13:55 +0900

    [Silver IV] Title: 큐, Time: 244 ms, Memory: 19752 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,83 +1,44 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.ListIterator;
-
-class Queue {
-	int front, back;
-	int[] q;
-	Queue(int n) {
-		front = 0;
-		back = 0;
-		q = new int[n];
-	}
-	
-	void push(int num) {
-		q[back++] = num;
-	}
-	
-	int pop() {
-		if (!empty()) {
-			int tmp = q[front++];
-			return tmp;
-		}
-		return -1;
-	}
-	
-	int size() {
-		return back-front;
-	}
-	
-	boolean empty() {
-		return front == back;
-	}
-
-	int front() {
-		if (empty()) return -1;
-		return q[front];
-	}
-
-	int back() {
-		if (empty()) return -1;
-		return q[back - 1];
-	}
-}
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(br.readLine());
-		
-		Queue q = new Queue(n);
-		
-		for (int i=0; i<n; i++) {
-			String[] s = br.readLine().split(" ");
-			switch(s[0]) {
-			case "push":
-				q.push(Integer.parseInt(s[1]));
-				break;
-			case "pop":
-				System.out.println(q.pop());
-				break;
-			case "size":
-				System.out.println(q.size());
-				break;
-			case "empty":
-				if (q.empty()) System.out.println("1");
-				else System.out.println("0");
-				break;
-			case "front":
-				System.out.println(q.front());
-				break;
-			case "back":
-				System.out.println(q.back());
-				break;
-			}
-		}
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        ArrayList<Integer> queue = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            switch (command) {
+                case "push":
+                    queue.add(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    if (queue.isEmpty()) bw.write("-1\n");
+                    else bw.write(queue.remove(0) + "\n");
+                    break;
+                case "size":
+                    bw.write(queue.size() + "\n");
+                    break;
+                case "empty":
+                    if (queue.isEmpty()) bw.write("1\n");
+                    else bw.write("0\n");
+                    break;
+                case "front":
+                    if (queue.isEmpty()) bw.write("-1\n");
+                    else bw.write(queue.get(0) + "\n");
+                    break;
+                case "back":
+                    if (queue.isEmpty()) bw.write("-1\n");
+                    else bw.write(queue.get(queue.size() - 1) + "\n");
+                    break;
+            }
+        }
+        bw.flush(); bw.close();
+    }
 }
