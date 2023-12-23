5652a5c1253d14750c8acf9e1419f67d8db7107d
2jin8 <78083320+2jin8@users.noreply.github.com>
2023-12-23 16:05:15 +0900

    [Silver I] Title: 나이트의 이동, Time: 316 ms, Memory: 71968 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,52 +1,52 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    private static int[] dx = {-1, -2, -2, -1, 1, 2, 2, 1};
-    private static int[] dy = {-2, -1, 1, 2, 2, 1, -1, -2};
-    private static boolean[][] visited;
-    private static int[][] board;
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        for (int i = 0; i < n; i++) {
-            int l = scan.nextInt();
-            visited = new boolean[l][l];
+    static int l;
+    static int[][] board;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // 최소 몇 번만에 이동 -> BFS 사용
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            l = Integer.parseInt(br.readLine());
             board = new int[l][l];
-            Point start = new Point(scan.nextInt(), scan.nextInt());
-            Point end = new Point(scan.nextInt(), scan.nextInt());
-            System.out.println(bfs(start, end, l));
+            String[] start = br.readLine().split(" ");
+            String[] end = br.readLine().split(" ");
+            Point sPoint = new Point(Integer.parseInt(start[0]), Integer.parseInt(start[1]));
+            Point ePoint = new Point(Integer.parseInt(end[0]), Integer.parseInt(end[1]));
+            bw.write(bfs(sPoint, ePoint) + "\n");
         }
+        bw.flush(); bw.close();
     }
 
-    private static int bfs(Point start, Point end, int l) {
-        visited[start.x][start.y] = true;
+    public static int bfs(Point start, Point end) {
         Queue<Point> queue = new LinkedList<>();
         queue.offer(start);
+        board[start.x][start.y] = 1;
 
+        int[] dx = {-2, -2, -1, 1, 2, 2, 1, -1};
+        int[] dy = {-1, 1, 2, 2, 1, -1, -2, -2};
         while (!queue.isEmpty()) {
             Point point = queue.poll();
-            int x = point.x;
-            int y = point.y;
-            if (x == end.x && y == end.y) {
+            if (point.x == end.x && point.y == end.y)
                 break;
-            }
 
             for (int i = 0; i < 8; i++) {
-                int tx = x + dx[i];
-                int ty = y + dy[i];
+                int tx = point.x + dx[i];
+                int ty = point.y + dy[i];
 
                 if (tx < 0 || ty < 0 || tx >= l || ty >= l)
                     continue;
 
-                if (!visited[tx][ty]) {
-                    queue.add(new Point(tx, ty));
-                    visited[tx][ty] = true;
-                    board[tx][ty] = board[x][y] + 1;
+                if (board[tx][ty] == 0) {
+                    queue.offer(new Point(tx, ty));
+                    board[tx][ty] = board[point.x][point.y] + 1;
                 }
             }
         }
-        return board[end.x][end.y];
+        return board[end.x][end.y] - 1;
     }
 }
 
