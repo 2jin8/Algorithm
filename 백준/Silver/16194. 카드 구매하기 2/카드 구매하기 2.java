6262e566dcd09b7263fc5e13f6df5367a1a35ef6
2jin8 <78083320+2jin8@users.noreply.github.com>
2023-12-26 17:37:19 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 144 ms, Memory: 14468 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] str = br.readLine().split(" ");
+        int[] cards = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            cards[i] = Integer.parseInt(str[i - 1]);
+        }
+
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = cards[i];
+            for (int j = 1; j <= i / 2; j++) {
+                dp[i] = Math.min(dp[i], dp[j] + dp[i - j]);
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
