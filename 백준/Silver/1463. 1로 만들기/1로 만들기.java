4203002838fee6e1043cdb7a128dcd747954549e
2jin8 <78083320+2jin8@users.noreply.github.com>
2023-12-26 16:45:21 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 18164 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,40 +1,20 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main {
-
-    private static int INF = 1000001;
-    private static int N;
-    private static int[] T;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
-        T = new int[N + 1];
-        for (int i = 2; i <= N; i++) {
-            T[i] = INF;
-        }
-
-        System.out.println(MakeOne());
-    }
-
-    private static int MakeOne() {
-        if (N == 1)
-            return 0;
-
-        for (int i = 1; i <= N; i++) { // 하나씩 증가
-
-            if (i % 3 == 0) {
-                T[i] = Math.min(T[i], T[i / 3] + 1);
-            }
+        int[] dp = new int[1000001];
+        int n = Integer.parseInt(br.readLine());
+        dp[2] = 1; dp[3] = 1;
+        for (int i = 4; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
             if (i % 2 == 0) {
-                T[i] = Math.min(T[i], T[i / 2] + 1);
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
             }
-
-            T[i] = Math.min(T[i], T[i - 1] + 1);
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
         }
-        return T[N];
+        System.out.println(dp[n]);
     }
 }
