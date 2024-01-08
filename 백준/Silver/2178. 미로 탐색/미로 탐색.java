e9d8f674c61651000232dd0853d4e26be05e1c0e
2jin8 <78083320+2jin8@users.noreply.github.com>
2024-01-08 10:49:07 +0900

    [Silver I] Title: 미로 탐색, Time: 132 ms, Memory: 14656 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,67 +1,55 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    static int[][] board;
     static int n, m;
-    public static void main(String[] args) throws IOException {
+    static int[][] board;
+    static boolean[][] visited;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        // 지나야 하는 최소의 칸 수 - BFS
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
-        board = new int[n + 1][m + 1];
-        for (int i = 1; i <= n; i++) {
+        board = new int[n][m];
+        visited = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
             String str = br.readLine();
-            for (int j = 1; j <= m; j++) {
-                board[i][j] = str.charAt(j - 1) - '0';
+            for (int j = 0; j < m; j++) {
+                board[i][j] = str.charAt(j) - '0';
             }
         }
 
-        bfs(1, 1);
-        System.out.println(board[n][m]);
+        System.out.println(bfs(0, 0));
     }
 
-    public static void bfs(int x, int y) {
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(x, y));
+    public static int bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y});
+        visited[x][y] = true;
 
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
         while (!queue.isEmpty()) {
-            Point point = queue.poll();
-            x = point.x;
-            y = point.y;
-            if (x == n && y == m)
-                return;
+            int[] poll = queue.poll();
+            x = poll[0];
+            y = poll[1];
+            if (x == n - 1 && y == m - 1)
+                break;
 
             for (int i = 0; i < 4; i++) {
                 int tx = x + dx[i];
                 int ty = y + dy[i];
-
-                if (tx <= 0 || ty <= 0 || tx > n || ty > m) // 범위를 벗어나는 경우
+                if (tx < 0 || ty < 0 || tx >= n || ty >= m)
                     continue;
 
-                if (board[tx][ty] == 0) // 이동할 수 없는 칸인 경우
-                    continue;
-
-                if (board[tx][ty] == 1) { // 이동할 수 있는 칸 & 아직 방문하지 않은 경우
-                    board[tx][ty] = board[x][y] + 1;
-                    queue.add(new Point(tx, ty));
+                if (board[tx][ty] != 0 && !visited[tx][ty]) {
+                    board[tx][ty] = board[x][y] + 1; // 지나온 칸의 개수
+                    queue.offer(new int[]{tx, ty});
+                    visited[tx][ty] = true;
                 }
             }
         }
-    }
-
-}
-
-class Point {
-    int x;
-    int y;
-
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+        return board[n - 1][m - 1];
     }
 }
