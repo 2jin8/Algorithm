0d6b70d93fa9a96be6b9c75912925d1abcc76625
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-01-29 12:53:32 +0900

    [Silver IV] Title: ATM, Time: 132 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,28 +1,25 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // 각 사람이 돈을 인출하는데 필요한 시간의 합의 최솟값
-        // 돈을 인출하는데 걸리는 시간을 기준으로 오름차순 정렬
-        // 앞 사람부터 인출
         int n = Integer.parseInt(br.readLine());
-        int[] lines = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] times = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < n; i++) {
-            lines[i] = Integer.parseInt(st.nextToken());
+            times[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(times);
 
-        Arrays.sort(lines);
-
-        int before = lines[0], sum = lines[0];
-        for (int i = 1; i < n; i++) {
-            int time = before + lines[i];
-            before = time;
-            sum += time;
+        long total = 0L, before = 0L;
+        for (int i = 0; i < n; i++) {
+            before += times[i];
+            total += before;
         }
-        System.out.println(sum);
+        System.out.println(total);
     }
 }
