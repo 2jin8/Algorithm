e1184b5f3174ceb5026e4437aa3f0091d7b4d4c5
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-02-13 19:32:20 +0900

    [Bronze II] Title: 공 바꾸기, Time: 120 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] box = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            box[i] = i;
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            swap(box, a, b);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(box[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+
+    public static void swap(int[] arr, int a, int b) {
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+}
