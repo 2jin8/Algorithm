93f505e14109aebcc03eb28480bd822898a1420e
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-02-14 12:56:58 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -1,14 +1,15 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        long[] dp = new long[n + 1];
-        dp[1] = 1;
+    static final int MAX = 90;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[] arr = new long[MAX + 1];
+        arr[0] = 0; arr[1] = 1;
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            arr[i] = arr[i - 1] + arr[i - 2];
         }
-        System.out.println(dp[n]);
+        System.out.println(arr[n]);
     }
 }
