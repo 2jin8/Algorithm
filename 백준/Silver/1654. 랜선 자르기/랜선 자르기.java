a1910510dfb7b16b08cf35a6b53f72faabdf1992
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-02-19 13:48:55 +0900

    [Silver II] Title: 랜선 자르기, Time: 184 ms, Memory: 16840 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,53 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-/**
-  후기)
-   이분탐색으로 푸려고 한 것은 좋았다
-   But, 입력받은 배열에 대해서만 이분탐색하려고 한 것이 문제
-   유연하게 생각하는 것이 중요함
-   !! 입력받은 배열이 아닌, '1~나무 최대 높이' 배열을 이분탐색하는 것이 포인트 !!
-
-  풀이 방식)
-   1. min, max 사이의 중간값(=mid)을 기준으로(min = 1, max = 나무 최대 높이)
-     1.1 자른 높이의 합 > M : 오른쪽 배열 탐색(min = mid + 1)
-     1.2 자른 높이의 합 < M : 왼쪽 배열 탐색(max = mid - 1)
-   2. min > max인 경우 탐색 종료
-   3. mid 출력
- */
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
+    static int N, M;
+    static int[] lines;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int K = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
-
-        int[] lan = new int[K];
-        long min = 1, max = 0;
-        for (int i = 0; i < K; i++) {
-            lan[i] = Integer.parseInt(br.readLine());
-            max = Math.max(max, lan[i]);
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        lines = new int[N];
+        for (int i = 0; i < N; i++) {
+            lines[i] = Integer.parseInt(br.readLine());
         }
+        Arrays.sort(lines);
 
-        long mid = (min + max) / 2;
-        while (min <= max) {
-            long sum = 0;
+        System.out.println(biSearch(1, lines[N - 1]));
+    }
 
-            for (int l : lan) {
-                sum += l / mid;
+    public static long biSearch(long start, long end) {
+        long ans = 0;
+        while (start <= end) {
+            long mid = (start + end) / 2;
+            int total = 0;
+            for (int i = 0; i < N; i++) {
+                total += lines[i] / mid;
             }
-
-            if (sum < N) {
-                max = mid - 1;
-            } else if (sum >= N) {
-                min = mid + 1;
+            if (total < M) { // 랜선이 더 필요한 경우
+                end = mid - 1; // 왼쪽으로 범위 이동
+            } else { // 랜선이 충분한 경우
+                ans = mid;
+                start = mid + 1;
             }
-            mid = (min + max) / 2;
         }
-        System.out.println(mid);
+        return ans;
     }
 }
