05321e63b569a02e6cd0b14690fd25aba5ce7f25
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-03-21 21:58:05 +0900

    [Gold IV] Title: 인구 이동, Time: 628 ms, Memory: 299036 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -3,97 +3,82 @@ import java.util.*;
 
 public class Main {
     static int N, L, R;
-    static int[][] land, tmp;
+    static boolean isMove; // 인구 이동 여부
+    static int[][] map, arr;
     static boolean[][] visited;
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         L = Integer.parseInt(st.nextToken());
         R = Integer.parseInt(st.nextToken());
-        land = new int[N][N];
+        map = new int[N][N];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             for (int j = 0; j < N; j++) {
-                land[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         int day = 0;
         while (true) {
-            tmp = new int[N][N]; // 인구 이동을 위한 임시 배열
-            for (int i = 0; i < N; i++) {
-                tmp[i] = land[i].clone();
-            }
+            arr = map.clone();
             visited = new boolean[N][N];
+            isMove = false;
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
                     if (!visited[i][j]) {
-                        bfs(new Point(i, j));
+                        bfs(i, j);
                     }
                 }
             }
-            if (equals()) { // 인구 이동 전이랑 후가 동일한 경우
-                break;
-            }
+            map = arr.clone();
+            if (!isMove) break;
             day++;
-            for (int i = 0; i < N; i++) {
-                land[i] = tmp[i].clone();
-            }
         }
         System.out.println(day);
     }
 
-    public static boolean equals() {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (land[i][j] != tmp[i][j]) return false;
-            }
-        }
-        return true;
-    }
+    public static void bfs(int x, int y) {
+        Queue<Pos> queue = new LinkedList<>();
+        queue.offer(new Pos(x, y));
+        visited[x][y] = true;
 
-    public static void bfs(Point point) {
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(point);
-        visited[point.x][point.y] = true;
-
-        int total = 0, cnt = 0;
-        List<Point> list = new ArrayList<>();
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
+        int total = 0, cnt = 0; // 이동하는 인구의 수와 국가의 수
+        List<Pos> posList = new ArrayList<>();
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
         while (!queue.isEmpty()) {
-            point = queue.poll();
-            list.add(point);
-            int x = point.x;
-            int y = point.y;
-            total += land[x][y];
+            Pos pos = queue.poll();
+            x = pos.x; y = pos.y;
+            total += map[x][y];
             cnt++;
+            posList.add(new Pos(x, y));
 
             for (int i = 0; i < 4; i++) {
-                int tx = x + dx[i];
-                int ty = y + dy[i];
-                if (tx < 0 || ty < 0 || tx >= N || ty >= N) continue;
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny]) continue;
 
-                int diff = Math.abs(land[x][y] - land[tx][ty]);
-                if (!visited[tx][ty] && diff >= L && diff <= R) {
-                    visited[tx][ty] = true;
-                    queue.offer(new Point(tx, ty));
+                int diff = Math.abs(map[x][y] - map[nx][ny]); // 인구 차이
+                if (diff >= L && diff <= R) { // 인구 차이가 L명 이상, R명 이하인 경우
+                    queue.offer(new Pos(nx, ny));
+                    visited[nx][ny] = true;
                 }
             }
         }
-        if (total == 0) return;
-        int newTotal = total / cnt;
-        for (Point p : list) { // 인구 조절
-            tmp[p.x][p.y] = newTotal;
+
+        int value = total / cnt; // 새로 변경될 연합의 인구 수
+        for (Pos pos : posList) { // 인구 이동
+            map[pos.x][pos.y] = value;
         }
+        isMove |= cnt != 1; // cnt가 1이면 인구 이동이 없었다는 의미
     }
 
-    static class Point {
-        int x;
-        int y;
+    static class Pos {
+        int x, y;
 
-        public Point(int x, int y) {
+        public Pos(int x, int y) {
             this.x = x;
             this.y = y;
         }
