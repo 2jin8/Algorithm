d53bf3009d4ab192b19736054cb6aae8d139dd14
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-03-27 12:47:03 +0900

    [Silver IV] Title: 큐 2, Time: 1432 ms, Memory: 334264 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -1,41 +1,39 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        Deque<Integer> q = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+        Deque<Integer> deque = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             String cmd = st.nextToken();
             switch (cmd) {
                 case "push":
-                    q.offerLast(Integer.parseInt(st.nextToken()));
+                    deque.offer(Integer.parseInt(st.nextToken()));
                     break;
                 case "pop":
-                    if (q.isEmpty()) bw.write("-1\n");
-                    else bw.write(q.removeFirst() + "\n");
+                    if (deque.isEmpty()) sb.append("-1\n");
+                    else sb.append(deque.pollFirst()).append("\n");
                     break;
                 case "size":
-                    bw.write(q.size() + "\n");
+                    sb.append(deque.size()).append("\n");
                     break;
                 case "empty":
-                    if (q.isEmpty()) bw.write("1\n");
-                    else bw.write("0\n");
+                    sb.append(deque.isEmpty() ? 1 : 0).append("\n");
                     break;
                 case "front":
-                    if (q.isEmpty()) bw.write("-1\n");
-                    else bw.write(q.getFirst() + "\n");
+                    if (deque.isEmpty()) sb.append("-1\n");
+                    else sb.append(deque.peekFirst()).append("\n");
                     break;
                 case "back":
-                    if (q.isEmpty()) bw.write("-1\n");
-                    else bw.write(q.getLast() + "\n");
+                    if (deque.isEmpty()) sb.append("-1\n");
+                    else sb.append(deque.peekLast()).append("\n");
                     break;
             }
         }
-        br.close();
-        bw.flush(); bw.close();
+        System.out.println(sb.toString());
     }
 }
