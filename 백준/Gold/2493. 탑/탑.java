2aca4048dbae0ef956ec56b92213c6cd337e2e5b
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-04-04 13:25:50 +0900

    [Gold V] Title: 탑, Time: 748 ms, Memory: 87056 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,38 +1,51 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Stack<Top> stack = new Stack<>();
+        int[] heights = new int[n];
         for (int i = 0; i < n; i++) {
-            int height = Integer.parseInt(st.nextToken());
-            if (stack.isEmpty()) { // 스택이 비어있으면
-                bw.write("0 "); // 0 출력
-            } else {
-                // 자기보다 높은 탑이 나올 때까지 pop
-                while (!stack.isEmpty() && height > stack.peek().height) {
+            heights[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Stack<Top> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            if (stack.isEmpty()) { // 스택이 비어있다면 push & 0 기록
+                stack.push(new Top(heights[i], i + 1));
+                sb.append("0 ");
+                continue;
+            }
+
+            boolean check = false;
+            while (!stack.isEmpty()) {
+                if (stack.peek().height > heights[i]) { // 'peek 높이 > 현재 높이'인 경우
+                    // 기록 & push
+                    sb.append(stack.peek().idx).append(" ");
+                    stack.push(new Top(heights[i], i + 1));
+                    check = true;
+                    break;
+                } else { // 아니라면 그냥 pop
                     stack.pop();
                 }
-                if (stack.isEmpty()) bw.write("0 ");
-                else bw.write(stack.peek().order + " ");
             }
-            stack.push(new Top(i + 1, height)); // 스택에 현재 탑 추가
+            if (!check) {
+                sb.append("0 ");
+                stack.push(new Top(heights[i], i + 1));
+            }
         }
-        bw.flush(); bw.close();
+        System.out.println(sb.toString());
     }
-}
 
-class Top {
-    int order; // 순서
-    int height; // 높이
+    static class Top {
+        int height, idx;
 
-    public Top(int order, int height) {
-        this.order = order;
-        this.height = height;
+        public Top(int height, int idx) {
+            this.height = height;
+            this.idx = idx;
+        }
     }
 }
