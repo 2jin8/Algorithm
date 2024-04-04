6561ab7f779c5abdc42606cd15ac6e22ec3232f5
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-04-04 14:27:30 +0900

    [Silver I] Title: 음식물 피하기, Time: 160 ms, Memory: 15764 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
@@ -2,9 +2,11 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N, M, K, maxArea;
+    static int N, M, K, maxArea, area;
     static int[][] map;
     static boolean[][] visited;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -23,36 +25,25 @@ public class Main {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 if (map[i][j] == 1 && !visited[i][j]) {
-                    maxArea = Math.max(bfs(i, j), maxArea);
+                    area = 0;
+                    dfs(i, j);
+                    maxArea = Math.max(area, maxArea);
                 }
             }
         }
         System.out.println(maxArea);
     }
 
-    public static int bfs(int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
+    public static void dfs(int x, int y) {
         visited[x][y] = true;
-        queue.offer(new int[]{x, y});
+        area++;
 
-        int area = 0;
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
-        while (!queue.isEmpty()) {
-            int[] poll = queue.poll();
-            area++;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny]) continue;
 
-            for (int i = 0; i < 4; i++) {
-                int nx = poll[0] + dx[i];
-                int ny = poll[1] + dy[i];
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny]) continue;
-
-                if (map[nx][ny] == 1) {
-                    queue.offer(new int[]{nx, ny});
-                    visited[nx][ny] = true;
-                }
-            }
+            if (map[nx][ny] == 1) dfs(nx, ny);
         }
-        return area;
     }
 }
