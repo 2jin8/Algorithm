60c73c93131bf5ad5aaa7ec861cb0b1b0d1183db
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-04-08 17:08:47 +0900

    [Gold V] Title: 김밥천국의 계단, Time: 172 ms, Memory: 18188 KB -BaekjoonHub

--- 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.java
+++ 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.java
@@ -3,53 +3,27 @@ import java.io.*;
 
 public class Main {
     static int N, K;
-    static boolean[] visited;
+    static int[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        visited = new boolean[N + 1];
-        System.out.println(bfs());
-    }
-    
-    public static String bfs() {
-        Queue<Stair> queue = new LinkedList<>();
-        queue.offer(new Stair(0, 0));
-        visited[0] = true;
-        
-        while (!queue.isEmpty()) {
-            Stair now = queue.poll();
-            if (now.num == N && now.cnt <= K) {
-                return "minigimbob";
+
+        dp = new int[N + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 0; i < N; i++) {
+            int next = i + 1;
+            if (next <= N) {
+                dp[next] = Math.min(dp[next], dp[i] + 1);
             }
-            
-            if (now.cnt > K) break;
-            
-            // 한 칸 올라가기
-            int next = now.num + 1;
-            if (next <= N && !visited[next]) {
-                queue.offer(new Stair(next, now.cnt + 1));
-                visited[next] = true;
-            }
-            
-            // 순간 이동하기
-            next = now.num + now.num / 2;
-            if (next <= N && !visited[next]) {
-                queue.offer(new Stair(next, now.cnt + 1));
-                visited[next] = true;
+
+            next = i + i / 2;
+            if (next <= N) {
+                dp[next] = Math.min(dp[next], dp[i] + 1);
             }
         }
-        return "water";
-    }
-    
-    static class Stair {
-        int num; // 계단의 번호
-        int cnt; // 해당 계단까지 올라가는 횟수
-        
-        public Stair(int num, int cnt) {
-            this.num = num;
-            this.cnt = cnt;
-        }
+        System.out.println(dp[N] <= K ? "minigimbob" : "water");
     }
 }
