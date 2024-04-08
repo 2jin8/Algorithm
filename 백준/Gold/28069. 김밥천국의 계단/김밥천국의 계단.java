b795941045e2c0afe4bbd27624809e6f610137f4
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-04-08 17:11:05 +0900

    [Gold V] Title: 김밥천국의 계단, Time: 168 ms, Memory: 18296 KB -BaekjoonHub

--- 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.java
+++ 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static final int INF = 1_000_001;
     static int N, K;
     static int[] dp;
     public static void main(String[] args) throws IOException {
@@ -11,15 +12,15 @@ public class Main {
         K = Integer.parseInt(st.nextToken());
 
         dp = new int[N + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
+        Arrays.fill(dp, INF);
         dp[0] = 0;
         for (int i = 0; i < N; i++) {
-            int next = i + 1;
+            int next = i + 1; // 한 칸 올라가기
             if (next <= N) {
                 dp[next] = Math.min(dp[next], dp[i] + 1);
             }
 
-            next = i + i / 2;
+            next = i + i / 2; // 순간이동하기
             if (next <= N) {
                 dp[next] = Math.min(dp[next], dp[i] + 1);
             }
