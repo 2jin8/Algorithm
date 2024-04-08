621a132cb2f230394074395220713ed93c7d513b
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-04-08 10:30:56 +0900

    [Silver IV] Title: 카드2, Time: 184 ms, Memory: 45556 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,24 +1,19 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        Deque<Integer> deque = new ArrayDeque<>();
+        Queue<Integer> queue = new LinkedList<>();
         for (int i = 1; i <= n; i++) {
-            deque.add(i);
+            queue.offer(i);
         }
 
-        while (deque.size() > 1) {
-            // 1. 제일 위에 있는 카드 버리기
-            deque.removeFirst();
-
-            // 2. 제일 위에 있는 카드를 제일 아래에 있는 카드 밑에 넣기
-            if (deque.size() == 1) break;
-            int remove = deque.removeFirst();
-            deque.offerLast(remove);
+        while (queue.size() > 1) {
+            queue.poll(); // 제일 위에 있는 카드 버리기
+            queue.offer(queue.poll()); // 그 다음으로 제일 위에 있는 카드 밑으로 옮기기
         }
-        System.out.println(deque.getFirst());
+        System.out.println(queue.poll());
     }
 }
