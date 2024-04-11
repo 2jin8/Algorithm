5b591e5292d30902ea23052abe94bd470ebb9a6e
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-04-11 14:28:59 +0900

    [Silver III] Title: N과 M (1), Time: 332 ms, Memory: 29152 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,38 +1,35 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    static int n, m;
-    static boolean[] visited;
-    static int[] result;
+    static int N, M;
+    static boolean[] used;
     static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // 1부터 n까지 m 자리의 수
-        n = sc.nextInt();
-        m = sc.nextInt();
-        visited = new boolean[n + 1];
-        result = new int[m];
-
-        dfs(0);
-        System.out.println(sb);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        used = new boolean[N + 1];
+        for (int i = 1; i <= N; i++) {
+            used[i] = true;
+            dfs(i + " ", 1);
+            used[i] = false;
+        }
+        System.out.println(sb.toString());
     }
 
-    public static void dfs(int cnt) {
-        if (cnt == m) { // 탐색 완료된 경우
-            for (int i = 0; i < m; i++) {
-                sb.append(result[i]).append(" ");
-            }
-            sb.append("\n");
+    public static void dfs(String num, int depth) {
+        if (depth == M) {
+            sb.append(num).append("\n");
             return;
         }
 
-        for (int i = 1; i <= n; i++) {
-            if (!visited[i]) { // 해당 수, 아직 방문하지 않았다면
-                visited[i] = true; // 방문 처리
-                result[cnt] = i;
-                dfs(cnt + 1); // dfs 탐색
-                visited[i] = false; // 탐색 후, 방문 처리 취소
+        for (int i = 1; i <= N; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                dfs(num + i + " ", depth + 1);
+                used[i] = false;
             }
         }
     }
