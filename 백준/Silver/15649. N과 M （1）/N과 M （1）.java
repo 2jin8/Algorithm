b2f98a12bfc34ad7d87b1ebd951a2fe9ce60b13c
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-04-11 14:33:38 +0900

    [Silver III] Title: N과 M (1), Time: 276 ms, Memory: 22764 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class Main {
     static int N, M;
     static boolean[] used;
+    static int[] answer;
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,24 +12,30 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         used = new boolean[N + 1];
+        answer = new int[M];
         for (int i = 1; i <= N; i++) {
             used[i] = true;
-            dfs(i + " ", 1);
+            answer[0] = i;
+            dfs(1);
             used[i] = false;
         }
         System.out.println(sb.toString());
     }
 
-    public static void dfs(String num, int depth) {
+    public static void dfs(int depth) {
         if (depth == M) {
-            sb.append(num).append("\n");
+            for (int a : answer) {
+                sb.append(a).append(" ");
+            }
+            sb.append("\n");
             return;
         }
 
         for (int i = 1; i <= N; i++) {
             if (!used[i]) {
                 used[i] = true;
-                dfs(num + i + " ", depth + 1);
+                answer[depth] = i;
+                dfs(depth + 1);
                 used[i] = false;
             }
         }
