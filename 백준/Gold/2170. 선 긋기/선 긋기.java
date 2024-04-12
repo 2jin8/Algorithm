298116c3cfc5d2bc044dc205a2c666008bf05cf8
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-04-12 20:03:29 +0900

    [Gold V] Title: 선 긋기, Time: 1796 ms, Memory: 272844 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.java
+++ 백준/Gold/2170. 선 긋기/선 긋기.java
@@ -6,46 +6,44 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        Line[] lines = new Line[n];
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            lines[i] = new Line(start, end);
-        }
-        Arrays.sort(lines, new Comparator<Line>() {
+        // x를 기준으로 오름차순 정렬, x가 같다면 y를 기준으로 오름차순 정렬
+        PriorityQueue<Line> pq = new PriorityQueue<>(new Comparator<Line>() {
             @Override
             public int compare(Line o1, Line o2) {
-                if (o1.start == o2.start) return o1.end - o2.end;
-                return o1.start - o2.start;
+                if (o1.x == o2.x) return o1.y - o2.y;
+                return o1.x - o2.x;
             }
         });
 
-        PriorityQueue<Line> pq = new PriorityQueue<>((l1, l2) -> l2.start - l1.start);
-        pq.offer(lines[0]);
-        for (int i = 1; i < n; i++) {
-            Line line = lines[i];
-            Line peek = pq.peek();
-            if (line.start >= peek.start && line.start <= peek.end) { // 시작점이 이미 그어진 선과 겹치는 경우
-                pq.poll();
-                pq.offer(new Line(peek.start, Math.max(peek.end, line.end))); // 종료점을 새로 갱신해서 넣기
-            } else {
-                pq.offer(line);
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            pq.offer(new Line(x, y));
+        }
+
+        Line line = pq.poll();
+        int start = line.x, end = line.y, total = 0;
+        while (!pq.isEmpty()) {
+            line = pq.poll();
+            if (line.x >= start && line.x <= end) { // 그으려는 선이 이미 그어진 선과 겹치는 경우라면
+                end = Math.max(line.y, end); // 두 y값 중 더 큰 값으로 갱신
+            } else { // 그렇지 않다면
+                total += end - start; // 이때까지 그은 선의 길이 세기
+                start = line.x; // 새로운 start, end 값으로 갱신하기
+                end = line.y;
             }
         }
-        int total = 0;
-        while (!pq.isEmpty()) {
-            Line line = pq.poll();
-            total += line.end - line.start;
-        }
+        total += end - start;
         System.out.println(total);
     }
-    static class Line {
-        int start, end;
 
-        public Line(int start, int end) {
-            this.start = start;
-            this.end = end;
+    static class Line {
+        int x, y;
+
+        public Line(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 }
