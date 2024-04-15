181b87e51a2623e27ef2e261194dd90682e347a5
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-04-15 13:55:38 +0900

    [Silver II] Title: 쇠막대기, Time: 180 ms, Memory: 16312 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,31 +1,31 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        Queue<Character> pipes = new LinkedList<>();
-        for (int i = 0; i < str.length(); i++) {
-            pipes.offer(str.charAt(i));
-        }
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String pipes = br.readLine();
         int total = 0;
         Stack<Character> stack = new Stack<>();
-        while (!pipes.isEmpty()) {
-            char p1 = pipes.poll().charValue();
-            if (p1 == ')') { // 쇠막대기의 오른쪽 끝 → 1 더하고, 스택에서 pop
-                stack.pop();
-                total++;
-            } else if (p1 == '(') { // 쇠막대기의 왼쪽 끝
-                if (!pipes.isEmpty() && pipes.peek().charValue() == ')') { // 레이저인 경우
-                    total += stack.size(); // 스택에 있는 수만큼 잘라짐
-                    pipes.poll();
-                } else { // 쇠막대기인 경우
-                    stack.push(p1);
+        for (int i = 0; i < pipes.length() - 1; i++) {
+            char now = pipes.charAt(i);
+            if (now == '(') { // 현재 괄호가 열린 괄호라면
+                char next = pipes.charAt(i + 1); // 다음 괄호 확인하기
+                if (next == ')') { // 다음 괄호가 닫힌 괄호라면
+                    // 레이저를 뜻하므로 잘린 파이프의 수(=스택의 크기) 더하기
+                    total += stack.size();
+                    i++;
+                } else {
+                    // 그렇지 않다면 막대기를 뜻하므로 스택에 넣기
+                    stack.push(now);
                 }
+            } else { // 닫힌 괄호라면 파이프의 끝을 뜻하므로 1 더하기
+                total++;
+                stack.pop();
             }
         }
+        // 편의상 pipes.length - 1까지 탐색했으므로, 마지막의 괄호(=파이프의 끝) 고려하기
+        if (!stack.isEmpty()) total++;
         System.out.println(total);
     }
 }
