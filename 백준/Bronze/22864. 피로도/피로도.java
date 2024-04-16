49ad928bb11222bbe6b3a94628ebb5ccdc83bd6b
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-04-16 11:44:07 +0900

    [Bronze II] Title: 피로도, Time: 124 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/22864. 피로도/피로도.java
+++ 백준/Bronze/22864. 피로도/피로도.java
@@ -1,23 +1,24 @@
+import java.util.*;
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int a = Integer.parseInt(st.nextToken()); // 쌓이는 피로도
-        int b = Integer.parseInt(st.nextToken()); // 처리할 수 있는 일의 양
-        int c = Integer.parseInt(st.nextToken()); // 줄어드는 피로도
-        int m = Integer.parseInt(st.nextToken()); // 최대 피로도
+        int A = Integer.parseInt(st.nextToken()); // 쌓이는 피로도
+        int B = Integer.parseInt(st.nextToken()); // 처리한 일
+        int C = Integer.parseInt(st.nextToken()); // 줄어드는 피로도
+        int M = Integer.parseInt(st.nextToken()); // 번아웃 방지 최대치 피로도
 
-        // 24번 반복
-        int work = 0, tired = 0;
+        int tired = 0, work = 0;
         for (int i = 0; i < 24; i++) {
-            if (tired + a > m) { // 쉬지 않을 때 피로도가 m을 넘는 경우
-                tired = Math.max(0, tired - c);
-            } else { // 쉬지 않고 일을 하는 경우
-                tired += a;
-                work += b;
+            if (tired + A > M) { // 일을 한다면 번아웃이 발생하는 경우
+                // 쉬어주기
+                tired = Math.max(0, tired - C); // 음수 방지
+            } else {
+                // 일하기
+                tired += A;
+                work += B;
             }
         }
         System.out.println(work);
