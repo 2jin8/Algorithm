5177779e085696cb4cb85f83872e2eca2040fd23
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-04-16 11:36:41 +0900

    [Bronze II] Title: 시각, Time: 196 ms, Memory: 19240 KB -BaekjoonHub

--- 백준/Bronze/18312. 시각/시각.java
+++ 백준/Bronze/18312. 시각/시각.java
@@ -1,22 +1,34 @@
+import java.util.*;
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        // 00시 00분 00초 ~ n시 59분 59초
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        // 00시 00분 00초 ~ N시 59분 59초
+        // K가 하나라도 포함되는 시각
         int total = 0;
-        for (int h = 0; h <= n; h++) {
+        String value = String.valueOf(K);
+        for (int h = 0; h <= N; h++) {
+            String hour = String.valueOf(h);
+            if (hour.length() == 1) hour = "0" + hour;
+            if (hour.contains(value)) { // 시에 K가 포함된 경우
+                total += 60 * 60;
+                continue;
+            }
             for (int m = 0; m < 60; m++) {
+                String minute = String.valueOf(m);
+                if (minute.length() == 1) minute = "0" + minute;
+                if (minute.contains(value)) { // 분에 K가 포함된 경우
+                    total += 60;
+                    continue;
+                }
                 for (int s = 0; s < 60; s++) {
-                    StringBuilder time = new StringBuilder();
-                    time.append(h < 10 ? "0" + h : "" + h)
-                            .append(m < 10 ? "0" + m : "" + m)
-                            .append(s < 10 ? "0" + s : "" + s);
-                    if (time.toString().contains("" + k)) {
+                    String second = String.valueOf(s);
+                    if (second.length() == 1) second = "0" + second;
+                    if (second.contains(value)) { // 초에 K가 포함된 경우
                         total++;
                     }
                 }
