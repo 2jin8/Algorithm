6d700f147f6de46d05b65bb671c3842ee83a9903
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-04-18 12:29:25 +0900

    [Gold V] Title: 괄호의 값, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.java
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -4,46 +4,45 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
+        String str = br.readLine();
 
         int answer = 0, total = 1;
-        boolean flag = false;
         Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char now = s.charAt(i);
-            if (now == '(') { 
-                stack.push(now); 
-                total *= 2; // '(' 값은 2
+        for (int i = 0; i < str.length(); i++) {
+            char now = str.charAt(i);
+            // 왼쪽 괄호면 스택에 push & total에 괄호 값 곱하기
+            if (now == '(') {
+                stack.push(now);
+                total *= 2;
             } else if (now == '[') {
                 stack.push(now);
-                total *= 3; // '[' 값은 3
+                total *= 3;
             } else if (now == ')') {
-                if (stack.isEmpty() || stack.peek() != '(') { // 짝이 맞는게 없다면
-                    // 입력이 올바르지 못한 것
+                // ')'의 짝인 '('가 없으면 입력이 올바르지 못한 괄호열
+                if (stack.isEmpty() || stack.peek() != '(') {
                     System.out.println(0);
                     return;
                 }
-
-                if (s.charAt(i - 1) == '(') { // 짝이 맞는 괄호가 연달아 나온다면 식의 마지막을 뜻함
-                    answer += total; // answer에 이때까지 구한 값 더하기
+                // 짝이 맞는 괄호가 연속해서 있다면 더할 차례
+                if (str.charAt(i - 1) == '(') {
+                    answer += total;
                 }
                 total /= 2;
                 stack.pop();
             } else if (now == ']') {
-                if (stack.isEmpty() || stack.peek() != '[') { // 짝이 맞는게 없다면
-                    // 입력이 올바르지 못한 것
+                // ']'의 짝인 '['가 없으면 입력이 올바르지 못한 문자열
+                if (stack.isEmpty() || stack.peek() != '[') {
                     System.out.println(0);
                     return;
                 }
-
-                if (s.charAt(i - 1) == '[') { // 짝이 맞는 괄호가 연달아 나온다면 식의 마지막을 뜻함
-                    answer += total; // answer에 이때까지 구한 값 더하기
+                if (str.charAt(i - 1) == '[') {
+                    answer += total;
                 }
                 total /= 3;
                 stack.pop();
             }
         }
-        if (!stack.isEmpty()) System.out.println(0);
-        else System.out.println(answer);
+        if (stack.isEmpty()) System.out.println(answer);
+        else System.out.println(0);
     }
 }
