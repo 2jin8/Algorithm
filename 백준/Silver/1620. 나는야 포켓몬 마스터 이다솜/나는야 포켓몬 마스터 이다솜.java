88b7cf961289e932c65c70fe70723b18e8e843ef
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-04-18 11:45:25 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 524 ms, Memory: 54364 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,31 +1,31 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        HashMap<Integer, String> findString = new HashMap<>();
-        HashMap<String, Integer> findDigit = new HashMap<>();
-
-        for (int i = 1; i <= n; i++) {
-            String s = br.readLine();
-            findString.put(i, s);
-            findDigit.put(s, i);
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        HashMap<String, String> getName = new HashMap<>();
+        HashMap<String, String> getNumber = new HashMap<>();
+        for (int i = 1; i <= N; i++) {
+            String number = String.valueOf(i);
+            String name = br.readLine();
+            getName.put(number, name);
+            getNumber.put(name, number);
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < m; i++) {
-            String str = br.readLine();
-            if (Character.isDigit(str.charAt(0))) { // 입력이 숫자
-                sb.append(findString.get(Integer.parseInt(str))).append("\n");
-            } else { // 입력이 문자
-                sb.append(findDigit.get(str)).append("\n");
+        for (int i = 0; i < M; i++) {
+            String input = br.readLine();
+            if (getName.containsKey(input)) {
+                sb.append(getName.get(input));
+            } else {
+                sb.append(getNumber.get(input));
             }
+            sb.append("\n");
         }
-        System.out.println(sb);
+        System.out.println(sb.toString());
     }
 }
