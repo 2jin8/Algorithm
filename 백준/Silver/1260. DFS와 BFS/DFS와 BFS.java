3e31a4b9cfc916a2fd0af29a68617874e444020e
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-04-25 13:37:37 +0900

    [Silver II] Title: DFS와 BFS, Time: 232 ms, Memory: 20104 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -2,67 +2,69 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    private static int n, m, v;
-    private static boolean[] bVisited, dVisited;
-    private static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-    private static StringBuilder sb = new StringBuilder();
+    static boolean[] visited;
+    static ArrayList<Integer>[] lists;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken()); // 정점의 개수
-        m = Integer.parseInt(st.nextToken()); // 간선의 개수
-        v = Integer.parseInt(st.nextToken()); // 탐색을 시작할 정점의 번호
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        bVisited = new boolean[n + 1];
-        dVisited = new boolean[n + 1];
-
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            // 양방향
-            graph.get(a).add(b);
-            graph.get(b).add(a);
+        // 정점 번호가 작은 것부터 탐색
+        lists = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+        for (int i = 0; i <= N; i++) {
+            lists[i] = new ArrayList<>();
         }
 
-        for (int i = 1; i <= n; i++) {
-            // 방문할 수 있는 정점이 여러 개 → 정점 번호가 작은 것부터
-            Collections.sort(graph.get(i));
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            lists[u].add(v);
+            lists[v].add(u);
         }
+        for (int i = 1; i <= N; i++) {
+            // 정점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 방문하기
+            Collections.sort(lists[i]);
+        }
+        visited = new boolean[N + 1];
+        dfs(V);
 
-        dfs(v, 0);
-        sb.append("\n");
-        bfs(v);
-        System.out.println(sb);
+        visited = new boolean[N + 1];
+        bfs(V);
+        System.out.println(sb.toString());
     }
 
-    private static void dfs(int x, int depth) {
-        dVisited[x] = true;
-        sb.append(x).append(" ");
+    public static void dfs(int v) {
+        visited[v] = true;
+        sb.append(v).append(" ");
 
-        for (int i = 0; i < graph.get(x).size(); i++) {
-            int y = graph.get(x).get(i);
-            if (!dVisited[y]) dfs(y, depth + 1);
+        for (int i = 0; i < lists[v].size(); i++) {
+            int next = lists[v].get(i);
+            if (!visited[next]) {
+                dfs(next);
+            }
         }
     }
 
-    private static void bfs(int x) {
+    public static void bfs(int v) {
         Queue<Integer> queue = new LinkedList<>();
-        queue.offer(x);
-        bVisited[x] = true;
+        queue.offer(v);
+        visited[v] = true;
 
+        sb.append("\n");
         while (!queue.isEmpty()) {
-            x = queue.poll();
-            sb.append(x).append(" ");
+            int now = queue.poll();
+            sb.append(now).append(" ");
 
-            for (int i = 0; i < graph.get(x).size(); i++) {
-                int y = graph.get(x).get(i);
-                if (!bVisited[y]) {
-                    bVisited[y] = true;
-                    queue.offer(y);
+            for (int i = 0; i < lists[now].size(); i++) {
+                int next = lists[now].get(i);
+                if (!visited[next]) {
+                    queue.offer(next);
+                    visited[next] = true;
                 }
             }
         }
