541f2914bd02a070569b567c01a84c01e0346961
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-04-26 13:16:45 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 692 ms, Memory: 50704 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -1,77 +1,66 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     static int N, M;
-    static int[][] map, ans;
+    static int[][] map, dist;
     static boolean[][] visited;
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         map = new int[N][M];
-        ans = new int[N][M];
+        dist = new int[N][M];
         visited = new boolean[N][M];
-        Point start = null;
+        int startX = -1, startY = -1;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
+            Arrays.fill(dist[i], -1);
             for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == 2) start = new Point(i, j);
-            }
-        }
-        bfs(start);
-        check();  // 원래 갈 수 있는 땅인 부분 중에서 도달할 수 없는 위치 체크하기
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                sb.append(ans[i][j] + " ");
-            }
-            sb.append("\n");
-        }
-        System.out.println(sb);
-    }
-
-    public static void check() {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] == 1 && ans[i][j] == 0) ans[i][j] = -1;
-            }
-        }
-    }
-
-    public static void bfs(Point start) {
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(start);
-        visited[start.x][start.y] = true;
-
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        while (!queue.isEmpty()) {
-            Point point = queue.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int tx = point.x + dx[i];
-                int ty = point.y + dy[i];
-                if (tx < 0 || ty < 0 || tx >= N || ty >= M) continue;
-
-                if (!visited[tx][ty] && map[tx][ty] == 1) {
-                    queue.offer(new Point(tx, ty));
-                    visited[tx][ty] = true;
-                    ans[tx][ty] = ans[point.x][point.y] + 1;
+                if (map[i][j] == 0) {
+                    dist[i][j] = 0;
+                } else if (map[i][j] == 2) {
+                    startX = i;
+                    startY = j;
                 }
             }
         }
+
+        bfs(startX, startY);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                sb.append(dist[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
     }
 
-    static class Point {
-        int x;
-        int y;
+    static void bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y});
+        visited[x][y] = true;
+        dist[x][y] = 0;
 
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny] || map[nx][ny] == 0) {
+                    continue;
+                }
+
+                dist[nx][ny] = dist[now[0]][now[1]] + 1; // 목표지점에서 걸리는 거리
+                queue.offer(new int[]{nx, ny});
+                visited[nx][ny] = true;
+            }
         }
     }
 }
