e8612bd0c63c970072558f7ffdfb6ec7934c493c
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-04-26 12:14:59 +0900

    [Silver I] Title: 단지번호붙이기, Time: 132 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,49 +2,63 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n, depth;
-    static char[][] house;
+    static int N;
+    static int[][] map;
     static boolean[][] visited;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
+    static ArrayList<Integer> answer = new ArrayList<>();
     public static void main(String[] args) throws IOException {
+        // 지나야하는 최소 칸 수 == 최단 거리 => BFS
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        house = new char[n][n];
-        visited = new boolean[n][n];
-
-        for (int i = 0; i < n; i++) {
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        visited = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
             String str = br.readLine();
-            for (int j = 0; j < n; j++) {
-                house[i][j] = str.charAt(j);
+            for (int j = 0; j < N; j++) {
+                map[i][j] = str.charAt(j) - '0';
             }
         }
 
-        ArrayList<Integer> results = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!visited[i][j] && house[i][j] == '1') {
-                    depth = 0;
-                    dfs(i, j);
-                    results.add(depth);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j] && map[i][j] == 1) {
+                    bfs(i, j);
                 }
             }
         }
-        System.out.println(results.size());
-        results.stream().sorted().forEach(System.out::println);
+        StringBuilder sb = new StringBuilder();
+        sb.append(answer.size()).append("\n");
+        Collections.sort(answer);
+        for (int a : answer) {
+            sb.append(a).append("\n");
+        }
+        System.out.println(sb.toString());
     }
 
-    public static void dfs(int x, int y) {
+    public static void bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y});
         visited[x][y] = true;
-        depth++;
 
-        for (int i = 0; i < 4; i++) {
-            int tx = x + dx[i];
-            int ty = y + dy[i];
-            if (tx < 0 || ty < 0 || tx >= n || ty >= n)
-                continue;
+        int area = 0;
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            area++;
 
-            if (!visited[tx][ty] && house[tx][ty] == '1') dfs(tx, ty);
+            for (int i = 0; i < 4; i++) {
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
+                // 범위 벗어나는 경우 or 이미 방문한 경우 or 이동할 수 없는 칸인 경우
+                if (nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny] || map[nx][ny] == 0) {
+                    continue;
+                }
+
+                visited[nx][ny] = true;
+                queue.offer(new int[]{nx, ny});
+            }
         }
+        answer.add(area);
     }
 }
