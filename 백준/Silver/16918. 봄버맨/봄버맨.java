baee302f9db1ac47657f6f548439eb75c817197c
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-04-26 13:00:14 +0900

    [Silver I] Title: 봄버맨, Time: 316 ms, Memory: 64576 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.java
+++ 백준/Silver/16918. 봄버맨/봄버맨.java
@@ -3,90 +3,84 @@ import java.io.*;
 
 public class Main {
     static int R, C, N;
-    static char[][] board, full;
-    static Queue<Point> queue = new LinkedList<>();
+    static char[][] map, full;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-        board = new char[R][C];
+        map = new char[R][C];
         full = new char[R][C];
         for (int i = 0; i < R; i++) {
             String str = br.readLine();
             for (int j = 0; j < C; j++) {
-                board[i][j] = str.charAt(j);
-                if (board[i][j] == 'O') {
-                    queue.offer(new Point(i, j));
-                }
+                map[i][j] = str.charAt(j);
             }
+            Arrays.fill(full[i], 'O');
         }
-        StringBuilder sb = new StringBuilder();
-        if (N % 2 == 0) { // 짝수번째라면 모두 폭탄으로 차있는 상태
-            for (int i = 0; i < R; i++) {
-                for (int j = 0; j < C; j++) {
-                    sb.append("O");
-                }
-                sb.append("\n");
-            }
-            System.out.println(sb);
+
+        if (N % 2 == 0) { // 2의 배수 초라면 폭탄으로 가득찬 상태
+            print(full);
             return;
         }
 
-        // 횟수가 1이면 처음 상태와 동일
-        if (N != 1) bfs();
-        sb = new StringBuilder();
+        int time = 1;
+        while (time != N) {
+            time += 2;
+            bfs();
+        }
+        print(map);
+    }
+
+    public static void print(char[][] arr) { // 출력을 위한 메소드
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < R; i++) {
             for (int j = 0; j < C; j++) {
-                sb.append(board[i][j]);
+                sb.append(arr[i][j]);
             }
             sb.append("\n");
         }
-        System.out.println(sb);
+        System.out.println(sb.toString());
     }
 
-    public static void bfs() {
-        int time = 1;
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
-        while (time != N) {
-            time += 2;
-            for (int i = 0; i < R; i++) {
-                Arrays.fill(board[i], 'O');
-            }
-
-            // 큐에 담긴 폭탄 터뜨리기
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                Point point = queue.poll();
-                board[point.x][point.y] = '.';
-
-                for (int j = 0; j < 4; j++) {
-                    int tx = point.x + dx[j];
-                    int ty = point.y + dy[j];
-                    if (tx < 0 || ty < 0 || tx >= R || ty >= C)
-                        continue;
-
-                    board[tx][ty] = '.';
-                }
-            }
-            // 남은 폭탄 큐에 저장하기
-            for (int i = 0; i < R; i++) {
-                for (int j = 0; j < C; j++) {
-                    if (board[i][j] == 'O')
-                        queue.offer(new Point(i, j));
-                }
+    static char[][] copyArr(char[][] arr) {
+        char[][] tmp = new char[R][C];
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                tmp[i][j] = arr[i][j];
             }
         }
+        return tmp;
     }
-}
-class Point {
-    int x;
-    int y;
 
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+    static void bfs() {
+        // 폭탄이 설치된 위치 기록하기
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (map[i][j] == 'O') queue.offer(new int[]{i, j});
+            }
+        }
+
+        // 폭발시키기
+        char[][] tmp = copyArr(full); // 폭탄으로 가득찬 상태에서 폭발이 발생함
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            tmp[now[0]][now[1]] = '.';
+
+            for (int i = 0; i < 4; i++) {
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
+                if (nx < 0 || ny < 0 || nx >= R || ny >= C || tmp[nx][ny] == '.') {
+                    continue;
+                }
+
+                tmp[nx][ny] = '.';
+            }
+        }
+        map = copyArr(tmp);
     }
 }
