bf7ecf53a15411929c6d3dc3a7c028728f81494c
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-05-01 11:08:10 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 128 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,53 +1,50 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static boolean[] switches;
-
-    public static void main(String[] args) throws Exception {
+    static int[] switches;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        String[] str = br.readLine().split(" ");
-        switches = new boolean[N + 1]; // 1: ON, 0: OFF
-        for (int i = 0; i < N; i++) {
-            switches[i + 1] = Integer.parseInt(str[i]) == 1;
+        int n = Integer.parseInt(br.readLine()); // 스위치 개수
+        switches = new int[n + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 1; i <= n; i++) {
+            switches[i] = Integer.parseInt(st.nextToken());
         }
-        M = Integer.parseInt(br.readLine()); // 학생 수
-        for (int i = 0; i < M; i++) {
-            str = br.readLine().split(" ");
-            int student = Integer.parseInt(str[0]); // 1: 남, 2: 여
-            int idx = Integer.parseInt(str[1]); // 받은 수
+
+        int m = Integer.parseInt(br.readLine()); // 학생의 수
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int student = Integer.parseInt(st.nextToken());
+            int number = Integer.parseInt(st.nextToken());
             if (student == 1) { // 남학생
-                // 받은 수의 배수에 해당하는 스위치 상태 변경
-                boyChange(idx);
-            } else if (student == 2){ // 여학생
-                // 받은 수를 기준으로 좌우 대칭 & 가장 범위 넓은 구간에 속하는 스위치 상태 변경
-                girlChange(idx);
+                switchStatus(number, n + 1, number);
+            } else { // 여학생
+                int left = number - 1, right = number + 1;
+                while (left >= 1 && right <= n) {
+                    if (switches[left] != switches[right]) {
+                        break;
+                    }
+                    left--; right++;
+                }
+                switchStatus(left + 1, right, 1);
             }
         }
+
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= N; i++) {
-            sb.append(switches[i] == true ? 1 : 0).append(" ");
+        for (int i = 1; i <= n; i++) {
+            sb.append(switches[i]).append(" ");
             if (i % 20 == 0) sb.append("\n");
         }
         System.out.println(sb);
     }
 
-    public static void boyChange(int num) {
-        for (int i = 1; i <= N; i++) {
-            if (i % num == 0) switches[i] = !switches[i];
-        }
-    }
-
-    public static void girlChange(int num) {
-        int l = num - 1, r = num + 1;
-        while (l > 0 && r <= N) { // 상태 변경할 범위 찾기
-            if (switches[l] != switches[r]) break;
-            l--; r++;
-        }
-        for (int i = l + 1; i < r; i++) {
-            switches[i] = !switches[i];
+    static void switchStatus(int l, int r, int add) {
+        for (int i = l; i < r; i += add) {
+            switches[i] = (switches[i] == 1) ? 0 : 1;
         }
     }
 }
