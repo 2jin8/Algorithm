a7cd6b0bcc9ab6efed3a5024b973ad515b1dd357
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-05-11 00:52:39 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 396 ms, Memory: 38864 KB -BaekjoonHub

--- 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.java
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.java
@@ -1,16 +1,17 @@
 import java.io.*;
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class Main {
     static int N, M;
     static final int K = 20;
-    static int[][] trains;
+    static int[] trains;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        trains = new int[N + 1][K + 1];
+        trains = new int[N + 1];
         for (int t = 0; t < M; t++) {
             st = new StringTokenizer(br.readLine());
             int cmd = Integer.parseInt(st.nextToken());
@@ -18,35 +19,26 @@ public class Main {
             int x;
             switch (cmd) {
                 case 1: // 기차에 태우기
-                    x = Integer.parseInt(st.nextToken());
-                    trains[i][x] = 1;
+                    x = Integer.parseInt(st.nextToken()) - 1; // 0번 ~ 19번
+                    trains[i] |= (1 << x); // x번째 자리의 값을 1로 변경
                     break;
                 case 2: // 기차에서 내리기
-                    x = Integer.parseInt(st.nextToken());
-                    trains[i][x] = 0;
+                    x = Integer.parseInt(st.nextToken()) - 1;
+                    trains[i] &= ~(1 << x); // x번째 자리의 값을 0으로 변경
                     break;
-                case 3: // 한 칸씩 뒤로 이동
-                    for (int j = K; j >= 2; j--) {
-                        trains[i][j] = trains[i][j - 1];
-                    }
-                    trains[i][1] = 0;
+                case 3: // 한 칸씩 뒤(←)로 이동
+                    trains[i] <<= 1; // 각 자리의 사람 모두 한 칸씩 뒤로
+                    trains[i] &= ~(1 << 20); // 20번 자리의 사람이 있다면 하차(0으로 변경)
                     break;
-                case 4: // 한 칸씩 앞으로 이동:
-                    for (int j = 1; j < K; j++) {
-                        trains[i][j] = trains[i][j + 1];
-                    }
-                    trains[i][K] = 0;
+                case 4: // 한 칸씩 앞(→)으로 이동
+//                    trains[i] &= ~1; // 0번 자리의 사람 하차(0으로 변경)
+                    trains[i] >>= 1; // 각 자리의 사람 모두 한 칸씩 앞으로
                     break;
             }
         }
-
-        HashSet<String> hashSet = new HashSet<>();
-        for (int i = 1; i <= N; i++) { // 문자열과 해시로 기차의 상태 확인하기
-            StringBuilder sb = new StringBuilder();
-            for (int j = 1; j <= K; j++) {
-                sb.append(trains[i][j]);
-            }
-            hashSet.add(sb.toString());
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int i = 1; i <= N; i++) {
+            hashSet.add(trains[i]);
         }
         System.out.println(hashSet.size());
     }
