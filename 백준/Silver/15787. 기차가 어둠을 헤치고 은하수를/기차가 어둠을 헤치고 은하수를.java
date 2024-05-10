8c8efbc2e0d6661b42ca2c5f76f70cd40a53f952
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-05-11 00:26:14 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 676 ms, Memory: 79972 KB -BaekjoonHub

--- 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.java
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.java
@@ -4,13 +4,13 @@ import java.util.*;
 public class Main {
     static int N, M;
     static final int K = 20;
-    static boolean[][] trains;
+    static int[][] trains;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        trains = new boolean[N + 1][K + 1];
+        trains = new int[N + 1][K + 1];
         for (int t = 0; t < M; t++) {
             st = new StringTokenizer(br.readLine());
             int cmd = Integer.parseInt(st.nextToken());
@@ -19,23 +19,23 @@ public class Main {
             switch (cmd) {
                 case 1: // 기차에 태우기
                     x = Integer.parseInt(st.nextToken());
-                    trains[i][x] = true;
+                    trains[i][x] = 1;
                     break;
                 case 2: // 기차에서 내리기
                     x = Integer.parseInt(st.nextToken());
-                    trains[i][x] = false;
+                    trains[i][x] = 0;
                     break;
                 case 3: // 한 칸씩 뒤로 이동
                     for (int j = K; j >= 2; j--) {
                         trains[i][j] = trains[i][j - 1];
                     }
-                    trains[i][1] = false;
+                    trains[i][1] = 0;
                     break;
                 case 4: // 한 칸씩 앞으로 이동:
                     for (int j = 1; j < K; j++) {
                         trains[i][j] = trains[i][j + 1];
                     }
-                    trains[i][K] = false;
+                    trains[i][K] = 0;
                     break;
             }
         }
@@ -44,8 +44,7 @@ public class Main {
         for (int i = 1; i <= N; i++) { // 문자열과 해시로 기차의 상태 확인하기
             StringBuilder sb = new StringBuilder();
             for (int j = 1; j <= K; j++) {
-                if (trains[i][j]) sb.append("1");
-                else sb.append("0");
+                sb.append(trains[i][j]);
             }
             hashSet.add(sb.toString());
         }
