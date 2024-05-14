c0455d903be37cfc45b4f8317c43b7d80fc2384e
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-05-14 14:10:36 +0900

    [D2] Title: 간단한 369게임, Time: 114 ms, Memory: 22,756 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
@@ -1,29 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		
+class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		for (int i=1; i<=n; i++) {
-			 String num = String.valueOf(i);
-			 if (!num.contains("3") && !num.contains("6") && !num.contains("9")) {
-				 sb.append(i + " ");
-			 }
-			 else {
-				 int cnt = 0;
-				 for (int j=0; j<num.length(); j++) {
-					 char c = num.charAt(j);
-					 if (c == '3' || c == '6' || c == '9') cnt++;
-				 }
-				 for (int j=0; j<cnt; j++) {
-					 sb.append("-");
-				 }
-				 sb.append(" ");
-			 }
+		int n = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= n; i++) {
+			String num = String.valueOf(i);
+			if (num.contains("3") || num.contains("6") || num.contains("9")) {
+				for (int j = 0; j < num.length(); j++) {
+					if (num.charAt(j) == '3' || num.charAt(j) == '6' || num.charAt(j) == '9') {
+						sb.append("-");
+					}
+				}
+			} else {
+				sb.append(num);
+			}
+			sb.append(" ");
 		}
 		System.out.println(sb.toString());
 	}
