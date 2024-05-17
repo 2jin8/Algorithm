ac6ed4819be05de73ddadfc64172c0545d432c40
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-05-18 01:26:36 +0900

    [D2] Title: 초심자의 회문 검사, Time: 103 ms, Memory: 23,864 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
@@ -1,24 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		int T = Integer.parseInt(sc.nextLine());
-
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			String[] strings = sc.nextLine().split("");
-			int i = 0, j = strings.length-1;
-			int ans = 1;
-			while (i < j) {
-				if (!strings[i++].equals(strings[j--])) {
-					ans = 0;
-					break;
-				}
-			}
-			System.out.println("#"+test_case+" "+ans);
+class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			String str = br.readLine();
+			String reverse = new StringBuilder(str).reverse().toString();
+			boolean isEqual = str.equals(reverse);
+			sb.append("#").append(t).append(" ").append(isEqual ? 1 : 0).append("\n");
 		}
+		System.out.println(sb.toString());
 	}
 }
