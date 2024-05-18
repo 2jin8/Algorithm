c11a38fdc57e516554f540014e866af83667cb2d
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-05-19 02:53:54 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 159 ms, Memory: 38,836 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -1,41 +1,68 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		for(int tc = 1; tc <= 10; tc++)
-		{
-			int n = sc.nextInt();
-			int[][] ary = new int[100][100];
-			int rowSum = 0;
-			for (int i=0; i<100; i++) {
-				int sum = 0;
-				for (int j=0; j<100; j++) {
-					ary[i][j] = sc.nextInt(); 
-					sum += ary[i][j];
+class Solution {
+	static final int N = 100;
+	static int[][] arr;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		for (int t = 1; t <= 10; t++) {
+			int testCase = Integer.parseInt(br.readLine());
+			arr = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				StringTokenizer stringTokenizer = new StringTokenizer(br.readLine(), " ");
+				for (int j = 0; j < N; j++) {
+					arr[i][j] = Integer.parseInt(stringTokenizer.nextToken());
 				}
-				rowSum = Math.max(rowSum, sum);
 			}
-			
-			int colSum = 0;
-			for (int i=0; i<100; i++) {
-				int sum = 0;
-				for (int j=0; j<100; j++) {
-					sum += ary[j][i];
-				}
-				colSum = Math.max(colSum, sum);
-			}
-			
-			int leftCrossSum = 0, rightCrossSum = 0;
-			for (int i=0; i<100; i++) {
-				leftCrossSum += ary[i][i];
-				rightCrossSum += ary[i][99-i]; 
-			}
-			
-			int max = Math.max(Math.max(rowSum, colSum), Math.max(leftCrossSum, rightCrossSum));
-			System.out.println("#"+n+" "+max);
+			int max = Math.max(Math.max(sumRow(), sumCol()), Math.max(sumCrossLeft(), sumCrossRight()));
+			sb.append('#').append(testCase).append(' ').append(max).append('\n');
 		}
+		System.out.println(sb.toString());
+	}
+
+	static int sumRow() { // 가로
+		int max = 0;
+		for (int i = 0; i < N; i++) {
+			int total = 0;
+			for (int j = 0; j < N; j++) {
+				total += arr[i][j];
+			}
+			max = Math.max(max, total);
+		}
+
+		return max;
+	}
+
+	static int sumCol() { // 세로
+		int max = 0;
+		for (int j = 0; j < N; j++) {
+			int total = 0;
+			for (int i = 0; i < N; i++) {
+				total += arr[i][j];
+			}
+			max = Math.max(max, total);
+		}
+		return max;
+	}
+
+	static int sumCrossLeft() { // 좌대각선
+		int max = 0;
+		for (int i = 0; i < N; i++) {
+			max += arr[i][i];
+		}
+		return max;
+	}
+
+	static int sumCrossRight() { // 우대각선
+		int max = 0;
+		for (int i = 0; i < N; i++) {
+			max += arr[i][N - i - 1];
+		}
+		return max;
 	}
 }
