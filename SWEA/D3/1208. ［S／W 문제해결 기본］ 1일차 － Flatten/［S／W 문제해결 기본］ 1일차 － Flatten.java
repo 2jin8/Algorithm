8966bc43e0634d0c11899e511c4c8ad254a19a38
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-05-18 23:30:53 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 111 ms, Memory: 25,604 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -1,43 +1,47 @@
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+class Solution {
+	static final int N = 100;
+	static int[] heights;
+	static int min, minIdx, max, maxIdx;
 
-
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-
-		for(int test_case = 1; test_case <= 10; test_case++)
-		{
-			int k = sc.nextInt(); // 덤프 횟수
-			PriorityQueue<Integer> lowQueue = new PriorityQueue<>();
-			PriorityQueue<Integer> highQueue = new PriorityQueue<>(Collections.reverseOrder());
-			int[] heights = new int[100];
-			for (int i=0; i<100; i++) {
-				heights[i] = sc.nextInt(); 
-				lowQueue.offer(heights[i]);
-				highQueue.offer(heights[i]);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		for (int t = 1; t <= 10; t++) {
+			int dump = Integer.parseInt(br.readLine()); // 덤프 횟수
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			heights = new int[N]; // 상자의 높이
+			for (int i = 0; i < N; i++) {
+				heights[i] = Integer.parseInt(st.nextToken());
 			}
-			
-			// 덤프 횟수 만큼 반복
-			int diff = 0;
-			for (int i=0; i<k; i++) {
-				int high = highQueue.poll();
-				int low = lowQueue.poll();
-				diff = high - low;
-				if (diff == 0 || diff == 1) { // 평탄화 작업이 완료된 경우
-					break;
-				}
-				
-				// 평탄화 작업 수행
-				highQueue.offer(high - 1);
-				lowQueue.offer(low + 1);
+
+			// 덤프하기
+			for (int d = 0; d < dump; d++) {
+				findMinMax();
+				heights[minIdx]++;
+				heights[maxIdx]--;
 			}
-			diff = highQueue.poll() - lowQueue.poll();
-			System.out.println("#"+test_case+" "+diff);
+			findMinMax();
+			sb.append('#').append(t).append(' ').append(max - min).append('\n');
+		}
+		System.out.println(sb.toString());
+	}
+
+	static void findMinMax() { // 최고점, 최저점 찾기
+		min = heights[0]; minIdx = 0; 
+		max = heights[0]; maxIdx = 0;
+		for (int i = 1; i < N; i++) {
+			if (heights[i] < min) {
+				min = heights[i];
+				minIdx = i;
+			} else if (heights[i] > max) {
+				max = heights[i];
+				maxIdx = i;
+			}
 		}
 	}
 }
