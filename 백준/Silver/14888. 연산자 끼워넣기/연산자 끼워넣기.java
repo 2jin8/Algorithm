a6e60dfb8df697866eb77708ca806d221eb446a3
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-05-19 01:26:26 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 124 ms, Memory: 14624 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,62 +1,58 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-class Main
-{
-	static int min, max;
-	static int n;
-	static int[] nums, operator;
-	public static void main(String args[]) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		nums = new int[n];
-		for (int i=0; i<n; i++) {
-			nums[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		st = new StringTokenizer(br.readLine());
-		operator = new int[4];
-		for (int i=0; i<4; i++) {
-			operator[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		min = Integer.MAX_VALUE;
-		max = Integer.MIN_VALUE;
-		dfs(nums[0], 1);
-		System.out.println(max);
-		System.out.println(min);
-	}
-	
-	public static void dfs(int total, int idx) {
-		if (idx == n) {
-			min = Math.min(min, total);
-			max = Math.max(max, total);
-			return;
-		}
-		
-		for (int i=0; i<4; i++) {
-			if (operator[i] > 0) {
-				operator[i]--;
-				
-				switch(i) {
-					case 0: 
-						dfs(total + nums[idx], idx+1);
-						break;
-					case 1: 
-						dfs(total - nums[idx], idx+1);
-						break;
-					case 2: 
-						dfs(total * nums[idx], idx+1);
-						break;
-					case 3:
-						dfs(total / nums[idx], idx+1);
-						break;
-				}
-				operator[i]++;
-			}
-		}
-	}
+public class Main {
+    static int n, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+    static int[] operand, operator;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        operand = new int[n]; // 피연산자
+        operator = new int[4]; // 연산자
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            operand[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < 4; i++) {
+            operator[i] = Integer.parseInt(st.nextToken());
+        }
+        dfs(1, operand[0]);
+        System.out.println(max);
+        System.out.println(min);
+    }
+
+    static void dfs(int depth, int total) {
+        if (depth == n) {
+            max = Math.max(max, total);
+            min = Math.min(min, total);
+            return;
+        }
+
+        // 덧셈
+        if (operator[0] > 0) {
+            operator[0]--;
+            dfs(depth + 1, total + operand[depth]);
+            operator[0]++;
+        }
+
+        if (operator[1] > 0) {
+            operator[1]--;
+            dfs(depth + 1, total - operand[depth]);
+            operator[1]++;
+        }
+
+        if (operator[2] > 0) {
+            operator[2]--;
+            dfs(depth + 1, total * operand[depth]);
+            operator[2]++;
+        }
+
+        if (operator[3] > 0) {
+            operator[3]--;
+            dfs(depth + 1, total / operand[depth]);
+            operator[3]++;
+        }
+    }
 }
