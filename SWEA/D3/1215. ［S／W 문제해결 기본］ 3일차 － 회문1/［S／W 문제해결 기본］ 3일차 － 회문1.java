fd98985e8045106a4040718d7ea905793cbc00dd
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-05-19 11:32:31 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 114 ms, Memory: 24,092 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.java
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.java
@@ -1,62 +1,56 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class Solution
-{
+class Solution {
+	static int len, palindromeCnt;
 	static final int N = 8;
-	static char[][] board = new char[N][N];
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
-		int T=10;
+	static char[][] board;
 
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			sb.append("#").append(test_case).append(" ");
-			int len = Integer.parseInt(sc.nextLine()); // 찾아야 하는 회문의 길이
-			for (int i=0; i<N; i++) {
-				String str = sc.nextLine();
-				for (int j=0; j<N; j++) {
-					char c = str.charAt(j);
-					board[i][j] = c;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		for (int t = 1; t <= 10; t++) {
+			len = Integer.parseInt(br.readLine()); // 찾아야 하는 회문의 길이
+			board = new char[N][N];
+			for (int i = 0; i < N; i++) {
+				String line = br.readLine();
+				for (int j = 0; j < N; j++) {
+					board[i][j] = line.charAt(j);
 				}
 			}
-			
-			// 행 검사
-			boolean isEqual;
-			int total = 0;
-			for (int i=0; i<N; i++) {
-				for (int j=0; j<=N-len; j++) { // j가 탐색 시작 위치
-					int start = j, end = j + len - 1;
-					isEqual = true; // 탐색 전 초기화 필수!
-					while (start <= end) {
-						if (board[i][start] != board[i][end]) {
-							isEqual = false;
-							break;
-						}
-						start++; end--;
-					}
-					if (isEqual) total++;
-				}
-			}
-			
-			// 열 검사
-			for (int i=0; i<N; i++) {
-				for (int j=0; j<=N-len; j++) {
-					int start = j, end = j + len - 1;
-					isEqual = true;
-					while (start <= end) {
-						if (board[start][i] != board[end][i]) {
-							isEqual = false;
-							break;
-						}
-						start++; end--;
-					}
-					if (isEqual) total++;
-				}
-			}
-			sb.append(total).append("\n");
+
+			palindromeCnt = 0; // 회문의 개수
+			findPalindrome();
+			sb.append("#").append(t).append(" ").append(palindromeCnt).append("\n");
 		}
 		System.out.println(sb.toString());
+	}
+
+	static void findPalindrome() {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j <= N - len; j++) {
+				// Row 확인
+				StringBuilder builder = new StringBuilder();
+				for (int k = j; k < j + len; k++) {
+					builder.append(board[i][k]);
+				}
+				String origin = builder.toString();
+				String reverse = builder.reverse().toString();
+				if (origin.equals(reverse))
+					palindromeCnt++;
+
+				// Col 확인
+				builder.setLength(0);
+				for (int k = j; k < j + len; k++) {
+					builder.append(board[k][i]);
+				}
+				origin = builder.toString();
+				reverse = builder.reverse().toString();
+				if (origin.equals(reverse))
+					palindromeCnt++;
+
+			}
+		}
 	}
 }
