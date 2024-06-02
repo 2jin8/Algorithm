cef1631d946abfd4f80a1ccdbecdc70289d259fd
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-06-02 22:32:38 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 764 ms, Memory: 151068 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,62 +1,62 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int[][] board;
+    static int n, m;
+    static int[][] map;
     static boolean[][][] visited;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] str = br.readLine().split(" ");
-        N = Integer.parseInt(str[0]);
-        M = Integer.parseInt(str[1]);
-        board = new int[N][M];
-        visited = new boolean[N][M][2];
-        for (int i = 0; i < N; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < M; j++) {
-                board[i][j] = s.charAt(j) - '0';
+        // 0: 이동 O, 1: 이동 X
+        // (1, 1)에서 (N, M)까지 최단 경로로 이동
+        // 벽을 한 개까지 부술 수 있음 ⇒ 상태가 도중에 변경될 수 있음
+        // 변할 수 있는 상태: 벽을 부순 경우 vs 벽을 부수지 않은 경우 → 총 2가지,,
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        visited = new boolean[n][m][2]; // [][][0]: 벽을 부수지 않은 경우, [][][1]: 벽을 부순 경우
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = line.charAt(j) - '0';
             }
         }
+
         System.out.println(bfs());
     }
 
-    public static int bfs() {
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(0, 0, false, 0));
+    static int bfs() {
+        Queue<Pos> queue = new LinkedList<>();
+        queue.offer(new Pos(0, 0, 1, false));
         visited[0][0][0] = true;
 
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
+        int[][] move = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         while (!queue.isEmpty()) {
-            Point point = queue.poll();
-            if (point.x == N - 1 && point.y == M - 1) {
-                return point.move + 1;
+            Pos pos = queue.poll();
+            if (pos.x == n - 1 && pos.y == m - 1) {
+                return pos.time;
             }
 
             for (int i = 0; i < 4; i++) {
-                int tx = point.x + dx[i];
-                int ty = point.y + dy[i];
-                if (tx < 0 || ty < 0 || tx >= N || ty >= M) continue;
+                int nx = pos.x + move[i][0];
+                int ny = pos.y + move[i][1];
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
+                    continue;
+                }
 
-                if (board[tx][ty] == 1) { // 벽인 경우
-                    if (!point.crashed) { // 벽을 부수지 않은 경우
-                        queue.offer(new Point(tx, ty, true, point.move + 1));
-                        visited[tx][ty][1] = true;
+                if (pos.crashed) { // 이미 벽을 부순 경우
+                    if (map[nx][ny] == 0 && !visited[nx][ny][1]) { // 벽이 아닌 경우만 이동 가능
+                        queue.offer(new Pos(nx, ny, pos.time + 1, true));
+                        visited[nx][ny][1] = true;
                     }
-                } else {
-                    if (point.crashed) { // 이미 벽을 부순 경우
-                        if (!visited[tx][ty][1]) {
-                            queue.offer(new Point(tx, ty, true, point.move + 1));
-                            visited[tx][ty][1] = true;
-                        }
-                    } else { // 벽을 부수지 않은 경우
-                        if (!visited[tx][ty][0]) {
-                            queue.offer(new Point(tx, ty, false, point.move + 1));
-                            visited[tx][ty][0] = true;
-                        }
+                } else { // 벽을 부수지 않은 경우
+                    if (map[nx][ny] == 0 && !visited[nx][ny][0]) { // 이동할 칸이 벽이 아니면 그냥 이동
+                        queue.offer(new Pos(nx, ny, pos.time + 1, false));
+                        visited[nx][ny][0] = true;
+                    } else if (map[nx][ny] == 1 && !visited[nx][ny][1]) { // 이동한 칸이 벽이면 부숴보기
+                        queue.offer(new Pos(nx, ny, pos.time + 1, true));
+                        visited[nx][ny][1] = true;
                     }
                 }
             }
@@ -64,17 +64,16 @@ public class Main {
         return -1;
     }
 
-    static class Point {
-        int x;
-        int y;
+    static class Pos {
+        int x, y;
+        int time; // 소요 시간
         boolean crashed; // 벽 부쉈는지 여부
-        int move; // 이동 횟수
 
-        public Point(int x, int y, boolean crashed, int move) {
+        public Pos(int x, int y, int time, boolean crashed) {
             this.x = x;
             this.y = y;
+            this.time = time;
             this.crashed = crashed;
-            this.move = move;
         }
     }
 }
