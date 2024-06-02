7d8d9771b263c442c8186409a3939d82c24a6a28
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-06-02 22:36:47 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -2,27 +2,21 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
         int[] coins = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = n - 1; i >= 0; i--) {
             coins[i] = Integer.parseInt(br.readLine());
         }
 
-        int idx = n - 1, cnt = 0;
+        int coinCnt = 0, i = 0;
         while (k > 0) {
-            if (coins[idx] > k) {
-                idx--;
-                continue;
-            }
-
-            int div = k / coins[idx];
-            k -= div * coins[idx];
-            cnt += div;
+            coinCnt += k / coins[i];
+            k %= coins[i++];
         }
-        System.out.println(cnt);
+        System.out.println(coinCnt);
     }
 }
