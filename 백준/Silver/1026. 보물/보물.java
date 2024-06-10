7f73f376825ad733cb28671e1166dc73e434d67f
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-06-10 14:23:06 +0900

    [Silver IV] Title: 보물, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -1,15 +1,13 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * 곱해서 가장 작게 만든다 ⇒ 큰 값을 작은 값이랑 곱한다
- */
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] A = new int[n];
         Integer[] B = new Integer[n];
+
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < n; i++) {
             A[i] = Integer.parseInt(st.nextToken());
@@ -20,7 +18,7 @@ public class Main {
         for (int i = 0; i < n; i++) {
             B[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(B, Collections.reverseOrder());
+        Arrays.sort(B, Collections.reverseOrder()); // 내림차순 정렬
 
         int total = 0;
         for (int i = 0; i < n; i++) {
