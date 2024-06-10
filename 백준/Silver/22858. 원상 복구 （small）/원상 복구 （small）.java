119db656253c2e6fadbe45af93ab8a72e79304c5
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-06-10 13:57:23 +0900

    [Silver III] Title: 원상 복구 (small), Time: 344 ms, Memory: 58412 KB -BaekjoonHub

--- 백준/Silver/22858. 원상 복구 （small）/원상 복구 （small）.java
+++ 백준/Silver/22858. 원상 복구 （small）/원상 복구 （small）.java
@@ -2,33 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, K;
-    static int[] D, S, P;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        D = new int[N + 1];
-        S = new int[N + 1];
-        P = new int[N + 1];
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] D = new int[n + 1];
+        int[] S = new int[n + 1];
+        int[] P = new int[n + 1];
+
         st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= n; i++) {
             S[i] = Integer.parseInt(st.nextToken());
         }
+
         st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= n; i++) {
             D[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < K; i++) {
-            for (int j = 1; j <= N; j++) {
+        for (int i = 0; i < k; i++) {
+            for (int j = 1; j <= n; j++) {
                 P[D[j]] = S[j];
             }
             S = P.clone();
         }
+
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= n; i++) {
             sb.append(P[i]).append(" ");
         }
         System.out.println(sb.toString());
