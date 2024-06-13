dcd373a2561514432b8f32d21752a8fd86fb201a
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-06-13 10:02:56 +0900

    [Silver II] Title: 주식, Time: 1068 ms, Memory: 312924 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -1,28 +1,29 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
+
         StringBuilder sb = new StringBuilder();
         for (int t = 0; t < T; t++) {
             int N = Integer.parseInt(br.readLine());
+
             int[] arr = new int[N];
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             for (int i = 0; i < N; i++) {
                 arr[i] = Integer.parseInt(st.nextToken());
             }
 
+            // 뒤에서부터 탐색
             int max = arr[N - 1];
-            long total = 0L;
+            long total = 0;
             for (int i = N - 2; i >= 0; i--) {
-                if (arr[i] < max) { // 주가가 가장 높을 떄보다 낮은 가격인 경우라면
-                    // 주식 사기
-                    total += max - arr[i];
-                } else { // 주가가 가장 높을 때보다 더 높은 가격인 경우라면
-                    // 최고 주가 갱신하기
+                if (max < arr[i]) { // 자신보다 큰 값 존재 ⇒ 최대값 갱신
                     max = arr[i];
+                } else { // 자신보다 작은 값 존재 ⇒ 주식 구매
+                    total += max - arr[i];
                 }
             }
             sb.append(total).append("\n");
