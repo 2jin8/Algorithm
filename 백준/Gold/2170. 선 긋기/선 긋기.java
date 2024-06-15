d1efcfd141e2cc3e3939917b64ec9946aef4576d
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-06-15 10:47:07 +0900

    [Gold V] Title: 선 긋기, Time: 2472 ms, Memory: 275300 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.java
+++ 백준/Gold/2170. 선 긋기/선 긋기.java
@@ -1,40 +1,45 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        // x를 기준으로 오름차순 정렬, x가 같다면 y를 기준으로 오름차순 정렬
-        PriorityQueue<Line> pq = new PriorityQueue<>(new Comparator<Line>() {
-            @Override
-            public int compare(Line o1, Line o2) {
-                if (o1.x == o2.x) return o1.y - o2.y;
-                return o1.x - o2.x;
-            }
-        });
-
+        Line[] lines = new Line[n];
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            pq.offer(new Line(x, y));
+            lines[i] = new Line(x, y);
         }
+        Arrays.sort(lines, (l1, l2) -> {
+            /// x를 기준으로 오름차순 정렬 (앞에서부터 선을 그릴 것이므로)
+            if (l1.x == l2.x) return l1.y - l2.y;
+            return l1.x - l2.x;
+        });
 
-        Line line = pq.poll();
-        int start = line.x, end = line.y, total = 0;
-        while (!pq.isEmpty()) {
-            line = pq.poll();
-            if (line.x <= end) { // 그으려는 선이 이미 그어진 선과 겹치는 경우라면
-                end = Math.max(line.y, end); // 두 y값 중 더 큰 값으로 갱신
-            } else { // 그렇지 않다면
-                total += end - start; // 이때까지 그은 선의 길이 세기
-                start = line.x; // 새로운 start, end 값으로 갱신하기
-                end = line.y;
+        ArrayList<Line> lineList = new ArrayList<>();
+        lineList.add(lines[0]);
+        for (int i = 1; i < n; i++) {
+            int nx = lines[i].x;
+            int ny = lines[i].y;
+            boolean isContinue = false;
+            for (Line line : lineList) {
+                int x = line.x, y = line.y;
+                if (x <= nx && nx <= y) { // 선을 연속해서 그을 수 있는 경우
+                    line.y = Math.max(line.y, ny); // 연속해서 긋기(종료 지점이 더 큰 것으로 갱신)
+                    isContinue = true;
+                    break;
+                }
             }
+            // 선을 연속해서 긋지 못하면 새로 긋기
+            if (!isContinue) lineList.add(lines[i]);
         }
-        total += end - start;
+        long total = 0;
+        for (Line line : lineList) {
+            total += line.y - line.x;
+        }
         System.out.println(total);
     }
 
