db37a922da8b7290d66ab48ed8935d0b2c943e20
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-06-17 14:30:37 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 14760 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,55 +1,54 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     static int n, m;
-    static int[][] board;
-    static boolean[][] visited;
+    static int[][] board, dist;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        // 지나야 하는 최소의 칸 수 - BFS
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         board = new int[n][m];
-        visited = new boolean[n][m];
+        dist = new int[n][m];
         for (int i = 0; i < n; i++) {
-            String str = br.readLine();
+            String line = br.readLine();
             for (int j = 0; j < m; j++) {
-                board[i][j] = str.charAt(j) - '0';
+                board[i][j] = line.charAt(j) - '0';
             }
         }
-
         System.out.println(bfs(0, 0));
     }
 
-    public static int bfs(int x, int y) {
+    static int bfs(int x, int y) {
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{x, y});
-        visited[x][y] = true;
+        dist[x][y] = 1; // 칸을 셀 때 시작 위치도 포함
 
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
+        int[][] move = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         while (!queue.isEmpty()) {
-            int[] poll = queue.poll();
-            x = poll[0];
-            y = poll[1];
-            if (x == n - 1 && y == m - 1)
-                break;
+            int[] now = queue.poll();
+            x = now[0];
+            y = now[1];
+            if (x == n - 1 && y == m - 1) {
+                // 칸을 셀 때 도착 위치도 포함 ⇒ 기록된 값 그래도 출력하면 됨
+                break; // 항상 도착위치로 이동할 수 있는 경우만 주어짐
+            }
 
             for (int i = 0; i < 4; i++) {
-                int tx = x + dx[i];
-                int ty = y + dy[i];
-                if (tx < 0 || ty < 0 || tx >= n || ty >= m)
+                int nx = x + move[i][0];
+                int ny = y + move[i][1];
+                // 범위 벗어나거나 이미 방문한 위치면 넘어가기
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m || dist[nx][ny] != 0) {
                     continue;
+                }
 
-                if (board[tx][ty] != 0 && !visited[tx][ty]) {
-                    board[tx][ty] = board[x][y] + 1; // 지나온 칸의 개수
-                    queue.offer(new int[]{tx, ty});
-                    visited[tx][ty] = true;
+                if (board[nx][ny] == 1) {
+                    queue.offer(new int[]{nx, ny});
+                    dist[nx][ny] = dist[x][y] + 1;
                 }
             }
         }
-        return board[n - 1][m - 1];
+        return dist[n - 1][m - 1];
     }
 }
