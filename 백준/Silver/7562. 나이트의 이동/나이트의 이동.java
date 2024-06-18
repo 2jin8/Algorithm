f452fe079bfb8512974f5395542fa6270385f57c
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-06-18 13:08:44 +0900

    [Silver I] Title: 나이트의 이동, Time: 312 ms, Memory: 74124 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -2,57 +2,53 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int l;
-    static int[][] map;
-    public static void main(String[] args) throws IOException {
+    static int l, startX, startY, endX, endY;
+    static int[][] board;
+    static boolean[][] visited;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
+
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < T; i++) {
+        for (int t = 0; t < T; t++) {
             l = Integer.parseInt(br.readLine());
+            board = new int[l][l];
+            visited = new boolean[l][l];
+
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            Pos start = new Pos(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            startX = Integer.parseInt(st.nextToken());
+            startY = Integer.parseInt(st.nextToken());
             st = new StringTokenizer(br.readLine(), " ");
-            Pos end = new Pos(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-            map = new int[l][l];
-            sb.append(bfs(start, end)).append("\n");
+            endX = Integer.parseInt(st.nextToken());
+            endY = Integer.parseInt(st.nextToken());
+            bfs();
+            sb.append(board[endX][endY]).append("\n");
         }
         System.out.println(sb.toString());
     }
 
-    public static int bfs(Pos start, Pos end) {
-        Queue<Pos> queue = new LinkedList<>();
-        queue.offer(start);
-        map[start.x][start.y] = 1;
+    static void bfs() {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{startX, startY});
+        visited[startX][startY] = true;
 
-        int[] dx = {-2, -2, -1, -1, 1, 1, 2, 2}; // 8방향 탐색
-        int[] dy = {-1, 1, -2, 2, -2, 2, -1, 1};
+        int[][] move = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
         while (!queue.isEmpty()) {
-            Pos pos = queue.poll();
-            if (pos.x == end.x && pos.y == end.y) {
-                break;
-            }
+            int[] now = queue.poll();
+            int x = now[0], y = now[1];
+            if (x == endX && y == endY) break;
 
             for (int i = 0; i < 8; i++) {
-                int nx = pos.x + dx[i];
-                int ny = pos.y + dy[i];
-                if (nx < 0 || ny < 0 || nx >= l || ny >= l) continue;
-
-                if (map[nx][ny] == 0) {
-                    queue.offer(new Pos(nx, ny));
-                    map[nx][ny] = map[pos.x][pos.y] + 1;
+                int nx = x + move[i][0];
+                int ny = y + move[i][1];
+                if (nx < 0 || ny < 0 || nx >= l || ny >= l || visited[nx][ny]) {
+                    continue;
                 }
+
+                queue.offer(new int[]{nx, ny});
+                visited[nx][ny] = true;
+                board[nx][ny] = board[x][y] + 1;
             }
-        }
-        return map[end.x][end.y] - 1;
-    }
-
-    static class Pos {
-        int x, y;
-
-        public Pos(int x, int y) {
-            this.x = x;
-            this.y = y;
         }
     }
 }
