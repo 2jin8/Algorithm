f7694be124b19d64f8de98d27c45a7d04cb3bcc4
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-06-19 11:20:53 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,64 +1,65 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     static int N;
     static int[][] map;
     static boolean[][] visited;
-    static ArrayList<Integer> answer = new ArrayList<>();
-    public static void main(String[] args) throws IOException {
-        // 지나야하는 최소 칸 수 == 최단 거리 => BFS
+    static ArrayList<Integer> apartments = new ArrayList<>();
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+
         map = new int[N][N];
         visited = new boolean[N][N];
         for (int i = 0; i < N; i++) {
-            String str = br.readLine();
+            String line = br.readLine();
             for (int j = 0; j < N; j++) {
-                map[i][j] = str.charAt(j) - '0';
+                map[i][j] = line.charAt(j) - '0';
             }
         }
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (!visited[i][j] && map[i][j] == 1) {
-                    bfs(i, j);
+                if (map[i][j] == 1 && !visited[i][j]) {
+                    apartments.add(bfs(i, j));
                 }
             }
         }
+        Collections.sort(apartments); // 단지내 집의 수 오름차순으로 정렬
+
         StringBuilder sb = new StringBuilder();
-        sb.append(answer.size()).append("\n");
-        Collections.sort(answer);
-        for (int a : answer) {
-            sb.append(a).append("\n");
+        sb.append(apartments.size()).append("\n");
+        for (int apartment : apartments) {
+            sb.append(apartment).append("\n");
         }
         System.out.println(sb.toString());
     }
 
-    public static void bfs(int x, int y) {
+    public static int bfs(int x, int y) {
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{x, y});
         visited[x][y] = true;
 
-        int area = 0;
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
+        int house = 0;
+        int[][] move = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         while (!queue.isEmpty()) {
             int[] now = queue.poll();
-            area++;
+            house++;
 
             for (int i = 0; i < 4; i++) {
-                int nx = now[0] + dx[i];
-                int ny = now[1] + dy[i];
-                // 범위 벗어나는 경우 or 이미 방문한 경우 or 이동할 수 없는 칸인 경우
-                if (nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny] || map[nx][ny] == 0) {
+                int nx = now[0] + move[i][0];
+                int ny = now[1] + move[i][1];
+                if (nx < 0 || ny < 0 || nx >= N || ny >= N) {
                     continue;
                 }
 
-                visited[nx][ny] = true;
-                queue.offer(new int[]{nx, ny});
+                if (map[nx][ny] == 1 && !visited[nx][ny]) {
+                    queue.offer(new int[]{nx, ny});
+                    visited[nx][ny] = true;
+                }
             }
         }
-        answer.add(area);
+        return house;
     }
 }
