0b07e43fac807569af98f4f4f7a02d9cf6272ee2
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-06-25 00:18:13 +0900

    [Silver III] Title: 1로 만들기, Time: 132 ms, Memory: 18108 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,26 +1,43 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static final int INF = 1_000_000;
+    static int[] dist;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] dp = new int[N + 1];
-        dp[1] = 0;
-        for (int i = 2; i <= N; i++) {
-            // 1을 빼는 연산
-            dp[i] = dp[i - 1] + 1;
+        dist = new int[N + 1];
 
-            // 2로 나누어 떨어지면, 2로 나누는 연산
-            if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+        bfs(N);
+        System.out.println(dist[1] - 1);
+    }
+
+    static void bfs(int x) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(x);
+        dist[x] = 1;
+
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+            if (now == 1) break; // 1을 만들었다면 탐색 종료
+
+            int next = now / 3;
+            if (now % 3 == 0 && dist[next] == 0) {
+                queue.offer(next);
+                dist[next] = dist[now] + 1;
             }
 
-            // 3으로 나누어 떨어지면, 3으로 나누는 연산
-            if (i % 3 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            next = now / 2;
+            if (now % 2 == 0 && dist[next] == 0) {
+                queue.offer(next);
+                dist[next] = dist[now] + 1;
+            }
+
+            next = now - 1;
+            if (dist[next] == 0) {
+                queue.offer(next);
+                dist[next] = dist[now] + 1;
             }
         }
-        System.out.println(dp[N]);
     }
 }
