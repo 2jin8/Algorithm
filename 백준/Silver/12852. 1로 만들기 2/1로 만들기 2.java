c32ab5b26453ce6e8786c6fae7b47865b1c61c69
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-06-26 17:56:16 +0900

    [Silver I] Title: 1로 만들기 2, Time: 148 ms, Memory: 22008 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -1,38 +1,42 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        int[] prev = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            if (i == 1) dp[1] = 0;
+            else if (i == 2 || i == 3) {
+                dp[i] = 1;
+                prev[i] = 1;
+            } else {
+                dp[i] = dp[i - 1] + 1;
+                prev[i] = i - 1;
 
-    public static final int INF = 1000001;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] dp = new int[INF];
-        int[] before = new int[INF];
-        dp[1] = 0; dp[2] = 1; dp[3] = 1;
-        before[1] = 1; before[2] = 1; before[3] = 1;
-        for (int i = 4; i <= n; i++) {
-            dp[i] = dp[i - 1] + 1;
-            before[i] = i - 1;
-            if (i % 3 == 0) {
-                if (dp[i] > (dp[i / 3] + 1)) {
+                if (i % 3 == 0 && dp[i / 3] + 1 < dp[i]) {
                     dp[i] = dp[i / 3] + 1;
-                    before[i] = i / 3;
+                    prev[i] = i / 3;
                 }
-            }
-            if (i % 2 == 0) {
-                if (dp[i] > (dp[i / 2] + 1)) {
+
+                if (i % 2 == 0 && dp[i / 2] + 1 < dp[i]) {
                     dp[i] = dp[i / 2] + 1;
-                    before[i] = i / 2;
+                    prev[i] = i / 2;
                 }
             }
         }
-        System.out.println(dp[n]);
-        StringBuilder sb = new StringBuilder(n + " ");
-        int beforeN = n;
-        for (int i = 0; i < dp[n]; i++) {
-            beforeN = before[beforeN];
-            sb.append(beforeN + " ");
+        StringBuilder sb = new StringBuilder();
+        sb.append(dp[n]).append("\n");
+        sb.append(n).append(" ");
+        int p = n;
+        while (true) {
+            if (prev[p] == 0) break;
+
+            sb.append(prev[p]).append(" ");
+            p = prev[p];
         }
-        System.out.println(sb);
+        System.out.println(sb.toString());
     }
 }
