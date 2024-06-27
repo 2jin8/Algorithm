05812d0750bbc479035684364f51eea1b312a012
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-06-27 18:27:55 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 148 ms, Memory: 14600 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -2,22 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int[] arr = new int[n];
+        int[] dp = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = arr[i];
         }
 
-        int[] dp = arr.clone();
+        int max = dp[0];
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                if (arr[j] < arr[i]) dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+                if (arr[i] > arr[j]) { // 증가하는 경우
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+                }
             }
+            max = Math.max(max, dp[i]);
         }
-        System.out.println(Arrays.stream(dp).max().getAsInt());
+        System.out.println(max);
     }
 }
