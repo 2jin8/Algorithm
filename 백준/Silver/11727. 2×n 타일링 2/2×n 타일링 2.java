ca54db26465c273e1b94e342a16173c494aacc44
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-06-27 17:22:40 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 132 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -1,13 +1,15 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static final int DIV = 10_007;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] dp = new int[1001];
         int n = Integer.parseInt(br.readLine());
-        dp[1] = 1; dp[2] = 3; dp[3] = 5;
-        for (int i = 4; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            if (i == 1) dp[i] = 1;
+            else if (i == 2) dp[i] = 3;
+            else dp[i] = (dp[i - 1] + dp[i - 2] * 2) % DIV; // 2×2를 만드는 방법이 총 2개이므로
         }
         System.out.println(dp[n]);
     }
