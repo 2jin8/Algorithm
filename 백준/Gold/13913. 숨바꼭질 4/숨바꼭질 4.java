274e70f9010aad5c6e2a72d36c9126cfbaffaafe
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-07-03 22:41:59 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 860 ms, Memory: 27904 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -1,68 +1,61 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    private static boolean[] visited;
-    private static int[] line, route;
-    private static final int INF = 100001;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        line = new int[INF];
-        route = new int[INF];
-        visited = new boolean[INF];
-        Arrays.fill(line, INF);
+    static int N, K;
+    static final int MAX = 100_000;
+    static int[] time, record;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        time = new int[MAX + 1];
+        record = new int[MAX + 1];
+        record[N] = -1;
 
-        int time = bfs(n, k); // 찾는 가장 빠른 시간
-        int before_idx = k; // 경로 출력을 위한 인덱스
-        Stack<Integer> stack = new Stack<>();
-        for (int i = time - 1; i >= 0; i--) {
-            int before = route[before_idx];
-            before_idx = before;
-            stack.push(before);
-        }
         StringBuilder sb = new StringBuilder();
-        sb.append(time).append("\n");
-        while (!stack.isEmpty()) {
-            sb.append(stack.pop()).append(" ");
+        int dist = bfs();
+        int now = K;
+        sb.append(now);
+        while (true) {
+            now = record[now];
+            if (now == -1) break;
+            sb.insert(0, now + " ");
         }
-        sb.append(k);
-        System.out.println(sb);
+        sb.insert(0, dist + "\n");
+        System.out.println(sb.toString());
     }
 
-    public static int bfs(int x, int k) {
+    static int bfs() {
         Queue<Integer> queue = new LinkedList<>();
-        queue.offer(x);
-        visited[x] = true;
-        line[x] = 0;
-
+        queue.offer(N);
+        time[N] = 1;
         while (!queue.isEmpty()) {
-            x = queue.poll();
-            if (x == k) break;
+            int now = queue.poll();
+            if (now == K) break;
 
-            int x1 = x - 1, x2 = x + 1, x3 = 2 * x;
-            if (x1 >= 0 && !visited[x1]) {
-                queue.offer(x1);
-                visited[x1] = true;
-                line[x1] = Math.min(line[x1], line[x] + 1);
-                route[x1] = x;
+            int next = now - 1;
+            if (next >= 0 && time[next] == 0) {
+                queue.offer(next);
+                time[next] = time[now] + 1;
+                record[next] = now;
             }
 
-            if (x2 < INF && !visited[x2]) {
-                queue.offer(x2);
-                visited[x2] = true;
-                line[x2] = Math.min(line[x2], line[x] + 1);
-                route[x2] = x;
+            next = now + 1;
+            if (next <= MAX && time[next] == 0) {
+                queue.offer(next);
+                time[next] = time[now] + 1;
+                record[next] = now;
             }
 
-            if (x3 < INF && !visited[x3]) {
-                queue.offer(x3);
-                visited[x3] = true;
-                line[x3] = Math.min(line[x3], line[x] + 1);
-                route[x3] = x;
+            next = now * 2;
+            if (next <= MAX && time[next] == 0) {
+                queue.offer(next);
+                time[next] = time[now] + 1;
+                record[next] = now;
             }
         }
-        return line[k];
+        return time[K] - 1;
     }
 }
