d83293aaf3108ad195b0a8114624c8dadd64ed43
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-07-05 23:10:02 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 2360 ms, Memory: 376476 KB -BaekjoonHub

--- 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
@@ -1,85 +1,87 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+import javax.security.auth.x500.X500Principal;
+import javax.tools.Diagnostic;
 
 public class Main {
-    static int N, M, K;
-    static int[][] map;
-    static boolean[][][] visited;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] str = br.readLine().split(" ");
-        N = Integer.parseInt(str[0]);
-        M = Integer.parseInt(str[1]);
-        K = Integer.parseInt(str[2]);
-        map = new int[N][M];
-        visited = new boolean[N][M][K + 1];
-        for (int i = 0; i < N; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < M; j++) {
-                map[i][j] = s.charAt(j) - '0';
-            }
-        }
-        System.out.println(bfs());
-    }
+	static int N, M, K;
+	static int[][] map;
+	static boolean[][][] visited;
 
-    public static int bfs() {
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(0, 0, 0, 0, true));
-        visited[0][0][0] = true;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
+		visited = new boolean[N][M][K + 1];
+		for (int i = 0; i < N; i++) {
+			String line = br.readLine();
+			for (int j = 0; j < M; j++) {
+				map[i][j] = line.charAt(j) - '0';
+			}
+		}
+		System.out.println(bfs());
+	}
 
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
-        while (!queue.isEmpty()) {
-            Point point = queue.poll();
-            int x = point.x;
-            int y = point.y;
-            if (x == N - 1 && y == M - 1) {
-                return point.move + 1;
-            }
+	static int bfs() {
+		Queue<Node> queue = new LinkedList<>();
+		queue.offer(new Node(0, 0, 0, 1, true));
+		visited[0][0][0] = true;
 
-            // 밤이라면 같은 칸에 머물기(다음 날에 벽을 부술수도 있으니)
-            if (!point.day) {
-                queue.offer(new Point(x, y, point.move + 1, point.crashed, !point.day));
-                visited[x][y][point.crashed] = true;
-            }
+		int[][] move = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+		while (!queue.isEmpty()) {
+			Node now = queue.poll();
+			int x = now.x, y = now.y;
+			if (x == N - 1 && y == M - 1) {
+				return now.dist;
+			}
 
-            // 상하좌우로 이동
-            for (int i = 0; i < 4; i++) {
-                int tx = x + dx[i];
-                int ty = y + dy[i];
-                if (tx < 0 || ty < 0 || tx >= N || ty >= M)
-                    continue;
+			// 현재 자리에 머무르는 것이 더 짧은 경로이려면 다음날 벽을 부수려고 그러는 것? 그렇담 밤에만 자기 자리에 있도록?
+			if (!now.day) {
+				queue.offer(new Node(x, y, now.crash, now.dist + 1, !now.day));
+				visited[x][y][now.crash] = true;
+			}
+			
+			for (int i = 0; i < 4; i++) {
+				int nx = x + move[i][0];
+				int ny = y + move[i][1];
+				if (nx < 0 || ny < 0 || nx >= N || ny >= M)
+					continue;
 
-                if (map[tx][ty] == 1 && point.day) { // 다음 칸이 벽이고 낮인 경우
-                    if (point.crashed < K && !visited[tx][ty][point.crashed + 1]) { // 아직 벽을 부술 기회가 남은 경우
-                        queue.offer(new Point(tx, ty, point.move + 1, point.crashed + 1, !point.day));
-                        visited[tx][ty][point.crashed + 1] = true;
-                    }
-                } else if (map[tx][ty] == 0) { // 다음 칸이 벽이 아닌 경우
-                    if (!visited[tx][ty][point.crashed]) {
-                        queue.offer(new Point(tx, ty, point.move + 1, point.crashed, !point.day));
-                        visited[tx][ty][point.crashed] = true;
-                    }
-                }
-            }
-        }
+				// 벽이 있는 곳 & 낮인 경우 & 벽을 부술 기회가 남은 경우
+				if (map[nx][ny] == 1 && now.day && now.crash < K) {
+					if (!visited[nx][ny][now.crash + 1]) {
+						queue.offer(new Node(nx, ny, now.crash + 1, now.dist + 1, !now.day));
+						visited[nx][ny][now.crash + 1] = true;
+					}
+				} else if (map[nx][ny] == 0 && !visited[nx][ny][now.crash]) {
+					queue.offer(new Node(nx, ny, now.crash, now.dist + 1, !now.day));
+					visited[nx][ny][now.crash] = true;
+				}
+			}
+		}
 
-        return -1;
-    }
+		return -1;
+	}
 
-    static class Point {
-        int x;
-        int y;
-        int move; // 이동 횟수
-        int crashed; // 벽을 부순 횟수
-        boolean day; // true: 낮, false: 밤
+	static class Node {
+		int x, y;
+		int crash; // 부순 벽의 개수
+		int dist; // 이동한 칸의 개수
+		boolean day; // 낮/밤 여부
 
-        public Point(int x, int y, int move, int crashed, boolean day) {
-            this.x = x;
-            this.y = y;
-            this.move = move;
-            this.crashed = crashed;
-            this.day = day;
-        }
-    }
+		public Node(int x, int y, int crash, int dist, boolean day) {
+			this.x = x;
+			this.y = y;
+			this.crash = crash;
+			this.dist = dist;
+			this.day = day;
+		}
+	}
 }
