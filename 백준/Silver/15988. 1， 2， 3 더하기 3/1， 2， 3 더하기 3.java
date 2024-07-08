3631ca9dfecce220be5a3e8ee571d852dbd7ae56
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-07-08 22:45:40 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 152 ms, Memory: 22084 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -1,26 +1,28 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static final int MAX = 1_000_000;
+    static final int DIV = 1_000_000_009;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int t = Integer.parseInt(br.readLine());
-        int[] nums = new int[t];
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
         int max = 0;
-        for (int i = 0; i < t; i++) {
-            nums[i] = Integer.parseInt(br.readLine());
-            max = Math.max(max, nums[i]);
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, arr[i]);
         }
 
-        long[] dp = new long[1000001];
+        long[] dp = new long[MAX + 1];
         dp[1] = 1; dp[2] = 2; dp[3] = 4;
         for (int i = 4; i <= max; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1000000009;
+            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % DIV;
         }
-        for (int i = 0; i < t; i++) {
-            bw.write((dp[nums[i]]) + "\n");
+
+        StringBuilder sb = new StringBuilder();
+        for (int a : arr) {
+            sb.append(dp[a]).append("\n");
         }
-        bw.flush(); bw.close();
+        System.out.println(sb);
     }
 }
