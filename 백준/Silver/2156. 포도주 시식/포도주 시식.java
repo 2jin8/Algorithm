e7b9ad1f7fcebb5c7dd67c90f3eb00e708baf5a9
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-07-11 23:15:36 +0900

    [Silver I] Title: 포도주 시식, Time: 124 ms, Memory: 14952 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,22 +1,22 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    static final int MAX = 10_001;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[MAX];
+        int[] arr = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        int[] dp = new int[MAX];
-        dp[1] = arr[1];
-        dp[2] = arr[1] + arr[2];
-        for (int i = 3; i <= n; i++) {
-            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 3] + arr[i - 1] + arr[i], dp[i - 2] + arr[i]));
+        int max = 0;
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            if (i == 1) dp[1] = arr[1];
+            else if (i == 2) dp[2] = arr[1] + arr[2];
+            else dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 3] + arr[i - 1] + arr[i], dp[i - 2] + arr[i]));
+            max = Math.max(max, dp[i]);
         }
-        System.out.println(dp[n]);
+        System.out.println(max);
     }
 }
