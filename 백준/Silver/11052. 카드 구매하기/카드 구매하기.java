23983cd6510bffd718c38da8a39915e325fcae6a
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-07-12 21:44:47 +0900

    [Silver I] Title: 카드 구매하기, Time: 128 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -1,22 +1,26 @@
+import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        String[] str = br.readLine().split(" ");
-        int[] cards = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            cards[i] = Integer.parseInt(str[i - 1]);
-        }
 
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[n + 1];
         int[] dp = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            dp[i] = cards[i];
-            for (int j = 1; j <= i / 2; j++) {
-                dp[i] = Math.max(dp[i], dp[j] + dp[i - j]);
-            }
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = arr[i];
         }
-        System.out.println(dp[n]);
+
+        int max = dp[1];
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], dp[i - j] + arr[j]);
+            }
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
     }
 }
