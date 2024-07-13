d9d35460c5f6e29cb109f05ea7ef704a27c7f558
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-07-13 19:15:59 +0900

    [Silver II] Title: 유기농 배추, Time: 132 ms, Memory: 16136 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -2,31 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
+    // 필요한 배추흰지렁이 수 == BFS 탐색 횟수
+    static int M, N, K;
     static int[][] map;
     static boolean[][] visited;
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine()); // 테스트 케이스의 수
-
+        int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         for (int t = 0; t < T; t++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             M = Integer.parseInt(st.nextToken());
             N = Integer.parseInt(st.nextToken());
-            int K = Integer.parseInt(st.nextToken());
-            map = new int[N][M];
-            visited = new boolean[N][M];
-            for (int k = 0; k < K; k++) {
+            K = Integer.parseInt(st.nextToken());
+            map = new int[M][N];
+            visited = new boolean[M][N];
+            // 배추의 위치
+            for (int i = 0; i < K; i++) {
                 st = new StringTokenizer(br.readLine(), " ");
-                int y = Integer.parseInt(st.nextToken());
                 int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
                 map[x][y] = 1;
             }
 
             int total = 0;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < N; j++) {
                     if (map[i][j] == 1 && !visited[i][j]) {
                         total++;
                         bfs(i, j);
@@ -35,7 +36,7 @@ public class Main {
             }
             sb.append(total).append("\n");
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 
     static void bfs(int x, int y) {
@@ -50,13 +51,14 @@ public class Main {
             for (int i = 0; i < 4; i++) {
                 int nx = now[0] + move[i][0];
                 int ny = now[1] + move[i][1];
-                // 범위 벗어나는 경우 or 방문한 경우 or 배추가 없는 경우 넘어가기
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny] || map[nx][ny] == 0) {
+                if (nx < 0 || ny < 0 || nx >= M || ny >= N) {
                     continue;
                 }
 
-                queue.offer(new int[]{nx, ny});
-                visited[nx][ny] = true;
+                if (!visited[nx][ny] && map[nx][ny] == 1) {
+                    queue.offer(new int[]{nx, ny});
+                    visited[nx][ny] = true;
+                }
             }
         }
     }
