88f344f70cadda47406d3812d960c583b739b55c
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-07-13 18:33:29 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14616 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,53 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, m;
-    static int[][] board, dist;
-    public static void main(String[] args) throws Exception {
+    static int N, M;
+    static int[][] map, dist;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        board = new int[n][m];
-        dist = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < m; j++) {
-                board[i][j] = line.charAt(j) - '0';
+        // 최소의 칸 수 구하기 = BFS (단, )
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        dist = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = str.charAt(j) - '0';
             }
         }
-        System.out.println(bfs(0, 0));
+        System.out.println(bfs());
     }
 
-    static int bfs(int x, int y) {
+    static int bfs() {
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{x, y});
-        dist[x][y] = 1; // 칸을 셀 때 시작 위치도 포함
+        queue.offer(new int[]{0, 0});
+        dist[0][0] = 1; // 시작 위치와 도착 위치도 칸을 셀 때 포함하기
 
         int[][] move = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         while (!queue.isEmpty()) {
             int[] now = queue.poll();
-            x = now[0];
-            y = now[1];
-            if (x == n - 1 && y == m - 1) {
-                // 칸을 셀 때 도착 위치도 포함 ⇒ 기록된 값 그래도 출력하면 됨
-                break; // 항상 도착위치로 이동할 수 있는 경우만 주어짐
-            }
+            int x = now[0], y = now[1];
+            if (x == N - 1 && y == M - 1) break;
 
+            // 네 방향으로 탐색하기
             for (int i = 0; i < 4; i++) {
                 int nx = x + move[i][0];
                 int ny = y + move[i][1];
-                // 범위 벗어나거나 이미 방문한 위치면 넘어가기
-                if (nx < 0 || ny < 0 || nx >= n || ny >= m || dist[nx][ny] != 0) {
+                // 범위를 벗어나면 탐색 X
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
                     continue;
                 }
 
-                if (board[nx][ny] == 1) {
+                // 이미 방문하지 않았고 이동할 수 있는 칸(=1)인 경우
+                if (dist[nx][ny] == 0 && map[nx][ny] == 1) {
                     queue.offer(new int[]{nx, ny});
                     dist[nx][ny] = dist[x][y] + 1;
                 }
             }
         }
-        return dist[n - 1][m - 1];
+        return dist[N - 1][M - 1];
     }
 }
