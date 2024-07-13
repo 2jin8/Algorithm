bc3b0a71f01b53fb1788bd70659b9879ca92e856
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-07-13 18:41:32 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,42 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    private static ArrayList<ArrayList<Integer>> computers = new ArrayList<>();
-    private static boolean[] visited;
-    private static int count = 0;
+    static int N;
+    static boolean[] visited;
+    static ArrayList<Integer>[] computers;
     public static void main(String[] args) throws IOException {
-        // 1번 컴퓨터가 웜 바이러스에 걸림
-        // 1번 컴퓨터를 통해 웜 바이러스에 걸리게 되는 컴퓨터의 수 출력
+        // 1번 컴퓨터를 통해 바이러스에 걸리는 컴퓨터의 수 = 1번 컴퓨터와 연결된 컴퓨터의 수
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); // 컴퓨터의 수
-        int pair = Integer.parseInt(br.readLine()); // 연결되어 있는 컴퓨터 쌍의 수
+        N = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-        visited = new boolean[n + 1];
-        for (int i = 0; i <= n; i++) {
-            computers.add(new ArrayList<>());
+        visited = new boolean[N + 1];
+        computers = new ArrayList[N + 1];
+        for (int i = 0; i <= N; i++) {
+            computers[i] = new ArrayList<>();
         }
 
-        for (int i = 0; i < pair; i++) {
+        // 연결 정보 기록하기
+        for (int i = 0; i < T; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            computers.get(u).add(v);
-            computers.get(v).add(u);
-        }
+            int c1 = Integer.parseInt(st.nextToken());
+            int c2 = Integer.parseInt(st.nextToken());
 
-        dfs(1);
-        System.out.println(count - 1);
+            computers[c1].add(c2);
+            computers[c2].add(c1);
+        }
+        System.out.println(bfs(1));
     }
 
-    public static void dfs(int x) {
+    static int bfs(int x) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(x);
         visited[x] = true;
-        count++;
 
-        for (int i = 0; i < computers.get(x).size(); i++) {
-            int y = computers.get(x).get(i);
+        int total = 0; // 바이러스에 걸리는 컴퓨터의 수
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
 
-            if (!visited[y]) dfs(y);
+            for (int i = 0; i < computers[now].size(); i++) {
+                int next = computers[now].get(i);
+                if (!visited[next]) {
+                    queue.offer(next);
+                    visited[next] = true;
+                    total++;
+                }
+            }
         }
+        return total;
     }
 }
