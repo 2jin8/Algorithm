6d6ad3ce8016f5d98d7780d6da957b8072fb075f
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-07-18 00:07:13 +0900

    [Silver III] Title: 이친수, Time: 108 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -1,16 +1,18 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        long[] dp = new long[n + 1];
-        for (int i = 1; i <= n; i++) {
-            if (i <= 2) dp[i] = 1;
-            else {
-                dp[i] = dp[i - 1] + dp[i - 2];
-            }
-        }
-        System.out.println(dp[n]);
-    }
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		long[] dp = new long[n + 1];
+		dp[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+		System.out.println(dp[n]);
+	}
+
 }
