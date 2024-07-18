9b65e1a9f3765ba40d4de78d92d81e4bbf940190
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-07-18 23:57:07 +0900

    [Silver I] Title: 음식물 피하기, Time: 172 ms, Memory: 15580 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
@@ -1,49 +1,65 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M, K, maxArea, area;
-    static int[][] map;
-    static boolean[][] visited;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
+    static int N, M, K;
+    static boolean[][] waste, visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
+        waste = new boolean[N][M];
         visited = new boolean[N][M];
         for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            int r = Integer.parseInt(st.nextToken()) - 1;
-            int c = Integer.parseInt(st.nextToken()) - 1;
-            map[r][c] = 1;
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            waste[x][y] = true;
         }
 
+        int answer = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (map[i][j] == 1 && !visited[i][j]) {
-                    area = 0;
-                    dfs(i, j);
-                    maxArea = Math.max(area, maxArea);
+                // 방문하지 않았고 음식물이 있는 위치인 경우
+                if (!visited[i][j] && waste[i][j]) {
+                    answer = Math.max(answer, bfs(i, j));
                 }
             }
         }
-        System.out.println(maxArea);
+        System.out.println(answer);
     }
 
-    public static void dfs(int x, int y) {
+    public static int bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y});
         visited[x][y] = true;
-        area++;
 
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny]) continue;
+        int size = 0;
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            size++;
 
-            if (map[nx][ny] == 1) dfs(nx, ny);
+            for (int i = 0; i < 4; i++) {
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
+                    continue;
+                }
+
+                if (!visited[nx][ny] && waste[nx][ny]) {
+                    queue.offer(new int[]{nx, ny});
+                    visited[nx][ny] = true;
+                }
+            }
         }
+        return size;
     }
 }
