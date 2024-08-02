19597bcffa4290b1553a83696fa13bc9c64e7a6a
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-08-03 00:19:26 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 128 ms, Memory: 23,676 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -1,31 +1,39 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Base64;
 import java.util.StringTokenizer;
 
 class Solution {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		for (int t = 1; t <= 10; t++) {
-			int n = Integer.parseInt(br.readLine());
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int[] height = new int[n];
-			for (int i = 0; i < n; i++) {
-				height[i] = Integer.parseInt(st.nextToken());
-			}
+    /**
+     * 테스트 개수는 총 10개
+     * n: 건물의 개수
+     * heights: 건물의 높이
+     * total: 조망권이 확보된 세대의 수
+     */
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        for (int t = 1; t <= 10; t++) {
+            // 건물의 개수, 건물의 높이 저장하기
+            int n = Integer.parseInt(br.readLine());
+            int[] heights = new int[n];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                heights[i] = Integer.parseInt(st.nextToken());
+            }
 
-			int total = 0;
-			for (int i = 2; i < n - 2; i++) {
-				int h = height[i];
-				if (h > height[i - 1] && h > height[i - 2] && h > height[i + 1] && h > height[i + 2]) {
-					int max = Math.max(Math.max(height[i - 1], height[i - 2]), Math.max(height[i + 1], height[i + 2]));
-					total += h - max;
-				}
-			}
-			sb.append('#').append(t).append(' ').append(total).append('\n');
-		}
-		System.out.println(sb.toString());
-	}
+            // 조망권이 확보된 세대의 수 찾기
+            int total = 0;
+            // 맨 왼쪽 두 칸과 오른쪽 두 칸에는 건물이 지어지지 않으므로 2 ~ (n - 3)까지 반복하기
+            for (int i = 2; i < n - 2; i++) {
+                int height = heights[i];
+                // 양 옆 건물의 최대 높이 구하기
+                int max = Math.max(Math.max(heights[i - 2], heights[i - 1]), Math.max(heights[i + 1], heights[i + 2]));
+                if (height > max) { // 양 옆 건물의 최대 높이보다 현재 건물의 높이가 크다면
+                    total += height - max; // 조망권이 확보됨
+                }
+            }
+            sb.append("#").append(t).append(" ").append(total).append("\n");
+        }
+        System.out.println(sb);
+    }
 }
