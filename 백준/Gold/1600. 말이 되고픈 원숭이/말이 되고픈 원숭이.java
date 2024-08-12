35a84aa297eaf8c747684d444dbfc165a569cc36
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-08-13 00:42:35 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 544 ms, Memory: 91648 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -5,82 +5,80 @@ public class Main {
     static int K, W, H;
     static int[][] board;
     static boolean[][][] visited;
-    static int[][] jump = {{2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}};
-    static int[][] move = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         K = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         W = Integer.parseInt(st.nextToken());
         H = Integer.parseInt(st.nextToken());
         board = new int[H][W];
-        visited = new boolean[H][W][K + 1];
-
+        visited = new boolean[H][W][K + 1]; // K번 점프 가능 (상태 변경!)
         for (int i = 0; i < H; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < W; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
         System.out.println(bfs());
     }
 
-    static int bfs() {
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(new Node(0, 0, 0, 0));
+    public static int bfs() {
+        Queue<Point> queue = new LinkedList<>();
+        queue.offer(new Point(0, 0, 0, 0));
         visited[0][0][0] = true;
 
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+        int[] hx = {2, -2, 2, -2, 1, -1, 1, -1};
+        int[] hy = {1, 1, -1, -1, 2, 2, -2, -2};
         while (!queue.isEmpty()) {
-            Node now = queue.poll();
-            int x = now.x, y = now.y;
+            Point point = queue.poll();
+            int x = point.x;
+            int y = point.y;
             if (x == H - 1 && y == W - 1) {
-                return now.dist;
+                return point.move;
             }
 
-            // 우선 원숭이처럼 이동하기
+            // 인접한 네 방향으로 움직이기
             for (int i = 0; i < 4; i++) {
-                int nx = x + move[i][0];
-                int ny = y + move[i][1];
-                if (nx < 0 || ny < 0 || nx >= H || ny >= W || visited[nx][ny][now.jump]) {
-                    continue;
-                }
+                int tx = x + dx[i];
+                int ty = y + dy[i];
+                if (tx < 0 || ty < 0 || tx >= H || ty >= W) continue;
 
-                if (board[nx][ny] == 0) { // 평지면 이동 가능
-                    queue.offer(new Node(nx, ny, now.jump, now.dist + 1));
-                    visited[nx][ny][now.jump] = true;
+                if (!visited[tx][ty][point.cnt] && board[tx][ty] == 0) {
+                    visited[tx][ty][point.cnt] = true;
+                    queue.offer(new Point(tx, ty, point.cnt, point.move + 1));
                 }
             }
 
-            // 말처럼 이동할 수 있는 횟수가 남은 경우
-            if (now.jump < K) {
+            // 말의 움직임으로 움직이기
+            if (point.cnt < K) { // 이동 횟수가 남았다면
                 for (int i = 0; i < 8; i++) {
-                    int nx = x + jump[i][0];
-                    int ny = y + jump[i][1];
-                    if (nx < 0 || ny < 0 || nx >= H || ny >= W || visited[nx][ny][now.jump + 1]) {
-                        continue;
-                    }
+                    int tx = x + hx[i];
+                    int ty = y + hy[i];
+                    if (tx < 0 || ty < 0 || tx >= H || ty >= W) continue;
 
-                    if (board[nx][ny] == 0) { // 평지면 이동 가능
-                        queue.offer(new Node(nx, ny, now.jump + 1, now.dist + 1));
-                        visited[nx][ny][now.jump + 1] = true;
+                    if (!visited[tx][ty][point.cnt + 1] && board[tx][ty] == 0) {
+                        visited[tx][ty][point.cnt + 1] = true;
+                        queue.offer(new Point(tx, ty, point.cnt + 1, point.move + 1));
                     }
                 }
             }
         }
         return -1;
     }
+}
 
-    static class Node {
-        int x, y;
-        int jump; // 말처럼 움직인 횟수
-        int dist; // 이동 횟수
+class Point {
+    int x;
+    int y;
+    int cnt; // 말처럼 이동한 횟수
+    int move; // 동작 횟수
 
-        public Node(int x, int y, int jump, int dist) {
-            this.x = x;
-            this.y = y;
-            this.jump = jump;
-            this.dist = dist;
-        }
+    public Point(int x, int y, int cnt, int move) {
+        this.x = x;
+        this.y = y;
+        this.cnt = cnt;
+        this.move = move;
     }
 }
