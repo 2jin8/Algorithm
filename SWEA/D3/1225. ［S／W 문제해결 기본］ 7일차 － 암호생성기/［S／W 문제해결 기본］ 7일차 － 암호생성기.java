386fed1d07ba8a0e9731d53c7be7019a951c4471
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-08-16 22:38:10 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 127 ms, Memory: 20,080 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -1,35 +1,40 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		for (int tc=0; tc<10; tc++) {
-			int n = sc.nextInt();
-			Queue<Integer> queue = new LinkedList<Integer>();
-			for (int i=0; i<8; i++) {
-				queue.offer(sc.nextInt());
-			}
-			
-			int minus = 1;
-			while (true) {
-				if (minus > 5) minus = 1; 
-				int num = queue.poll() - minus++;
-				if (num <= 0) {
-					queue.offer(0);
-					break;
-				}
-				queue.offer(num);
-			}
-			
-			StringBuilder sBuilder = new StringBuilder("#"+n+" ");
-			while (!queue.isEmpty()) {
-				sBuilder.append(queue.poll()+" ");
-			}
-			System.out.println(sBuilder.toString());
-		}
-	}
+public class Solution {
+
+    static int N = 8;
+    static int[] nums = new int[N];
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        for (int t = 1; t <= 10; t++) {
+            int tc = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                nums[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int idx = 0, value = 1;
+            while (true) {
+                nums[idx] = Math.max(0, nums[idx] - value++);
+                if (nums[idx++] == 0) break; // 0이 되면 종료하기
+
+                // 인덱스를 넘어가면 조정하기
+                if (idx == N) idx = 0;
+                // 1 사이클이 끝나면 감소하는 값 초기화하기
+                if (value > 5) value = 1;
+            }
+
+            sb.append("#").append(tc).append(" ");
+            for (int i = 0; i < N; i++) {
+                // 제일 앞(idx)부터부터 총 N개 출력하기
+                sb.append(nums[idx++]).append(" ");
+                if (idx == N) idx = 0;
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
 }
