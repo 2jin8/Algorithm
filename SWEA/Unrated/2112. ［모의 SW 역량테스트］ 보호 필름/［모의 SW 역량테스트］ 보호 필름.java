ca0d07e36ee2809973827b570f9f4a0d41ff02a2
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-08-17 16:34:00 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 666 ms, Memory: 107,852 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -1,92 +1,86 @@
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
-    static int D, W, K, minCnt;
-    static int[][] film, checkFilm;
-    static boolean[] changed; // 약품 투입 여부를 확인하기 위한 배열
-    static final int MAX = 13;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
+	static int D, W, K, minCnt; // D: 깊이, W: 너비, K: 합격 기준, minCnt: 최소 약품 투입 횟수
+	static int[][] filmInfo; // 보호 필름 단면의 정보
 
-        for (int t = 1; t <= T; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            D = Integer.parseInt(st.nextToken());
-            W = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			D = Integer.parseInt(st.nextToken());
+			W = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+			filmInfo = new int[D][W];
+			for (int i = 0; i < D; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < W; j++) {
+					filmInfo[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
 
-            film = new int[D][W];
-            checkFilm = new int[D][W];
-            changed = new boolean[D];
+			minCnt = Integer.MAX_VALUE;
+			dfs(0, 0);
+			sb.append("#").append(t).append(" ").append(minCnt).append("\n");
+		}
+		System.out.println(sb);
+	}
 
-            for (int i = 0; i < D; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < W; j++) {
-                    // A는 0, B는 1
-                    film[i][j] = Integer.parseInt(st.nextToken());
-                    checkFilm[i][j] = film[i][j];
-                }
-            }
-            minCnt = MAX + 1; // 두께는 최대 13
-            if (K == 1) minCnt = 0;
-            else dfs(0, 0);
-            sb.append("#").append(t).append(" ").append(minCnt).append("\n");
-        }
-        System.out.println(sb);
-    }
+	private static void dfs(int depth, int cnt) {
+		if (depth == D) { // 보호 필름 두께만큼 다 탐색한 경우
+			// 검사를 통과한 경우
+			if (checkFilm()) {
+				minCnt = Math.min(minCnt, cnt); // 최소 투입 횟수 갱신
+			}
+			return;
+		}
 
-    static void dfs(int depth, int cnt) {
-        if (cnt >= minCnt) {
-            return;
-        }
-        else if (depth == D) {
-            if (checkFilm()) {
-                minCnt = Math.min(minCnt, cnt);
-            }
-            return;
-        }
+		// 최소 투입 횟수보다 크다면 답이 아니므로 종료하기
+		if (cnt > minCnt) {
+			return;
+		}
 
-        int[] tmp = film[depth].clone();
+		// 약품 투입 X
+		dfs(depth + 1, cnt);
 
-        // 약품 투입 X
-        dfs(depth + 1, cnt);
+		// 기존 상태 기록하기
+		int[] tmp = filmInfo[depth].clone();
 
-        // 약품 투입 O (A)
-        Arrays.fill(film[depth], 0);
-        dfs(depth + 1, cnt + 1);
+		// A 약품 투입
+		Arrays.fill(filmInfo[depth], 0);
+		dfs(depth + 1, cnt + 1);
 
-        // 약품 투입 O (B)
-        Arrays.fill(film[depth], 1);
-        dfs(depth + 1, cnt + 1);
+		// B 약품 투입
+		Arrays.fill(filmInfo[depth], 1);
+		dfs(depth + 1, cnt + 1);
 
-        // 되돌리기
-        film[depth] = tmp.clone();
-    }
+		// 기존 상태로 되돌리기
+		filmInfo[depth] = tmp.clone();
+	}
 
-    // 성능 검사
-    static boolean checkFilm() {
-        for (int i = 0; i < W; i++) {
-            int cnt = 1, now = film[0][i];
-            for (int j = 1; j < D; j++) {
-                if (now == film[j][i]) { // 동일한 특성을 가지는 경우
-                    cnt++;
-                } else { // 다른 특성을 가지는 경우, 리셋
-                    cnt = 1;
-                    now = film[j][i];
-                }
-
-                // 검사에 통과한 경우
-                if (cnt >= K) {
-                    break;
-                }
-            }
-            if (cnt < K) return false;
-        }
-        return true;
-    }
+	private static boolean checkFilm() {
+		for (int j = 0; j < W; j++) {
+			int now = filmInfo[0][j], cnt = 1;
+			for (int i = 1; i < D; i++) {
+				if (filmInfo[i][j] == now) {
+					cnt++;
+				} else {
+					now = filmInfo[i][j];
+					cnt = 1;
+				}
+				
+				if (cnt == K) break;
+			}
+			if (cnt < K) return false;
+		}
+		return true;
+	}
 }
