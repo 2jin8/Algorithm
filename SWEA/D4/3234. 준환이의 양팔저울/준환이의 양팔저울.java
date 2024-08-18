a798a5046a8b3fb0d33785143fdaed96e8debe99
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-08-18 21:37:10 +0900

    [D4] Title: 준환이의 양팔저울, Time: 818 ms, Memory: 20,732 KB -BaekjoonHub

--- SWEA/D4/3234. 준환이의 양팔저울/준환이의 양팔저울.java
+++ SWEA/D4/3234. 준환이의 양팔저울/준환이의 양팔저울.java
@@ -4,7 +4,9 @@ import java.util.StringTokenizer;
 
 public class Solution {
 
-	static int total;
+	static int N, total;
+	static int[] weights, nums; // weights: 각 무게추의 무게를 저장하는 배열, nums: 순열을 저장하는 배열
+	static boolean[] used; // 무게추 사용 여부를 저장하는 배열
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,9 +14,10 @@ public class Solution {
 		int T = Integer.parseInt(br.readLine());
 
 		for (int t = 1; t <= T; t++) {
-			int N = Integer.parseInt(br.readLine());
-			int[] weights = new int[N]; // 각 무게추의 무게를 저장하는 배열
-			boolean[] used = new boolean[N]; // 무게추 사용 여부를 저장하는 배열
+			N = Integer.parseInt(br.readLine());
+			weights = new int[N];
+			nums = new int[N];
+			used = new boolean[N];
 
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < N; i++) {
@@ -22,37 +25,45 @@ public class Solution {
 			}
 
 			total = 0;
-			dfs(weights, used, N, 0, 0, 0);
+			dfs(0);
 			sb.append("#").append(t).append(" ").append(total).append("\n");
 		}
 		System.out.println(sb);
 	}
 
-	private static void dfs(int[] weights, boolean[] used, int N, int depth, int left, int right) {
+    // 순열 만들기
+	private static void dfs(int depth) {
 		// 모든 추를 다 사용한 경우
 		if (depth == N) {
-			// 오른쪽 무게 총합이 왼쪽 무게보다 작거나 같으므로 세기
-			total++;
+			leftOrRight(0, 0, 0);
 			return;
 		}
 
 		// 모든 무게 추를 양팔저울 위에 올리는 순서
 		for (int i = 0; i < N; i++) {
 			if (!used[i]) {
-				// 현재 추 사용 처리하기
-				used[i] = true;
 
-				// 왼쪽에 올리기
-				dfs(weights, used, N, depth + 1, left + weights[i], right);
-
-				// 오른쪽에 올리기 (올려도 왼쪽보다 작을 때만)
-				if (left >= right + weights[i]) {
-					dfs(weights, used, N, depth + 1, left, right + weights[i]);
-				}
-
-				// 현재 추 사용 처리 취소하기
-				used[i] = false;
+				used[i] = true; // 현재 추 사용 처리하기
+				nums[depth] = weights[i]; // 현재 값 기록하기
+				dfs(depth + 1);
+				used[i] = false; // 현재 추 사용 처리 취소하기
 			}
+		}
+	}
+
+    // 왼쪽 저울 또는 오른쪽 저울에 올리기
+	static void leftOrRight(int depth, int left, int right) {
+		if (depth == N) {
+			total++;
+			return;
+		}
+
+		// 왼쪽 저울에 넣기
+		leftOrRight(depth + 1, left + nums[depth], right);
+
+		// 오른쪽 저울에 넣기
+		if (left >= right + nums[depth]) {
+			leftOrRight(depth + 1, left, right + nums[depth]);
 		}
 	}
 }
