56e274484b98005d2e7e0679a16458d7b59ee28b
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-08-22 21:49:00 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 126 ms, Memory: 28,080 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -4,50 +4,60 @@ import java.util.StringTokenizer;
 
 public class Solution {
 
-    static int minCost; // 최소 비용
-    static int[] costs = new int[4]; // 1일, 1달, 3달, 1년
-    static int[] monthPlan = new int[13]; // 1월 ~ 12월
+	static int N = 4, M = 12, minPrice;
+	static int[] prices, month;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception {
 
-        for (int t = 1; t <= T; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < 4; i++) {
-                costs[i] = Integer.parseInt(st.nextToken());
-            }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			prices = new int[N]; // 이용권 금액
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				prices[i] = Integer.parseInt(st.nextToken());
+			}
 
-            st = new StringTokenizer(br.readLine());
-            for (int i = 1; i <= 12; i++) {
-                monthPlan[i] = Integer.parseInt(st.nextToken());
-            }
+			month = new int[M + 1]; // 월 별 이용 계획
+			st = new StringTokenizer(br.readLine());
+			for (int i = 1; i <= M; i++) {
+				month[i] = Integer.parseInt(st.nextToken());
+			}
+			
+            // 최대 금액인 1년치 금액으로 초기화하기
+            minPrice = prices[3];
+            // 1월부터 시작하기
+			dfs(1, 0);
+			sb.append("#").append(t).append(" ").append(minPrice).append("\n");
+		}
+		System.out.println(sb);
+	}
 
-            minCost = Integer.MAX_VALUE;
-            dfs(1, 0);
-            sb.append("#").append(t).append(" ").append(minCost).append("\n");
-        }
-        System.out.println(sb);
-    }
+	static void dfs(int depth, int price) {
+		// 최소 사용 금액보다 크다면 return
+		if (minPrice <= price)
+			return;
 
-    static void dfs(int month, int cost) {
-        // 12달 다 확인하면 최소 비용 갱신 후 탐색 종료
-        if (month > 12) {
-            minCost = Math.min(minCost, cost);
-            return;
-        }
+		// 모든 달을 다 계산했다면 최소 사용 금액 갱신하기
+		if (depth > M) {
+			minPrice = price;
+			return;
+		}
+		
+		// 해당 달에 이용하지 않는다면 바로 다음 달로 넘어가기
+		if (month[depth] == 0) {
+			dfs(depth+1, price);
+			return;
+		}
 
-        // 1일권
-        dfs(month + 1, cost + costs[0] * monthPlan[month]);
+		// 1일권
+		dfs(depth + 1, price + prices[0] * month[depth]);
 
-        // 1달권
-        dfs(month + 1, cost + costs[1]);
+		// 1달권
+		dfs(depth + 1, price + prices[1]);
 
-        // 3달권
-        dfs(month + 3, cost + costs[2]);
-
-        // 1년권
-        dfs(month + 12, cost + costs[3]);
-    }
+		// 3달권
+		dfs(depth + 3, price + prices[2]);
+	}
 }
