9043ef1767a06f8938da4d3eeaca4aad689e1776
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-08-22 19:46:41 +0900

    [Silver IV] Title: 색종이 - 2, Time: 68 ms, Memory: 11612 KB -BaekjoonHub

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
@@ -1,70 +1,45 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static final int N = 102, W = 10;
+	static int N = 100, W = 10;
 	static int[][] arr = new int[N][N];
-	static boolean[][] visited = new boolean[N][N];
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int M = Integer.parseInt(br.readLine()); // 색종이의 수
-		
-		// 색종이의 위치 기록하기
+		int M = Integer.parseInt(br.readLine());
 		for (int i = 0; i < M; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int row = Integer.parseInt(st.nextToken()) + 1;
-			int col = Integer.parseInt(st.nextToken()) + 1;
+			int row = Integer.parseInt(st.nextToken());
+			int col = Integer.parseInt(st.nextToken());
 			for (int r = row, rEnd = row + W; r < rEnd; r++) {
 				for (int c = col, cEnd = col + W; c < cEnd; c++) {
 					arr[r][c] = 1;
 				}
 			}
+
 		}
 
 		int area = 0;
-		// 색종이가 붙여지지 않은 영역 BFS 탐색
-		// 1. 색종이가 붙여지지 않은 지역에서 네 방향 탐색하기
-		// 2. 이동하려는 곳이 색종이가 붙여진 곳 => 색종이의 둘레에 해당함
+		int[] dx = { 1, -1, 0, 0 }, dy = { 0, 0, 1, -1 };
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if (arr[i][j] == 0 && !visited[i][j]) {
-					area += bfs(i, j);
+				if (arr[i][j] == 0)
+					continue;
+
+				// 네 방향 탐색
+				for (int k = 0; k < 4; k++) {
+					int nx = i + dx[k];
+					int ny = j + dy[k];
+					// 배열 범위를 벗어나거나 색종이로 덮인 부분이 아니면 둘레로 판단
+					if (nx < 0 || ny < 0 || nx >= N || ny >= N || arr[nx][ny] == 0) {
+						area++;
+					}
 				}
 			}
 		}
 		System.out.println(area);
-	}
-
-	static int bfs(int x, int y) {
-		Queue<int[]> queue = new ArrayDeque<>();
-		queue.offer(new int[] { x, y });
-		visited[x][y] = true;
-
-		int area = 0;
-		int[] dx = { 1, -1, 0, 0 }, dy = { 0, 0, 1, -1 };
-		while (!queue.isEmpty()) {
-			int[] now = queue.poll();
-
-			for (int i = 0; i < 4; i++) {
-				int nx = now[0] + dx[i];
-				int ny = now[1] + dy[i];
-				if (nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny])
-					continue;
-
-				// 색종이가 붙여진 지역이면 둘레에 포함되므로 세기
-				if (arr[nx][ny] == 1) {
-					area++;
-				} else { // 색종이가 붙여지지 않은 지역이면 큐에 넣기
-					queue.offer(new int[] { nx, ny });
-					visited[nx][ny] = true;
-				}
-			}
-		}
-		return area;
 	}
 }
