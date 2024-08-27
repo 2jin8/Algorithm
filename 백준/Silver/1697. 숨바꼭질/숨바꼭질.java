0e03df98b87c7e2088b760693f40727b084d0976
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-08-27 14:50:30 +0900

    [Silver I] Title: 숨바꼭질, Time: 88 ms, Memory: 14636 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,46 +1,66 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, K;
-    static final int MAX = 100_000;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        System.out.println(bfs());
-    }
 
-    static int bfs() {
-        Queue<Integer> queue = new LinkedList<>();
-        int[] dist = new int[MAX + 1];
-        dist[N] = 1;
-        queue.offer(N);
+	static int N, K, L = 100_001;
+	static int[] pos = new int[L];
+	static boolean[] visited = new boolean[L];
 
-        while (!queue.isEmpty()) {
-            int x = queue.poll();
-            if (x == K) break;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
 
-            // x - 1
-            int nx = x - 1;
-            if (nx >= 0 && dist[nx] == 0) {
-                dist[nx] = dist[x] + 1;
-                queue.offer(nx);
-            }
+		bfs();
+	}
 
-            nx = x + 1;
-            if (nx <= MAX && dist[nx] == 0) {
-                dist[nx] = dist[x] + 1;
-                queue.offer(nx);
-            }
+	static void bfs() {
+		Queue<Point> queue = new ArrayDeque<>();
+		queue.offer(new Point(N, 0));
+		visited[N] = true;
 
-            nx = 2 * x;
-            if (nx <= MAX && dist[nx] == 0) {
-                dist[nx] = dist[x] + 1;
-                queue.offer(nx);
-            }
-        }
-        return dist[K] - 1;
-    }
+		while (!queue.isEmpty()) {
+			Point now = queue.poll();
+			if (now.x == K) {
+				System.out.println(now.hour);
+				break;
+			}
+
+			// X + 1로 이동
+			int next = now.x + 1;
+			if (next < L && !visited[next]) {
+				queue.offer(new Point(next, now.hour + 1));
+				visited[next] = true;
+			}
+
+			// X - 1로 이동
+			next = now.x - 1;
+			if (next >= 0 && !visited[next]) {
+				queue.offer(new Point(next, now.hour + 1));
+				visited[next] = true;
+			}
+			
+			// 2 * X로 이동
+			next = now.x  * 2;
+			if (next < L && !visited[next]) {
+				queue.offer(new Point(next, now.hour + 1));
+				visited[next] = true;
+			}
+		}
+	}
+
+	static class Point {
+		int x;
+		int hour;
+
+		public Point(int x, int hour) {
+			this.x = x;
+			this.hour = hour;
+		}
+	}
 }
