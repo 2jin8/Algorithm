757e369cb4b94797b1da05c31dabab275ddf3cb7
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-08-27 14:36:20 +0900

    [Silver I] Title: 단지번호붙이기, Time: 68 ms, Memory: 11648 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,68 +1,85 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Queue;
 
 public class Main {
-    static int N;
-    static int[][] map;
-    static boolean[][] visited;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // 연결 == 상하좌우 연결
-        // 단지수, 각 단지에 속하는 집의 수 출력
-        // 단지수 = BFS 탐색 횟수
-        N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        visited = new boolean[N][N];
-        for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < N; j++) {
-                map[i][j] = str.charAt(j) - '0';
-            }
-        }
 
-        ArrayList<Integer> apartments = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (map[i][j] == 1 && !visited[i][j]) {
-                    apartments.add(bfs(i, j));
-                }
-            }
-        }
-        // 집의 수를 오름차순으로 정렬
-        Collections.sort(apartments);
-        StringBuilder sb = new StringBuilder();
-        sb.append(apartments.size()).append("\n");
-        for (int apartment : apartments) {
-            sb.append(apartment).append("\n");
-        }
-        System.out.println(sb);
-    }
+	static int N;
+	static int[][] map;
+	static boolean[][] visited;
 
-    static int bfs(int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{x, y});
-        visited[x][y] = true;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		ArrayList<Integer> ans = new ArrayList<>(); // 단지에 속하는 집의 수를 저장하기 위한 리스트
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		visited = new boolean[N][N];
+		for (int i = 0; i < N; i++) {
+			String line = br.readLine();
+			for (int j = 0; j < N; j++) {
+				map[i][j] = line.charAt(j) - '0';
+			}
+		}
 
-        int total = 0;
-        int[][] move = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            total++;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				// 아직 방문하지 않았고 집이 있는 곳인 경우
+				if (!visited[i][j] && map[i][j] == 1) {
+					ans.add(bfs(i, j));
+				}
+			}
+		}
 
-            for (int i = 0; i < 4; i++) {
-                int nx = now[0] + move[i][0];
-                int ny = now[1] + move[i][1];
-                if (nx < 0 || ny < 0 || nx >= N || ny >= N) {
-                    continue;
-                }
+		// 집의 수를 오름차순으로 정렬
+		Collections.sort(ans);
 
-                // 방문하지 않았고 집이 있는 곳인 경우
-                if (!visited[nx][ny] && map[nx][ny] == 1) {
-                    queue.offer(new int[]{nx, ny});
-                    visited[nx][ny] = true;
-                }
-            }
-        }
-        return total;
-    }
+		StringBuilder sb = new StringBuilder();
+		sb.append(ans.size()).append("\n");
+		for (int a : ans) {
+			sb.append(a).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	static int bfs(int sx, int sy) {
+		Queue<Point> queue = new ArrayDeque<>();
+		queue.offer(new Point(sx, sy));
+		visited[sx][sy] = true;
+
+		int home = 0;
+		int[] dx = { 1, -1, 0, 0 }, dy = { 0, 0, 1, -1 };
+		while (!queue.isEmpty()) {
+			Point now = queue.poll();
+			int x = now.x, y = now.y;
+			home++;
+
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				// 범위를 벗어난 경우
+				if (nx < 0 || ny < 0 || nx >= N || ny >= N)
+					continue;
+
+				// 이미 방문했거나 집이 없는 곳인 경우
+				if (visited[nx][ny] || map[nx][ny] == 0)
+					continue;
+
+				queue.offer(new Point(nx, ny));
+				visited[nx][ny] = true;
+			}
+		}
+		return home;
+	}
+
+	static class Point {
+		int x, y;
+
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 }
