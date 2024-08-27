56e09bf54ec016c705414ebf3f7d1e78b34aacf8
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-08-27 14:26:57 +0900

    [Silver II] Title: 섬의 개수, Time: 100 ms, Memory: 13700 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,62 +1,85 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    private static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
-    private static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
-    private static boolean[][] visited;
-    private static int[][] land;
-    private static int w, h, count;
+	static int w, h; // w: 너비, h: 높이
+	static int[][] map;
+	static boolean[][] visited;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		while (true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			w = Integer.parseInt(st.nextToken());
+			h = Integer.parseInt(st.nextToken());
+			if (w == 0 && h == 0) {
+				System.out.println(sb);
+				break;
+			}
 
-        while (true) {
-            st = new StringTokenizer(br.readLine());
-            w = Integer.parseInt(st.nextToken()); // 너비
-            h = Integer.parseInt(st.nextToken()); // 높이
-            if (w == 0 || h == 0) return;
+			map = new int[h][w];
+			visited = new boolean[h][w];
+			for (int i = 0; i < h; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < w; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
 
-            count = 0;
-            land = new int[h][w];
-            visited = new boolean[h][w];
+			int island = 0;
+			for (int i = 0; i < h; i++) {
+				for (int j = 0; j < w; j++) {
+					// 방문하지 않았고 섬인 경우 BFS 탐색
+					if (!visited[i][j] && map[i][j] == 1) {
+						bfs(i, j);
+						island++;
+					}
+				}
+			}
+			sb.append(island).append("\n");
+		}
+	}
 
-            for (int i = 0; i < h; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < w; j++) {
-                    land[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
+	static void bfs(int sx, int sy) {
+		Queue<Point> queue = new ArrayDeque<>();
+		queue.offer(new Point(sx, sy));
+		visited[sx][sy] = true;
 
-            for (int i = 0; i < h; i++) {
-                for (int j = 0; j < w; j++) {
-                    if (land[i][j] == 1 && !visited[i][j]) { // 아직 방문하지 않은 섬인 경우
-                        DFS(i, j);
-                        count++;
-                    }
-                }
-            }
-            System.out.println(count);
-        }
-    }
+		// 가로, 세로, 대각선으로 연결되어 있는 섬을 찾아야 함
+		int[] dx = { 1, -1, 0, 0, 1, 1, -1, -1 };
+		int[] dy = { 0, 0, 1, -1, 1, -1, 1, -1 };
+		while (!queue.isEmpty()) {
+			Point now = queue.poll();
+			int x = now.x, y = now.y;
 
-    private static void DFS(int i, int j) {
-        visited[i][j] = true;
+			for (int i = 0; i < 8; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				// 범위를 벗어나는 경우
+				if (nx < 0 || ny < 0 || nx >= h || ny >= w)
+					continue;
 
-        for (int k = 0; k < 8; k++) {
-            int x = i + dx[k];
-            int y = j + dy[k];
+				// 이미 방문했거나 섬이 아닌 바다인 경우
+				if (visited[nx][ny] || map[nx][ny] == 0)
+					continue;
 
-            if (x < 0 || y < 0 || x >= h || y >= w)
-                continue;
+				queue.offer(new Point(nx, ny));
+				visited[nx][ny] = true;
+			}
+		}
+	}
 
-            if (land[x][y] == 1 && !visited[x][y]) {
-                DFS(x, y);
-            }
-        }
-    }
+	static class Point {
+		int x, y;
+
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 }
