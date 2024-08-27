bbe6b42cebbb87b0907d4c4714a6b1d48f9e02d2
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-08-27 15:04:40 +0900

    [Gold V] Title: 적록색약, Time: 80 ms, Memory: 13100 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,69 +1,88 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class Main {
-    static int N;
-    static char[][] pictureO, pictureX; // O: 적록색약 O, X: 적록색약 X
-    static boolean[][] visited;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        pictureO = new char[N][N]; // R == G
-        pictureX = new char[N][N];
-        for (int i = 0; i < N; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < N; j++) {
-                pictureO[i][j] = line.charAt(j);
-                pictureX[i][j] = line.charAt(j);
-                if (pictureO[i][j] == 'G') pictureO[i][j] = 'R';
-            }
-        }
 
-        int totalO = 0;
-        visited = new boolean[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (!visited[i][j]) {
-                    totalO++;
-                    bfs(i, j, pictureO);
-                }
-            }
-        }
+	static int N;
+	static char[][] pictureO, pictureX; // pictureO: 적록색약 O, pictureX: 적록색약 X
+	static boolean[][] visited;
 
-        int totalX = 0;
-        visited = new boolean[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (!visited[i][j]) {
-                    totalX++;
-                    bfs(i, j, pictureX);
-                }
-            }
-        }
-        System.out.println(totalX + " " + totalO);
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		pictureO = new char[N][N];
+		pictureX = new char[N][N];
+		for (int i = 0; i < N; i++) {
+			String line = br.readLine();
+			for (int j = 0; j < N; j++) {
+				pictureX[i][j] = line.charAt(j);
+				// 적록색약인 사람의 그림 중 초록색을 빨간색으로 변경 ('R' = 'G')
+				pictureO[i][j] = pictureX[i][j] == 'G' ? 'R' : pictureX[i][j];
+			}
+		}
 
-    static void bfs(int x, int y, char[][] picture) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{x, y});
-        visited[x][y] = true;
+		// 적록색약 X
+		int area = 0;
+		visited = new boolean[N][N];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (!visited[i][j]) {
+					area++;
+					bfs(i, j, pictureX);
+				}
+			}
+		}
+		System.out.print(area + " ");
 
-        int[][] move = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
+		// 적록색약 O
+		area = 0;
+		visited = new boolean[N][N];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (!visited[i][j]) {
+					area++;
+					bfs(i, j, pictureO);
+				}
+			}
+		}
+		System.out.println(area);
+	}
 
-            for (int i = 0; i < 4; i++) {
-                int nx = now[0] + move[i][0];
-                int ny = now[1] + move[i][1];
-                if (nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny]) {
-                    continue;
-                }
+	static void bfs(int sx, int sy, char[][] picture) {
+		Queue<Point> queue = new ArrayDeque<>();
+		queue.offer(new Point(sx, sy));
+		visited[sx][sy] = true;
 
-                if (picture[nx][ny] == picture[x][y]) {
-                    queue.offer(new int[]{nx, ny});
-                    visited[nx][ny] = true;
-                }
-            }
-        }
-    }
+		int[] dx = { 1, -1, 0, 0 }, dy = { 0, 0, 1, -1 };
+		char c = picture[sx][sy];
+		while (!queue.isEmpty()) {
+			Point now = queue.poll();
+
+			for (int i = 0; i < 4; i++) {
+				int nx = now.x + dx[i];
+				int ny = now.y + dy[i];
+				// 범위를 벗어나는 경우
+				if (nx < 0 || ny < 0 || nx >= N || ny >= N)
+					continue;
+
+				// 이미 방문했거나 같은 구역이 아닌 경우
+				if (visited[nx][ny] || picture[nx][ny] != c)
+					continue;
+
+				queue.offer(new Point(nx, ny));
+				visited[nx][ny] = true;
+			}
+		}
+	}
+
+	static class Point {
+		int x, y;
+
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 }
