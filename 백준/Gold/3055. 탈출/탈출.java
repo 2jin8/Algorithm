82a2d30eceb0e4390dc2b00cac1ff30f013b72b3
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-08-29 16:09:34 +0900

    [Gold IV] Title: 탈출, Time: 72 ms, Memory: 11772 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -1,101 +1,104 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int r, c;
-    static char[][] forest;
-    static boolean[][] full; // 물 방문 여부 기록
-    static int[][] time; // 이동할 수 있는 시간 기록
-    static Queue<Point> water = new LinkedList<>(); // 물 이동 가능한 위치
-    static Queue<Point> animal = new LinkedList<>(); // 도치 이동 가능한 위치
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-        forest = new char[r][c];
-        full = new boolean[r][c];
-        time = new int[r][c];
-        Point start = null, end = null;
-        for (int i = 0; i < r; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < c; j++) {
-                forest[i][j] = str.charAt(j);
-                if (forest[i][j] == 'S') start = new Point(i, j);
-                else if (forest[i][j] == 'D') end = new Point(i, j);
-                else if (forest[i][j] == '*') {
-                    water.offer(new Point(i, j));
-                    full[i][j] = true;
-                }
-            }
-        }
-        int bfs = bfs(start, end);
-        if (bfs == -1) System.out.println("KAKTUS");
-        else System.out.println(bfs);
-    }
 
-    public static int bfs(Point start, Point end) {
-        animal.offer(start);
-        time[start.x][start.y] = 1;
+	static int R, C;
+	static char[][] map;
+	static boolean[][] waterVisited, dochiVisited; // 물 전용 방문 체크 배열
+	static Queue<Point> water = new ArrayDeque<>();
+	static Queue<Point> dochi = new ArrayDeque<>();
 
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        while (!animal.isEmpty()) {
-            // 물 이동
-            int water_size = water.size();
-            for (int i = 0; i < water_size; i++) {
-                Point point = water.poll();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		map = new char[R][C];
+		waterVisited = new boolean[R][C];
+		dochiVisited = new boolean[R][C];
 
-                for (int j = 0; j < 4; j++) {
-                    int tx = point.x + dx[j];
-                    int ty = point.y + dy[j];
+		Point goal = null;
+		for (int i = 0; i < R; i++) {
+			String line = br.readLine();
+			for (int j = 0; j < C; j++) {
+				map[i][j] = line.charAt(j);
+				if (map[i][j] == 'D') { // 비버의 굴
+					goal = new Point(i, j);
+				} else if (map[i][j] == 'S') { // 고슴도치
+					dochi.offer(new Point(i, j));
+					dochiVisited[i][j] = true;
+				} else if (map[i][j] == '*') { // 물
+					water.offer(new Point(i, j));
+					waterVisited[i][j] = true;
+				}
+			}
+		}
+		int result = dfs(goal);
+		System.out.println(result == -1 ? "KAKTUS" : result);
+	}
 
-                    if (tx < 0 || ty < 0 || tx >= r || ty >= c) // 범위 벗어나면 이동 불가
-                        continue;
+	static int dfs(Point goal) {
+		int[] dx = { 1, -1, 0, 0 }, dy = { 0, 0, 1, -1 };
+		
+		int time = 0;
+		while (!dochi.isEmpty()) { // 고슴도치 큐가 비면 -1 출력
+			// 물 이동
+			int size = water.size();
+			for (int i = 0; i < size; i++) {
+				Point now = water.poll();
 
-                    if (forest[tx][ty] == 'X' || forest[tx][ty] == 'D') // 벽이거나 굴이면 이동 불가
-                        continue;
+				for (int j = 0; j < 4; j++) {
+					int nx = now.x + dx[j];
+					int ny = now.y + dy[j];
+					// 범위 벗어나거나 이미 방문
+					if (nx < 0 || ny < 0 || nx >= R || ny >= C || waterVisited[nx][ny])
+						continue;
 
-                    if (!full[tx][ty]) {
-                        water.offer(new Point(tx, ty));
-                        full[tx][ty] = true;
-                    }
-                }
-            }
+					// 돌이 아니거나 비버 굴이 아니면 이동 가능
+					if (map[nx][ny] != 'X' && map[nx][ny] != 'D') {
+						water.offer(new Point(nx, ny));
+						waterVisited[nx][ny] = true;
+						map[nx][ny] = '*';
+					}
+				}
+			}
 
-            // 도치 이동
-            int animal_size = animal.size();
-            for (int i = 0; i < animal_size; i++) {
-                Point point = animal.poll();
-                if (point.x == end.x && point.y == end.y)
-                    return time[point.x][point.y] - 1;
+			size = dochi.size();
+			for (int i = 0; i < size; i++) {
+				Point now = dochi.poll();
+				if (now.x == goal.x && now.y == goal.y) { // 비버의 굴에 도착
+					return time;
+				}
 
-                for (int j = 0; j < 4; j++) {
-                    int tx = point.x + dx[j];
-                    int ty = point.y + dy[j];
-                    if (tx < 0 || ty < 0 || tx >= r || ty >= c) // 범위 벗어나면 이동 불가
-                        continue;
+				for (int j = 0; j < 4; j++) {
+					int nx = now.x + dx[j];
+					int ny = now.y + dy[j];
+					// 범위 벗어나거나 이미 방문한 경우
+					if (nx < 0 || ny < 0 || nx >= R || ny >= C || dochiVisited[nx][ny])
+						continue;
 
-                    if (forest[tx][ty] == 'X' || full[tx][ty]) // 벽이거나 물이 찼으면 이동 불가
-                        continue;
+					// 돌이 아니고 물이 없는 경우
+					if (map[nx][ny] != 'X' && map[nx][ny] != '*') {
+						dochi.offer(new Point(nx, ny));
+						dochiVisited[nx][ny] = true;
+					}
+				}
+			}
+			time++;
+		}
+		return -1;
+	}
 
-                    if ((forest[tx][ty] == '.' || forest[tx][ty] == 'D') && time[tx][ty] == 0) {
-                        animal.offer(new Point(tx, ty));
-                        time[tx][ty] = time[point.x][point.y] + 1;
-                    }
-                }
-            }
-        }
-        return -1;
-    }
-}
+	static class Point {
+		int x, y;
 
-class Point {
-    int x;
-    int y;
-
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 }
