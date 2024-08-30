345a553330c62903ded631d48a394e4fc1405bfc
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-08-30 14:09:37 +0900

    [D4] Title: 최소 스패닝 트리, Time: 3,542 ms, Memory: 207,600 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,87 +1,89 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
 
-	static int V, E; // V: 정점의 개수, E: 간선의 개수
-	static int[] parents;
+	static int V, E, MAX = 999_999_999;
+	static boolean[] visited; // 방문 확인용
+	static ArrayList<Vertex>[] graph; // 정점 연결 정보
+	static PriorityQueue<Vertex> pq; // 최소 가중치를 찾기 위한 우선순위 큐
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-
+		StringBuilder sb = new StringBuilder();
 		for (int t = 1; t <= T; t++) {
+
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			V = Integer.parseInt(st.nextToken());
 			E = Integer.parseInt(st.nextToken());
+			// 초기화
+			visited = new boolean[V + 1];
+			graph = new ArrayList[V + 1];
+			for (int i = 1; i <= V; i++) {
+				graph[i] = new ArrayList<>();
+			}
+			pq = new PriorityQueue<>();
 
-			make();
-			
-			Edge[] edges = new Edge[E];
+			// 정점의 정보 저장 (양방향)
 			for (int i = 0; i < E; i++) {
 				st = new StringTokenizer(br.readLine());
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
 				int c = Integer.parseInt(st.nextToken());
-				edges[i] = new Edge(a, b, c);
+				graph[a].add(new Vertex(b, c));
+				graph[b].add(new Vertex(a, c));
 			}
-			// 가중치를 기준으로 오름차순 정렬
-			Arrays.sort(edges);
 
-			long totalWeight = 0L, cnt = 0;
-			for (Edge edge : edges) {
-				// 정점 x, y가 다른 집합에 포함된 경우
-				if (union(edge.x, edge.y)) {
-					totalWeight += edge.weight;
-					if (++cnt == V - 1) break;
+			// 정점 정보 저장
+			pq.offer(new Vertex(1, 0)); // 1번 정점에서 시작
+			for (int i = 2; i <= V; i++) {
+				pq.offer(new Vertex(i, MAX));
+			}
+
+			long cost = 0; // cost: 최소 스패닝 트리의 가중치
+			for (int i = 0; i < V; i++) { // 정점의 수만큼 반복
+				// 최소 가중치를 가지는 노드 빼기
+				
+				Vertex vertex = null;
+				while (true) {
+					vertex = pq.poll();
+					if (!visited[vertex.idx]) break; 
+				}
+				
+				int min = vertex.weight, minIdx = vertex.idx;
+
+				// 방문 처리
+				visited[minIdx] = true;
+				cost += min;
+
+				// 가중치 업데이트
+				for (Vertex v : graph[minIdx]) {
+					// 방문하지 않은 노드 우선순위 큐에 넣기
+					if (!visited[v.idx]) {
+						pq.offer(v); // 우선순위 큐에 넣으면 자동 정렬 > 항상 가중치가 작은 것이 먼저 나옴
+					}
 				}
 			}
-			sb.append("#").append(t).append(" ").append(totalWeight).append("\n");
+			sb.append("#").append(t).append(" ").append(cost).append("\n");
 		}
 		System.out.println(sb);
 	}
 
-	static void make() {
-		parents = new int[V + 1];
-		for (int i = 1; i <= V; i++) {
-			parents[i] = -1;
-		}
-	}
+	static class Vertex implements Comparable<Vertex> {
+		int idx, weight;
 
-	static int findSet(int a) {
-		// 대표자 찾기(parents[a] >= 0: 대표자 번호, parents[a] < 0: 그룹 개수(=대표자를 뜻함))
-		if (parents[a] < 0) // 대표자를 찾은 경우
-			return a;
-
-		return parents[a] = findSet(parents[a]);
-	}
-
-	static boolean union(int a, int b) {
-		int aRoot = findSet(a);
-		int bRoot = findSet(b);
-		if (aRoot == bRoot)
-			return false;
-
-		parents[aRoot] += parents[bRoot]; // b의 자식 수를 a에 더하기
-		parents[bRoot] = aRoot; // 새로운 대표자 정보 기록
-		return true;
-	}
-
-	static class Edge implements Comparable<Edge> {
-		int x, y, weight;
-
-		public Edge(int x, int y, int weight) {
-			this.x = x;
-			this.y = y;
+		public Vertex(int idx, int weight) {
+			this.idx = idx;
 			this.weight = weight;
 		}
 
 		@Override
-		public int compareTo(Edge o) {
-			return Integer.compare(this.weight, o.weight);
+		public int compareTo(Vertex o) {
+			return this.weight - o.weight; // 가중치 기준 오름차순 정렬
 		}
 	}
 }
