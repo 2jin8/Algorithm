b5dbf93ba70ab566874c2caf599627450cf6391f
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-08-30 15:05:37 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 12064 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,53 +1,64 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static int[][] map, dist;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        // 최소의 칸 수 구하기 = BFS (단, )
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
-        dist = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < M; j++) {
-                map[i][j] = str.charAt(j) - '0';
-            }
-        }
-        System.out.println(bfs());
-    }
 
-    static int bfs() {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{0, 0});
-        dist[0][0] = 1; // 시작 위치와 도착 위치도 칸을 셀 때 포함하기
+	static int N, M;
+	static int[][] map;
 
-        int[][] move = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            int x = now[0], y = now[1];
-            if (x == N - 1 && y == M - 1) break;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new int[N + 1][M + 1];
+		for (int i = 1; i <= N; i++) {
+			String line = br.readLine();
+			for (int j = 1; j <= M; j++) {
+				map[i][j] = line.charAt(j - 1) - '0';
+			}
+		}
+		System.out.println(bfs());
+	}
 
-            // 네 방향으로 탐색하기
-            for (int i = 0; i < 4; i++) {
-                int nx = x + move[i][0];
-                int ny = y + move[i][1];
-                // 범위를 벗어나면 탐색 X
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
-                    continue;
-                }
+	static int bfs() {
+		Queue<Point> queue = new ArrayDeque<>();
+		queue.offer(new Point(1, 1));
+		map[1][1] = 2; // 방문 체크를 위해 2로 초기화 (나중에 거리에서 1을 빼줘야 함)
 
-                // 이미 방문하지 않았고 이동할 수 있는 칸(=1)인 경우
-                if (dist[nx][ny] == 0 && map[nx][ny] == 1) {
-                    queue.offer(new int[]{nx, ny});
-                    dist[nx][ny] = dist[x][y] + 1;
-                }
-            }
-        }
-        return dist[N - 1][M - 1];
-    }
+		int dist = -1;
+		int[] dx = { 1, -1, 0, 0 }, dy = { 0, 0, 1, -1 };
+		while (!queue.isEmpty()) {
+			Point now = queue.poll();
+			if (now.x == N && now.y == M) {
+				dist = map[now.x][now.y] - 1;
+				break;
+			}
+
+			for (int i = 0; i < 4; i++) {
+				int nx = now.x + dx[i];
+				int ny = now.y + dy[i];
+				if (nx <= 0 || ny <= 0 || nx > N || ny > M)
+					continue;
+
+				if (map[nx][ny] == 1) {
+					queue.offer(new Point(nx, ny));
+					map[nx][ny] = map[now.x][now.y] + 1;
+				}
+			}
+		}
+		return dist;
+	}
+
+	static class Point {
+		int x, y;
+
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 }
