f541eb604393393c4d584d54967e1fbcdeb4e7a3
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-09-01 18:08:07 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 135 ms, Memory: 24,568 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -2,28 +2,29 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 창용 마을 무리의 개수
 public class Solution {
-	static int N, M, group;
+
+	static int N, M;
 	static int[] parents;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
 		for (int t = 1; t <= T; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			M = Integer.parseInt(st.nextToken());
-			parents = new int[N + 1];
+			N = Integer.parseInt(st.nextToken()); // 마을 사람 수
+			M = Integer.parseInt(st.nextToken()); // 사람의 관계 수
 
-			group = N;
-			makeSet();
+			int group = N;
+			make();
 			for (int i = 0; i < M; i++) {
 				st = new StringTokenizer(br.readLine());
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
-				if (union(a, b)) { // 합집합 연산 성공
-					group--; // 그룹 수 감소
+				if (union(a, b)) { // 그룹이 만들어지는 경우
+					group--;
 				}
 			}
 			sb.append("#").append(t).append(" ").append(group).append("\n");
@@ -31,26 +32,27 @@ public class Solution {
 		System.out.println(sb);
 	}
 
-	static void makeSet() {
+	static void make() {
+		parents = new int[N + 1];
 		for (int i = 1; i <= N; i++) {
 			parents[i] = i;
 		}
 	}
 
-	static int findRoot(int x) {
-		if (x == parents[x])
-			return x;
+	static int findSet(int a) {
+		if (a == parents[a])
+			return a;
 
-		return parents[x] = findRoot(parents[x]);
+		return parents[a] = findSet(parents[a]);
 	}
 
 	static boolean union(int a, int b) {
-		int rootA = findRoot(a);
-		int rootB = findRoot(b);
-		if (rootA == rootB)
+		int aRoot = findSet(a);
+		int bRoot = findSet(b);
+		if (aRoot == bRoot) // 부모가 같은 경우
 			return false;
 
-		parents[rootB] = rootA; // b를 A에 연결
+		parents[bRoot] = aRoot; // 왼쪽 노드에 오른쪽 노드를 붙이기
 		return true;
 	}
 }
