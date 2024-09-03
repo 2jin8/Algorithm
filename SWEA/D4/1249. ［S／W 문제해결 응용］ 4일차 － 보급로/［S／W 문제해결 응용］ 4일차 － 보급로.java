1cb8ab93129c78820bd8999e006b56d3b41cf15b
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-09-03 14:32:37 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 229 ms, Memory: 30,224 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -1,11 +1,11 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 
+// BFS + PQ
 public class Solution {
 	static int N;
-	static int[][] arr, minDist;
+	static int[][] arr;
 	static boolean[][] visited;
 	static int[] dx = { 1, -1, 0, 0 }, dy = { 0, 0, 1, -1 };
 
@@ -17,7 +17,6 @@ public class Solution {
 			N = Integer.parseInt(br.readLine());
 
 			arr = new int[N][N];
-			minDist = new int[N][N];
 			visited = new boolean[N][N];
 			for (int i = 0; i < N; i++) {
 				char[] chars = br.readLine().toCharArray();
@@ -25,60 +24,43 @@ public class Solution {
 					arr[i][j] = chars[j] - '0';
 				}
 			}
-			sb.append("#").append(t).append(" ").append(getMinDist(0, 0, N - 1, N - 1)).append("\n");
+			sb.append("#").append(t).append(" ").append(bfs(0, 0, N - 1, N - 1)).append("\n");
 		}
 		System.out.println(sb);
 	}
 
-	static int getMinDist(int sx, int sy, int ex, int ey) {
-		final int INF = Integer.MAX_VALUE;
-		// 거리 배열 초기화
-		for (int i = 0; i < N; i++) {
-			Arrays.fill(minDist[i], INF);
-		}
-
-		PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2) -> Integer.compare(n1.w, n2.w));
-
-		minDist[sx][sy] = 0;
-		pq.offer(new Node(sx, sy, minDist[sx][sy]));
+	static int bfs(int sx, int sy, int ex, int ey) {
+		// 시간 기준 내림차순 정렬
+		PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2) -> Integer.compare(n1.time, n2.time));
+		pq.offer(new Node(0, 0, 0));
+		visited[0][0] = true;
 
 		while (!pq.isEmpty()) {
-			Node now = pq.poll();
+			Node now = pq.poll(); // 시간이 가장 짧은게 나옴
 			int x = now.x, y = now.y;
-			if (x == ex && y == ey)
-				return now.w;
+			if (x == N - 1 && y == N - 1)
+				return now.time;
 
-			// 이미 방문한 경우
-			if (visited[x][y])
-				continue;
-
-			// 방문 처리
-			visited[x][y] = true;
-
-			// 네 방향 탐색
 			for (int i = 0; i < 4; i++) {
 				int nx = x + dx[i];
 				int ny = y + dy[i];
 				if (nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny])
 					continue;
 
-				// 저장된 최단 경로보다 짧다면 갱신
-				if (minDist[nx][ny] > now.w + arr[nx][ny]) {
-					minDist[nx][ny] = now.w + arr[nx][ny];
-					pq.offer(new Node(nx, ny, minDist[nx][ny]));
-				}
+				pq.offer(new Node(nx, ny, now.time + arr[nx][ny]));
+				visited[nx][ny] = true;
 			}
 		}
 		return -1;
 	}
 
 	static class Node {
-		int x, y, w;
+		int x, y, time;
 
-		public Node(int x, int y, int w) {
+		public Node(int x, int y, int time) {
 			this.x = x;
 			this.y = y;
-			this.w = w;
+			this.time = time;
 		}
 	}
 }
