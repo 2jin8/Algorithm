d02dc3c30f412b833dcfd7f8e6019734e5f6c66e
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-09-03 13:55:07 +0900

    [Gold IV] Title: 최단경로, Time: 692 ms, Memory: 120884 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,72 +1,83 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int V, E, K;
-    static int[] d;
-    static final int INF = (int) 1e9;
-    static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        V = Integer.parseInt(st.nextToken()); // 1부터 시작
-        E = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(br.readLine()) - 1; // 시작 정점
-        d = new int[V];
-        Arrays.fill(d, INF);
-        for (int i = 0; i < V; i++) {
-            graph.add(new ArrayList<>());
-        }
 
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int u = Integer.parseInt(st.nextToken()) - 1;
-            int v = Integer.parseInt(st.nextToken()) - 1;
-            int w = Integer.parseInt(st.nextToken());
-            graph.get(u).add(new Node(v, w));
-        }
-        findRoute(K);
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < V; i++) {
-            if (d[i] == INF) sb.append("INF\n");
-            else sb.append(d[i]).append("\n");
-        }
-        System.out.println(sb);
-    }
+	static final int INF = Integer.MAX_VALUE;
+	static int V, E, K; // K: 시작 정점
+	static int[] minDist;
+	static boolean[] visited;
+	static ArrayList<Node>[] graph;
 
-    public static void findRoute(int start) {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(start, 0));
-        d[start] = 0;
-        while (!pq.isEmpty()) {
-            Node node = pq.poll();
-            int now = node.idx;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(br.readLine());
+		minDist = new int[V + 1];
+		visited = new boolean[V + 1];
+		graph = new ArrayList[V + 1];
+		for (int i = 1; i <= V; i++) {
+			graph[i] = new ArrayList<>();
+		}
 
-            // 기록된 최단 경로가 dist보다 짧다 → 최단 경로로 이미 갱신된 것
-            if (d[now] < node.dist) continue;
-            for (int i = 0; i < graph.get(now).size(); i++) {
-                int next = graph.get(now).get(i).idx;
-                int cost = d[now] + graph.get(now).get(i).dist;
-                if (cost < d[next]) {
-                    d[next] = cost;
-                    pq.offer(new Node(next, cost));
-                }
-            }
-        }
-    }
+		// 연결 정보
+		for (int i = 0; i < E; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			graph[u].add(new Node(v, w));
+		}
+		getMinDist();
 
-    static class Node implements Comparable<Node> {
-        private int idx;
-        private int dist;
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i <= V; i++) {
+			sb.append(minDist[i] == INF ? "INF" : minDist[i]).append("\n");
+		}
+		System.out.println(sb);
+	}
 
-        public Node(int idx, int dist) {
-            this.idx = idx;
-            this.dist = dist;
-        }
+	static void getMinDist() {
+		Arrays.fill(minDist, INF);
+		PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2) -> Integer.compare(n1.w, n2.w));
 
-        @Override
-        public int compareTo(Node n) {
-            return this.dist - n.dist;
-        }
-    }
+		// 시작 정점
+		minDist[K] = 0;
+		pq.offer(new Node(K, minDist[K]));
+
+		while (!pq.isEmpty()) {
+			Node now = pq.poll();
+
+			// 이미 방문한 정점
+			if (visited[now.v])
+				continue;
+			// 방문 처리
+			visited[now.v] = true;
+
+			for (Node n : graph[now.v]) {
+				if (visited[n.v])
+					continue;
+
+				if (minDist[n.v] > now.w + n.w) {
+					minDist[n.v] = now.w + n.w;
+					pq.offer(new Node(n.v, minDist[n.v]));
+				}
+			}
+		}
+	}
+
+	static class Node {
+		int v, w;
+
+		public Node(int v, int w) {
+			this.v = v;
+			this.w = w;
+		}
+	}
 }
