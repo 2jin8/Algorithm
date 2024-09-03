f02abcb3c7e222c7552621af888c68ed9b140601
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-09-03 17:17:19 +0900

    [Gold IV] Title: 회전 초밥, Time: 496 ms, Memory: 168580 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.java
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -24,40 +23,27 @@ public class Main {
 	}
 
 	static void getMaxKind() {
-		ArrayList<Integer> list = new ArrayList<>();
+		int kind = 0;
 
 		// 초기값 넣기
-		int kind = 1;
-		list.add(c);
-		visited[c]++;
 		for (int i = 0; i < k; i++) {
-			if (visited[arr[i]] == 0) {
+			if (visited[arr[i]]++ == 0)
 				kind++;
-			}
-			visited[arr[i]]++;
-			list.add(arr[i]);
 		}
 
-		int end = k - 1;
 		int maxKind = kind;
 		// 각 위치를 기준으로 연속한 k개의 접시 확인하기
-		for (int i = 1; i < N; i++) {
-			end++;
-			if (end == N)
-				end = 0;
-
-			int remove = list.remove(1);
-			visited[remove]--;
+		for (int i = 0; i < N; i++) {
 			// 제거한 값이 더 이상 list에 있지 않다면 1 감소
-			if (visited[remove] == 0)
+			if (--visited[arr[i]] == 0)
 				kind--;
 
-			list.add(arr[end]);
-			visited[arr[end]]++;
+			int end = (i + k) % N;
 			// 추가하려는 값이 처음으로 등록되는 것이면 1 증가
-			if (visited[arr[end]] == 1)
+			if (visited[arr[end]]++ == 0)
 				kind++;
-			maxKind = Math.max(maxKind, kind);
+
+			maxKind = Math.max(maxKind, visited[c] == 0 ? kind + 1 : kind);
 		}
 		System.out.println(maxKind);
 	}
