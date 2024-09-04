036b229b1a19dc5c52933ab46e83c51b42236a85
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-09-04 16:01:39 +0900

    [Silver III] Title: 2×n 타일링, Time: 60 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,17 +1,20 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    static final int MAX = 1_000;
-    static final int DIV = 10_007;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[MAX + 1];
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % DIV;
-        }
-        System.out.println(dp[n]);
-    }
+
+	static final int DIV = 10_007;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			if (i <= 3)
+				dp[i] = i;
+			else
+				dp[i] = (dp[i - 1] + dp[i - 2]) % DIV;
+		}
+		System.out.println(dp[N]);
+	}
 }
