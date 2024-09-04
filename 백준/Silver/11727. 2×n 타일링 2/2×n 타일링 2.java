3e7093325920d3e2b8dcc712a8bcbb58d0c595ba
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-09-04 17:06:56 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 60 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -1,16 +1,23 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    static final int DIV = 10_007;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            if (i == 1) dp[i] = 1;
-            else if (i == 2) dp[i] = 3;
-            else dp[i] = (dp[i - 1] + dp[i - 2] * 2) % DIV; // 2×2를 만드는 방법이 총 2개이므로
-        }
-        System.out.println(dp[n]);
-    }
+
+	static final int DIV = 10_007;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[N + 1];
+		dp[1] = 1;
+		for (int i = 2; i <= N; i++) {
+			if (i == 2)
+				dp[i] = 3;
+			else if (i == 3)
+				dp[i] = 5;
+			else
+				dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % DIV;
+		}
+		System.out.println(dp[N]);
+	}
 }
