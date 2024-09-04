533bbaa03c605506c8c462769f3f916b6ef789fc
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-09-04 09:29:53 +0900

    [D4] Title: 준환이의 양팔저울, Time: 846 ms, Memory: 20,508 KB -BaekjoonHub

--- SWEA/D4/3234. 준환이의 양팔저울/준환이의 양팔저울.java
+++ SWEA/D4/3234. 준환이의 양팔저울/준환이의 양팔저울.java
@@ -4,9 +4,9 @@ import java.util.StringTokenizer;
 
 public class Solution {
 
-	static int N, total;
-	static int[] weights, nums; // weights: 각 무게추의 무게를 저장하는 배열, nums: 순열을 저장하는 배열
-	static boolean[] used; // 무게추 사용 여부를 저장하는 배열
+	static int N, ans;
+	static int[] weights, nums;
+	static boolean[] used;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,55 +15,51 @@ public class Solution {
 
 		for (int t = 1; t <= T; t++) {
 			N = Integer.parseInt(br.readLine());
-			weights = new int[N];
+			weights = new int[N]; // 무게추의 무게
 			nums = new int[N];
 			used = new boolean[N];
-
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < N; i++) {
 				weights[i] = Integer.parseInt(st.nextToken());
 			}
 
-			total = 0;
+			ans = 0;
 			dfs(0);
-			sb.append("#").append(t).append(" ").append(total).append("\n");
+			sb.append("#").append(t).append(" ").append(ans).append("\n");
 		}
 		System.out.println(sb);
 	}
 
-    // 순열 만들기
-	private static void dfs(int depth) {
-		// 모든 추를 다 사용한 경우
+	// 무게추를 올리는 순서
+	static void dfs(int depth) {
 		if (depth == N) {
+			// 왼쪽/오른쪽 나눠서 올리기
 			leftOrRight(0, 0, 0);
 			return;
 		}
 
-		// 모든 무게 추를 양팔저울 위에 올리는 순서
 		for (int i = 0; i < N; i++) {
-			if (!used[i]) {
+			if (used[i])
+				continue;
 
-				used[i] = true; // 현재 추 사용 처리하기
-				nums[depth] = weights[i]; // 현재 값 기록하기
-				dfs(depth + 1);
-				used[i] = false; // 현재 추 사용 처리 취소하기
-			}
+			nums[depth] = weights[i];
+			used[i] = true;
+			dfs(depth + 1);
+			used[i] = false;
 		}
 	}
 
-    // 왼쪽 저울 또는 오른쪽 저울에 올리기
 	static void leftOrRight(int depth, int left, int right) {
+		// 오른쪽 무게 총합이 왼쪽 무게 총합보다 크면 안됨
+		if (left < right)
+			return;
+
 		if (depth == N) {
-			total++;
+			ans++;
 			return;
 		}
 
-		// 왼쪽 저울에 넣기
 		leftOrRight(depth + 1, left + nums[depth], right);
-
-		// 오른쪽 저울에 넣기
-		if (left >= right + nums[depth]) {
-			leftOrRight(depth + 1, left, right + nums[depth]);
-		}
+		leftOrRight(depth + 1, left, right + nums[depth]);
 	}
 }
