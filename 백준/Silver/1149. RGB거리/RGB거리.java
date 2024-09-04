8d9aa1c7f6ad68d22d09e2a3fc14d289874d1d34
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-09-04 17:18:07 +0900

    [Silver I] Title: RGB거리, Time: 72 ms, Memory: 12104 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,29 +1,34 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][3];
-        int[][] dp = new int[n][3];
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < 3; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-                dp[i][j] = arr[i][j];
-            }
-        }
 
-        for (int i = 1; i < n; i++) {
-            // 현재 집에 빨강을 칠하는 경우, 이전 집은 초록이나 파랑만 가능
-            dp[i][0] += Math.min(dp[i - 1][1], dp[i - 1][2]);
-            // 현재 집에 초록을 칠하는 경우, 이전 집은 빨강이나 파랑만 가능
-            dp[i][1] += Math.min(dp[i - 1][0], dp[i - 1][2]);
-            // 현재 집에 파랑을 칠하는 경우, 이전 집은 빨강이나 초록만 가능
-            dp[i][2] += Math.min(dp[i - 1][0], dp[i - 1][1]);
-        }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[N][3];
+		int[][] dp = new int[N][3];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 3; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				dp[i][j] = arr[i][j];
+			}
+		}
 
-        System.out.println(Math.min(dp[n - 1][0], Math.min(dp[n - 1][1], dp[n - 1][2])));
-    }
+		for (int i = 1; i < N; i++) {
+			// i번째 집을
+			// 빨강으로 칠할 때
+			dp[i][0] += Math.min(dp[i - 1][1], dp[i - 1][2]);
+
+			// 초록으로 칠할 때
+			dp[i][1] += Math.min(dp[i - 1][0], dp[i - 1][2]);
+
+			// 파랑으로 칠할 때
+			dp[i][2] += Math.min(dp[i - 1][0], dp[i - 1][1]);
+		}
+		int ans = Math.min(dp[N - 1][0], Math.min(dp[N - 1][1], dp[N - 1][2]));
+		System.out.println(ans);
+	}
 }
