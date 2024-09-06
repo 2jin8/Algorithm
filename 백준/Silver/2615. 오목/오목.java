7df3ea2712128e56cf7458dc92c7a8cfba353a96
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-09-06 13:53:11 +0900

    [Silver I] Title: 오목, Time: 68 ms, Memory: 11620 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.java
+++ 백준/Silver/2615. 오목/오목.java
@@ -1,58 +1,112 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
-    static final int N = 19;
-    static int[][] board = new int[N][N];
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // 검: 1, 흰: 2, 빈 칸: 0
-        // 검 win: 1, 흰 win: 2, 결정 x: 0
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < N; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+	static final int N = 19;
+	static int[][] map = new int[N][N];
 
-        int[][] move = {{1, 0}, {0, 1}, {1, 1}, {-1, 1}};
-        for (int j = 0; j < N; j++) {
-            for (int i = 0; i < N; i++) {
-                if (board[i][j] != 0) {
-                    for (int k = 0; k < 4; k++) {
-                        int x = i, y = j;
-                        int cnt = 1;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 
-                        // 한 방향으로 탐색하기
-                        while (true) {
-                            x += move[k][0];
-                            y += move[k][1];
-                            if (x < 0 || y < 0 || x >= N || y >= N || board[x][y] != board[i][j]) {
-                                break;
-                            }
-                            cnt++;
-                        }
+		for (int j = 0; j < N; j++) {
+			for (int i = 0; i < N; i++) {
+				if (map[i][j] != 0) {
+					if (checkRow(i, j, map[i][j]) || checkCol(i, j, map[i][j]) || checkLCross(i, j, map[i][j])
+							|| checkRCross(i, j, map[i][j])) {
+						System.out.println(map[i][j]);
+						System.out.println((i + 1) + " " + (j + 1));
+						return;
+					}
+				}
+			}
+		}
+		System.out.println(0); // 승부가 결정되지 않은 경우
+	}
 
-                        // 반대 방향으로 탐색하기
-                        x = i; y = j;
-                        while (true) {
-                            x -= move[k][0];
-                            y -= move[k][1];
-                            if (x < 0 || y < 0 || x >= N || y >= N || board[x][y] != board[i][j]) {
-                                break;
-                            }
-                            cnt++;
-                        }
-                        if (cnt == 5) { // 바둑알이 연속으로 다섯 개 놓인 경우
-                            System.out.println(board[i][j]);
-                            System.out.println((i + 1) + " " + (j + 1));
-                            return;
-                        }
-                    }
-                }
-            }
-        }
-        // 아직 승부가 결정되지 않은 경우
-        System.out.println(0);
-    }
+	static boolean checkRow(int x, int y, int bw) {
+		int cnt = 1; // (x, y)
+		// 왼쪽
+		for (int i = y - 1; i >= 0; i--) {
+			if (map[x][i] != bw)
+				break;
+
+			cnt++;
+		}
+
+		// 오른쪽
+		for (int i = y + 1; i < N; i++) {
+			if (map[x][i] != bw)
+				break;
+
+			cnt++;
+		}
+		return cnt == 5;
+	}
+
+	static boolean checkCol(int x, int y, int bw) {
+		int cnt = 1;
+		// 위
+		for (int i = x - 1; i >= 0; i--) {
+			if (map[i][y] != bw)
+				break;
+
+			cnt++;
+		}
+		// 아래
+		for (int i = x + 1; i < N; i++) {
+			if (map[i][y] != bw)
+				break;
+
+			cnt++;
+		}
+		return cnt == 5;
+	}
+
+	static boolean checkLCross(int x, int y, int bw) {
+		int cnt = 1;
+		// 좌상단
+		for (int i = x - 1, j = y - 1; i >= 0 && j >= 0; i--, j--) {
+			if (map[i][j] != bw)
+				break;
+
+			cnt++;
+		}
+		// 좌하단
+		for (int i = x + 1, j = y + 1; i < N && j < N; i++, j++) {
+			if (map[i][j] != bw)
+				break;
+
+			cnt++;
+		}
+		return cnt == 5;
+	}
+
+	static boolean checkRCross(int x, int y, int bw) {
+		int cnt = 1;
+		// 우상단
+		for (int i = x - 1, j = y + 1; i >= 0 && j < N; i--, j++) {
+			if (map[i][j] != bw)
+				break;
+
+			cnt++;
+		}
+
+		// 우하단
+		for (int i = x + 1, j = y - 1; i < N && j >= 0; i++, j--) {
+			if (map[i][j] != bw)
+				break;
+
+			cnt++;
+		}
+		return cnt == 5;
+	}
 }
