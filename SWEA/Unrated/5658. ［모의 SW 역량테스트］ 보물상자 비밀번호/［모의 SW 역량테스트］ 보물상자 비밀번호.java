67dab58dc9d14e9d52aa7451f20629670f3c40bc
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-09-06 14:42:56 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보물상자 비밀번호, Time: 190 ms, Memory: 26,488 KB -BaekjoonHub

--- SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
+++ SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
@@ -1,51 +1,65 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.StringTokenizer;
 
 public class Solution {
 
-    static int N, K;
-    static HashSet<String> password = new HashSet<>();
+	static int N, K, M;
+	static HashMap<Character, Integer> pwMap = new HashMap<>();
+	static HashSet<String> pwSet = new HashSet<>();
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st = null;
 
-        for (int t = 1; t <= T; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken());
-            password.clear();
+		init();
+		for (int t = 1; t <= T; t++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+			M = N / 4; //
+			String s = br.readLine();
+			s += s.substring(0, M);
+			pwSet.clear();
+			for (int i = 0; i < M; i++) { // 시작점
+				int start = i;
+				for (int j = 0; j < 4; j++) { // 4번
+					pwSet.add(s.substring(start, start + M));
+					start += M;
+				}
+			}
 
-            String box = br.readLine();
-            box += box;
-            int repeat = N / 4;
-            for (int i = 0; i < repeat; i++) {
-                for (int j = 0; j < 4; j++) {
-                    int start = i + repeat * j;
-                    password.add(box.substring(start, start + repeat));
-                }
-            }
-            List<String> pwList = password.stream().sorted(Collections.reverseOrder()).collect(Collectors.toList());
-            sb.append("#").append(t).append(" ").append(getNum(pwList.get(K - 1))).append("\n");
-        }
-        System.out.println(sb);
-    }
+			ArrayList<Integer> list = new ArrayList<>();
+			for (String pw : pwSet) {
+				char[] chars = pw.toCharArray();
+				int number = 0, mul = 0;
+				for (int i = chars.length - 1; i >= 0; i--) {
+					if (chars[i] - 'A' < 0) {
+						number += Math.pow(16, mul++) * (chars[i] - '0');
+					} else {
+						number += Math.pow(16, mul++) * (pwMap.get(chars[i]));
+					}
+				}
+				list.add(number);
+			}
+			Collections.sort(list, (i1, i2) -> Integer.compare(i2, i1));
+			sb.append("#").append(t).append(" ").append(list.get(K - 1)).append("\n");
+		}
+		System.out.println(sb);
+	}
 
-    static int getNum(String str) {
-        int total = 0, len = str.length() - 1;
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            int num = 0;
-            if (c - 'A' < 0) {
-                num = c - '0';
-            } else {
-                num = (c - 'A') + 10;
-            }
-            total += num * (int) Math.pow(16, len - i);
-        }
-        return total;
-    }
+	static void init() {
+		pwMap.put('A', 10);
+		pwMap.put('B', 11);
+		pwMap.put('C', 12);
+		pwMap.put('D', 13);
+		pwMap.put('E', 14);
+		pwMap.put('F', 15);
+	}
 }
