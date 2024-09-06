36242839a6e954d79fd230efc02c7d045f193761
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-09-06 13:07:51 +0900

    [D4] Title: [Professional] 키 순서, Time: 505 ms, Memory: 92,220 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -1,54 +1,52 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
 
 	static int N, M;
-	static boolean[] visited;
-	static int[] count; // 연결된 정점의 수를 저장하는 배열
-	static ArrayList<Integer>[] originList, reverseList; // 순방향/역방향 그래프
+	static int[][] map;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
 		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st = null;
+
 		for (int t = 1; t <= T; t++) {
 			N = Integer.parseInt(br.readLine());
 			M = Integer.parseInt(br.readLine());
-
-			originList = new ArrayList[N + 1];
-			reverseList = new ArrayList[N + 1];
-			for (int i = 1; i <= N; i++) {
-				originList[i] = new ArrayList<Integer>();
-				reverseList[i] = new ArrayList<Integer>();
-			}
-
+			map = new int[N + 1][N + 1];
 			for (int i = 0; i < M; i++) {
 				st = new StringTokenizer(br.readLine());
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
-				originList[a].add(b);
-				reverseList[b].add(a);
+				map[a][b] = 1;
 			}
 
-			// 각 정점에서 DFS 탐색 > 연결된 정점의 수 세기
-			visited = new boolean[N + 1];
-			count = new int[N + 1];
 			for (int i = 1; i <= N; i++) {
-				Arrays.fill(visited, false);
-				dfs(originList, i);
-				Arrays.fill(visited, false);
-				dfs(reverseList, i);
+				map[i][0] = -1;
+			}
+
+			for (int i = 1; i <= N; i++) {
+				// 해당 정점에서 이미 탐색을 완료한 경우라면 넘어가기
+				if (map[i][0] != -1)
+					continue;
+
+				dfs(i);
+			}
+
+			// 자신보다 키가 작은 사람의 수 구하기
+			for (int i = 1; i <= N; i++) {
+				for (int j = 1; j <= N; j++) {
+					map[0][j] += map[i][j];
+				}
 			}
 
 			int ans = 0;
-			for (int cnt : count) {
-				// 모든 정점과 연결되어 있다면 순서를 알 수 있음
-				if (cnt == N - 1)
+			for (int i = 1; i <= N; i++) {
+				// 자신보다 키가 작은 사람의 수 + 키가 큰 사람의 수가 N-1 이면 순서를 알 수 있음
+				if (map[i][0] + map[0][i] == N - 1)
 					ans++;
 			}
 			sb.append("#").append(t).append(" ").append(ans).append("\n");
@@ -56,17 +54,31 @@ public class Solution {
 		System.out.println(sb);
 	}
 
-	// 단순 DFS 탐색
-	static void dfs(ArrayList<Integer>[] list, int x) {
-		visited[x] = true;
-
-		for (int y : list[x]) {
-			if (visited[y])
+	static void dfs(int x) {
+		for (int i = 1; i <= N; i++) {
+			// 연결되어 있지 않은 정점이면 넘어가기
+			if (map[x][i] == 0)
 				continue;
 
-			// 연결된 정점의 수 1 증가
-			count[y]++;
-			dfs(list, y);
+			// 정점 i에서 탐색한 적이 없는 경우
+			if (map[i][0] == -1) {
+				dfs(i);
+			}
+
+			// 정점 i에서 갈 수 있는 경로가 1개 이상이면 해당 배열의 값을 현재 x에 더해주기
+			if (map[i][0] > 0) {
+				for (int j = 1; j <= N; j++) {
+					if (map[i][j] != 0) {
+						map[x][j] = 1;
+					}
+				}
+			}
+		}
+
+		// 연결된 정점 개수 구하기
+		map[x][0] = 0;
+		for (int j = 1; j <= N; j++) {
+			map[x][0] += map[x][j];
 		}
 	}
 }
