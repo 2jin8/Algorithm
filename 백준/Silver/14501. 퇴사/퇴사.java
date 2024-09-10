a5e1335cd5db6d9e62939e69cad2e5f48e4f9fa3
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-09-10 16:38:50 +0900

    [Silver III] Title: 퇴사, Time: 64 ms, Memory: 11620 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -4,25 +4,39 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+	// 완탐(부분집합)
+	static int N, ans;
+	static int[] time, pay;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[] time = new int[n + 1];
-		int[] pay = new int[n + 1];
-		int[] dp = new int[n + 1];
-
-		for (int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(br.readLine());
+		time = new int[N + 2];
+		pay = new int[N + 2];
+		StringTokenizer st = null;
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
 			time[i] = Integer.parseInt(st.nextToken());
 			pay[i] = Integer.parseInt(st.nextToken());
 		}
+		time[N + 1] = 1; // N+2일로 넘어가기 위해
+		dfs(1, 0);
+		System.out.println(ans);
+	}
 
-		for (int i = 0; i < n; i++) {
-			if (i + time[i] <= n) {
-				dp[i + time[i]] = Math.max(dp[i + time[i]], dp[i] + pay[i]);
-			}
-			dp[i + 1] = Math.max(dp[i], dp[i + 1]);
-		}
-		System.out.println(dp[n]);
+	static void dfs(int day, int totalPay) {
+		// 회사에 없는데 상담을 하려는 경우 종료
+		if (day > N + 1)
+			return;
+
+		ans = Math.max(ans, totalPay);
+		if (day == N + 1) // 마지막 날에 도달하면 종료
+			return;
+
+		// 현재 날짜 선택 O
+		dfs(day + time[day], totalPay + pay[day]);
+
+		// 현재 날짜 선택 X
+		dfs(day + 1, totalPay);
 	}
 }
