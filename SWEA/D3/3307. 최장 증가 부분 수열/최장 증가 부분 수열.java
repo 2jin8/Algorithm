ef5c3d0a121ff3e29c1421bf5afd7374bfc1b6c8
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-10-02 14:29:43 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 133 ms, Memory: 23,704 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
@@ -1,37 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
-import java.util.stream.IntStream;
+import java.util.StringTokenizer;
 
-class Solution
-{
-	static int[] dp;
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
+public class Solution {
+
+	static int N, MAX = Integer.MAX_VALUE;
+	static int[] arr, dp;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T=sc.nextInt();
+		StringTokenizer st = null;
+		int T = Integer.parseInt(br.readLine());
+		// 최장 부분 증가 수열
+		for (int t = 1; t <= T; t++) {
+			N = Integer.parseInt(br.readLine());
+			arr = new int[N];
+			st = new StringTokenizer(br.readLine());
+			dp = new int[N];
+			for (int i = 0; i < N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
 
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			sb.append("#").append(test_case).append(" ");
-			int n = sc.nextInt();
-			int[] ary = new int[n];
-			for (int i=0; i<n; i++) {
-				ary[i] = sc.nextInt();
-			}
-			
-			dp = new int[n]; // dp 테이블
-			Arrays.fill(dp, 1); // 자기 자신의 길이는 1
-			for (int i=1; i<n; i++) {
-				for (int j=0; j<i; j++) {
-					if (ary[i] > ary[j]) {
-						dp[i] = Math.max(dp[i], dp[j]+1);
-					}
+			Arrays.fill(dp, MAX);
+			dp[0] = arr[0];
+			for (int i = 1; i < N; i++) {
+				int idx = Arrays.binarySearch(dp, arr[i]);
+				if (idx < 0) {
+					idx = -1 * idx - 1;
 				}
+				dp[idx] = arr[i];
 			}
-			int max = IntStream.of(dp).max().getAsInt();
-			sb.append(max).append("\n");
+
+			int len = 0;
+			for (int i = 0; i < N; i++) {
+				if (dp[i] == MAX) // 값이 기록되지 않았으면 LIS 종료
+					break;
+				len++; // LIS 길이 세기
+			}
+			sb.append("#").append(t).append(" ").append(len).append("\n");
 		}
-		System.out.println(sb.toString());
-	}	
+		System.out.println(sb);
+	}
 }
