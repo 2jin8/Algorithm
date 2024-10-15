68422b2d8988131f635d43a83e64d1d1eddd6892
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-10-16 00:24:08 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 84 ms, Memory: 13188 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -5,41 +5,46 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-
-		int[] arr = new int[n];
-		StringTokenizer tokenizer = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(tokenizer.nextToken());
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
-		int[][] dp = new int[n][2];
-		for (int i = 0; i < n; i++) {
+		int[][] dp = new int[N][2]; // [][0]: 부분 수열의 길이, [][]: 부분 수열 중 이전 값의 인덱스
+		for (int i = 0; i < N; i++) {
 			dp[i][0] = 1;
-			dp[i][1] = -1;
+			dp[i][1] = i;
 		}
-		
-		int max = dp[0][0], maxIdx = 0;
-		for (int i = 1; i < n; i++) {
+
+		int max = 1, maxIdx = 0;
+		for (int i = 1; i < N; i++) {
 			for (int j = 0; j < i; j++) {
-				if (arr[i] > arr[j] && dp[i][0] <= dp[j][0]) {
-					dp[i][0] = dp[j][0] + 1;
-					dp[i][1] = j;
+				// 현재 값이 이전 값보다 증가한다면
+				if (arr[i] > arr[j]) {
+					if (dp[i][0] <= dp[j][0]) {
+						dp[i][0] = dp[j][0] + 1;
+						dp[i][1] = j;
+					}
 				}
 			}
-			if (max < dp[i][0]) {
+
+			if (dp[i][0] > max) {
 				max = dp[i][0];
 				maxIdx = i;
 			}
 		}
 
-		StringBuilder builder = new StringBuilder();
+		System.out.println(max);
+		StringBuilder sb = new StringBuilder();
 		int now = maxIdx;
-		while (now != -1) {
-			builder.insert(0, arr[now] + " ");
+		while (true) {
+			sb.insert(0, arr[now] + " ");
+			if (now == dp[now][1]) // 자기 자신을 가리킴 == 부분 수열의 첫 번째
+				break;
 			now = dp[now][1];
 		}
-		builder.insert(0, max + "\n"); 
-		System.out.println(builder);
+		System.out.println(sb);
 	}
 }
