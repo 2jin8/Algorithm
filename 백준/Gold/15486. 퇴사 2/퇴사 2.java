a5dcd6c055ce6fac57181faca64e3a49db09b389
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-10-17 23:21:21 +0900

    [Gold V] Title: 퇴사 2, Time: 620 ms, Memory: 314476 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,25 +1,33 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] time = new int[n + 1];
-        int[] pay = new int[n + 1];
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            time[i] = Integer.parseInt(st.nextToken());
-            pay[i] = Integer.parseInt(st.nextToken());
-        }
 
-        int[] dp = new int[n + 1];
-        for (int i = 0; i < n; i++) {
-            if (i + time[i] <= n) {
-                dp[i + time[i]] = Math.max(dp[i + time[i]], dp[i] + pay[i]);
-            }
-            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
-        }
-        System.out.println(dp[n]);
-    }
+	static int N, M;
+	static int[] time, pay, dp;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		M = N + 2;
+		time = new int[M];
+		pay = new int[M];
+		dp = new int[M];
+		for (int i = 1; i <= N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			time[i] = Integer.parseInt(st.nextToken());
+			pay[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = 1; i < M; i++) {
+			dp[i] = Math.max(dp[i], dp[i - 1]); // 이전까지의 최대 금액 vs 현재까지의 최대 금액
+
+			// 현재 회의를 선택할 수 있는 경우
+			if (i + time[i] < M) {
+				dp[i + time[i]] = Math.max(dp[i + time[i]], dp[i] + pay[i]);
+			}
+		}
+		System.out.println(dp[N + 1]);
+	}
 }
