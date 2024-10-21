357b8b295eb1065f038e355dcfa456e59d26fce8
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-10-21 17:30:07 +0900

    [Gold V] Title: 상범 빌딩, Time: 108 ms, Memory: 14608 KB -BaekjoonHub

--- 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
@@ -1,75 +1,84 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int L, R, C;
-    static char[][][] building;
-    static boolean[][][] visited;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        while (true) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            L = Integer.parseInt(st.nextToken());
-            R = Integer.parseInt(st.nextToken());
-            C = Integer.parseInt(st.nextToken());
-            if (L == 0 && R == 0 && C == 0) break;
 
-            building = new char[R][C][L];
-            visited = new boolean[R][C][L];
+	static int L, R, C;
+	static char[][][] building;
+	static boolean[][][] visited;
+	static int[] dx = { 1, -1, 0, 0, 0, 0 }, dy = { 0, 0, 1, -1, 0, 0 }, dz = { 0, 0, 0, 0, 1, -1 };
 
-            int x = 0, y = 0, z = 0;
-            for (int l = 0; l < L; l++) {
-                for (int r = 0; r < R; r++) {
-                    String line = br.readLine();
-                    for (int c = 0; c < C; c++) {
-                        building[r][c][l] = line.charAt(c);
-                        if (building[r][c][l] == 'S') { // 시작 위치
-                            x = r;
-                            y = c;
-                            z = l;
-                        }
-                    }
-                }
-                br.readLine();
-            }
-            int bfs = bfs(x, y, z);
-            if (bfs == -1) sb.append("Trapped!\n");
-            else sb.append("Escaped in ").append(bfs).append(" minute(s).\n");
-        }
-        System.out.println(sb.toString());
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		while (true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			L = Integer.parseInt(st.nextToken());
+			R = Integer.parseInt(st.nextToken());
+			C = Integer.parseInt(st.nextToken());
+			if (L == 0 && R == 0 && C == 0) break;
+			
+			building = new char[R][C][L];
+			visited = new boolean[R][C][L];
+			int sx = -1, sy = -1, sz = -1;
+			for (int k = 0; k < L; k++) {
+				for (int i = 0; i < R; i++) {
+					String line = br.readLine();
+					for (int j = 0; j < C; j++) {
+						building[i][j][k] = line.charAt(j);
+						if (building[i][j][k] == 'S') {
+							sx = i;
+							sy = j;
+							sz = k;
+						}
+					}
+				}
+				br.readLine();
+			}
+			int bfs = bfs(sx, sy, sz);
+			sb.append(bfs == -1 ? "Trapped!" : ("Escaped in " + bfs + " minute(s).")).append("\n");
+		}
+		System.out.println(sb);
+	}
 
-    public static int bfs(int x, int y, int z) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{x, y, z});
-        visited[x][y][z] = true;
+	static int bfs(int sx, int sy, int sz) {
+		Queue<Point> queue = new ArrayDeque<>();
+		queue.offer(new Point(sx, sy, sz, 0));
+		visited[sx][sy][sz] = true;
 
-        int[][][] dist = new int[R][C][L];
-        int[][] move = {{1, 0, 0}, {-1, 0, 0}, {0, 1, 0}, {0, -1, 0}, {0, 0, 1}, {0, 0, -1}};
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            x = now[0]; y = now[1]; z = now[2];
-            if (building[x][y][z] == 'E') { // 출구에 도착한 경우
-                return dist[x][y][z];
-            }
+		while (!queue.isEmpty()) {
+			Point now = queue.poll();
+			if (building[now.x][now.y][now.z] == 'E')
+				return now.dist;
 
-            for (int i = 0; i < 6; i++) {
-                int nx = x + move[i][0];
-                int ny= y + move[i][1];
-                int nz = z + move[i][2];
-                if (nx < 0 || ny < 0 || nz < 0 || nx >= R || ny >= C || nz >= L) {
-                    continue;
-                }
+			for (int i = 0; i < 6; i++) {
+				int nx = now.x + dx[i];
+				int ny = now.y + dy[i];
+				int nz = now.z + dz[i];
+				if (nx < 0 || ny < 0 || nz < 0 || nx >= R || ny >= C || nz >= L)
+					continue;
 
-                // 벽이 아니고 이미 방문한 지점이 아니면 이동 가능
-                if (building[nx][ny][nz] != '#' && !visited[nx][ny][nz]) {
-                    queue.offer(new int[]{nx, ny, nz});
-                    visited[nx][ny][nz] = true;
-                    dist[nx][ny][nz] = dist[x][y][z] + 1;
-                }
-            }
-        }
-        return -1;
-    }
+				if (visited[nx][ny][nz] || building[nx][ny][nz] == '#')
+					continue;
+
+				queue.offer(new Point(nx, ny, nz, now.dist + 1));
+				visited[nx][ny][nz] = true;
+			}
+		}
+		return -1;
+	}
+
+	static class Point {
+		int x, y, z, dist;
+
+		public Point(int x, int y, int z, int dist) {
+			this.x = x;
+			this.y = y;
+			this.z = z;
+			this.dist = dist;
+		}
+	}
 }
