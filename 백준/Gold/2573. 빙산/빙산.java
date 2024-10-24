e0341542b571ef12f082e616491e0ed6a07b8ebb
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-10-24 15:14:21 +0900

    [Gold IV] Title: 빙산, Time: 588 ms, Memory: 280560 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -1,83 +1,107 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static int[][] ice, tmpIce;
-    static boolean[][] visited;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        ice = new int[N][M];
-        tmpIce = new int[N][M]; // 다음 연도의 빙하 상태
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < M; j++) {
-                ice[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        System.out.println(melting());
-    }
 
-    static int melting() {
-        int year = 0;
-        while (true) {
-            visited = new boolean[N][M];
-            copyArray(ice, tmpIce);
-            int callBfs = 0; // bfs 메소드 호출 횟수 (두 번째 호출이라면 빙산이 쪼개진 것)
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (ice[i][j] != 0 && !visited[i][j]) {
-                        callBfs++;
-                        if (callBfs >= 2) return year;
+	static int N, M;
+	static int[][] arr;
+	static Queue<int[]> queue = new ArrayDeque<>();
+	static int[] dx = { 1, 0, -1, 0 }, dy = { 0, 1, 0, -1 };
 
-                        bfs(i, j);
-                    }
-                }
-            }
-            copyArray(tmpIce, ice);
-            year++;
-            if (callBfs == 0) return 0; // 전부 다 녹을 때까지 두 덩어리 이상으로 분리되지 않는 경우
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				if (arr[i][j] != 0) {
+					queue.offer(new int[] { i, j });
+				}
+			}
+		}
+		System.out.println(bfs());
+	}
 
-    static void bfs(int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{x, y});
-        visited[x][y] = true;
+	static int bfs() {
+		int year = 0;
+		while (!queue.isEmpty()) {
+			// 덩어리가 분리되었으면 return year
+			int[] peek = queue.peek();
+			if (!checkSeperate(peek[0], peek[1]))
+				return year;
 
-        int[][] move = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            x = now[0]; y = now[1];
+			year++;
+			int size = queue.size();
+			int[][] tmp = copyArray(arr);
+			for (int s = 0; s < size; s++) {
+				int[] now = queue.poll();
+				int x = now[0], y = now[1];
 
-            // 네 방향에 바다가 몇 개인지 세기
-            int sea = 0;
-            for (int i = 0; i < 4; i++) {
-                int nx = x + move[i][0];
-                int ny = y + move[i][1];
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
-                    continue;
-                }
+				// 네 방향 탐색 - 바다의 개수 세기
+				int sea = 0;
+				for (int i = 0; i < 4; i++) {
+					int nx = x + dx[i];
+					int ny = y + dy[i];
+					if (nx < 0 || ny < 0 || nx >= N || ny >= M)
+						continue;
 
-                if (ice[nx][ny] == 0) { // 바다인 경우
-                    sea++;
-                } else if (ice[nx][ny] != 0 && !visited[nx][ny]) { // 다음에 BFS 탐색을 할 빙산인 경우
-                    queue.offer(new int[]{nx, ny});
-                    visited[nx][ny] = true;
-                }
-            }
-            tmpIce[x][y] = Math.max(0, ice[x][y] - sea);
-        }
-    }
+					if (arr[nx][ny] == 0)
+						sea++;
+				}
 
-    static void copyArray(int[][] from, int[][] to) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                to[i][j] = from[i][j];
-            }
-        }
-    }
+				tmp[x][y] = Math.max(0, arr[x][y] - sea);
+				if (tmp[x][y] != 0) { // 아직 빙산이 남아있다면 큐에 넣기
+					queue.offer(new int[] { x, y });
+				}
+			}
+			// 변경된 정보 복사하기
+			arr = copyArray(tmp);
+		}
+		return 0;
+	}
+
+	static boolean checkSeperate(int x, int y) {
+		int area = 0;
+		Queue<int[]> tmpQ = new ArrayDeque<>();
+		tmpQ.offer(new int[] { x, y });
+		boolean[][] visited = new boolean[N][M];
+		visited[x][y] = true;
+
+		while (!tmpQ.isEmpty()) {
+			int[] now = tmpQ.poll();
+			area++;
+
+			for (int i = 0; i < 4; i++) {
+				int nx = now[0] + dx[i];
+				int ny = now[1] + dy[i];
+				if (nx < 0 || ny < 0 || nx >= N || ny >= M)
+					continue;
+
+				if (arr[nx][ny] == 0 || visited[nx][ny])
+					continue;
+
+				tmpQ.offer(new int[] { nx, ny });
+				visited[nx][ny] = true;
+			}
+		}
+		// 모든 빙산이 연결되어 있다면 큐에 저장된 크기와 같음
+		return area == queue.size();
+	}
+
+	static int[][] copyArray(int[][] arr) {
+		int[][] tmp = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				tmp[i][j] = arr[i][j];
+			}
+		}
+		return tmp;
+	}
 }
