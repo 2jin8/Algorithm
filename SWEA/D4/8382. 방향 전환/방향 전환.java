4c7a36f2ff7c523e45b7c3cfff26491ee6d97814
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-11-07 19:59:36 +0900

    [D4] Title: 방향 전환, Time: 408 ms, Memory: 87,896 KB -BaekjoonHub

--- SWEA/D4/8382. 방향 전환/방향 전환.java
+++ SWEA/D4/8382. 방향 전환/방향 전환.java
@@ -5,80 +5,68 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int N = 100, M = 200; // 음수값을 양수값으로 변경시키기 위함
-	static boolean[][][] visited;
-	static int[] move = { 1, -1 };
+
+	static final int N = 100, M = 201; // 음수 좌표 > 양수 좌표
+	static int sx, sy, ex, ey;
+	static int[][][] visited; // [][][0]: 가로 이동으로 시작, [][][1]: 세로 이동으로 시작
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// 시작이 가로냐 세로냐?
 		int T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = null;
-
 		for (int t = 1; t <= T; t++) {
 			st = new StringTokenizer(br.readLine());
-			int sx = Integer.parseInt(st.nextToken()) + N;
-			int sy = Integer.parseInt(st.nextToken()) + N;
-			int ex = Integer.parseInt(st.nextToken()) + N;
-			int ey = Integer.parseInt(st.nextToken()) + N;
+			sx = Integer.parseInt(st.nextToken()) + N;
+			sy = Integer.parseInt(st.nextToken()) + N;
+			ex = Integer.parseInt(st.nextToken()) + N;
+			ey = Integer.parseInt(st.nextToken()) + N;
 
-			visited = new boolean[2][M + 1][M + 1]; // [0]: 가로로 시작, [1][][]: 세로로 시작
-			sb.append("#").append(t).append(" ").append(bfs(sx, sy, ex, ey)).append("\n");
+			// 가로 이동
+			visited = new int[M][M][2];
+			int d1 = bfs(0);
+			visited = new int[M][M][2];
+			int d2 = bfs(1);
+			sb.append("#").append(t).append(" ").append(Math.min(d1, d2)).append("\n");
 		}
 		System.out.println(sb);
 	}
 
-	static int bfs(int sx, int sy, int ex, int ey) {
-		Queue<Point> queue = new ArrayDeque<>();
-		queue.offer(new Point(sx, sy, 0, 0, 0)); // 가로로 시작
-		visited[0][sx][sy] = true;
-		queue.offer(new Point(sx, sy, 1, 1, 0)); // 세로로 시작
-		visited[1][sx][sy] = true;
+	static int[][] moveLR = { { 0, 1 }, { 0, -1 } }, moveUD = { { 1, 0 }, { -1, 0 } };
 
+	static int bfs(int sd) {
+		Queue<Pos> queue = new ArrayDeque<>();
+		queue.offer(new Pos(sx, sy, sd));
+		visited[sx][sy][sd] = 1;
 		while (!queue.isEmpty()) {
-			Point now = queue.poll();
-			int x = now.x, y = now.y;
-			if (x == ex && y == ey) {
-				return now.dist;
-			}
+			Pos now = queue.poll();
+			int x = now.x, y = now.y, d = now.d;
+			if (x == ex && y == ey)
+				return visited[x][y][d] - 1;
 
-			if (now.d == 0) { // 가로 이동이었으면 지금은 세로 이동
-				for (int i = 0; i < 2; i++) {
-					int ny = y + move[i];
-					if (ny < 0 || ny > M)
-						continue;
-
-					if (!visited[now.start][x][ny]) {
-						queue.offer(new Point(x, ny, now.start, 1, now.dist + 1));
-						visited[now.start][x][ny] = true;
-					}
-				}
-			} else { // 세로 이동이었으면 지금은 가로 이동
-				for (int i = 0; i < 2; i++) {
-					int nx = x + move[i];
-					if (nx < 0 || nx > M)
-						continue;
-
-					if (!visited[now.start][nx][y]) {
-						queue.offer(new Point(nx, y, now.start, 0, now.dist + 1));
-						visited[now.start][nx][y] = true;
-					}
-				}
+			// 전에 가로로 이동(d = 0)했다면 세로로, 세로로 이동(d = 1)했다면 가로로 이동하기
+			int nd = (d == 0? 1 : 0);
+			int[][] move = (d == 0? moveUD : moveLR);
+			for (int i = 0; i < 2; i++) {
+				int nx = x + move[i][0];
+				int ny = y + move[i][1];
+				if (nx < 0 || ny < 0 || nx >= M || ny >= M || visited[nx][ny][nd] != 0)
+					continue;
+				
+				queue.offer(new Pos(nx, ny, nd));
+				visited[nx][ny][nd] = visited[x][y][d] + 1;
 			}
 		}
-		return -1;
+		return Integer.MAX_VALUE;
 	}
 
-	static class Point {
-		int x, y, start, d, dist;
+	static class Pos {
+		int x, y, d;
 
-		public Point(int x, int y, int start, int d, int dist) {
+		public Pos(int x, int y, int d) {
 			this.x = x;
 			this.y = y;
-			this.start = start;
 			this.d = d;
-			this.dist = dist;
 		}
 	}
 }
