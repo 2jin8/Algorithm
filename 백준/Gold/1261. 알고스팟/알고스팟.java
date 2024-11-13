fe1513aba599ceea812136c5e874e115548271e8
HyeonJin <78083320+2jin8@users.noreply.github.com>
2024-11-13 23:45:31 +0900

    [Gold IV] Title: 알고스팟, Time: 220 ms, Memory: 18656 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -1,69 +1,75 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
-    static int[][] board;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        m = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
-        board = new int[n][m];
-        // 벽 최소 몇 개 부수어야 하느냐 - 1일 때마다 수 카운팅 -> BFS 탐색
-        for (int i = 0; i < n; i++) {
-            String[] str = br.readLine().split("");
-            for (int j = 0; j < m; j++) {
-                board[i][j] = Integer.parseInt(str[j]);
-            }
-        }
-        System.out.println(bfs());
-    }
 
-    public static int bfs() {
-        // 벽을 부순 개수 기준으로 오름차순 정렬 ⇒ 벽을 부순 개수가 적은 것부터 poll
-        PriorityQueue<Point> pq = new PriorityQueue<>(); 
-        pq.offer(new Point(0, 0, 0));
-        boolean[][] visited = new boolean[n][m];
-        visited[0][0] = true;
+	static int N, M, INF = 10000;
+	static int[][] map, minCrash;
+	static boolean[][] visited;
+	static int[] dx = { 1, -1, 0, 0 }, dy = { 0, 0, 1, -1 };
 
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
-        while (!pq.isEmpty()) {
-            Point point = pq.poll();
-            if (point.x == n - 1 && point.y == m - 1) {
-                return point.cnt;
-            }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new int[M][N];
+		minCrash = new int[M][N];
+		visited = new boolean[M][N];
 
-            for (int i = 0; i < 4; i++) {
-                int tx = point.x + dx[i];
-                int ty = point.y + dy[i];
-                if (tx < 0 || ty < 0 || tx >= n || ty >= m)
-                    continue;
+		for (int i = 0; i < M; i++) {
+			char[] chars = br.readLine().toCharArray();
+			for (int j = 0; j < N; j++) {
+				map[i][j] = chars[j] - '0';
+			}
+		}
 
-                if (!visited[tx][ty]) {
-                    pq.offer(new Point(tx, ty, point.cnt + board[tx][ty]));
-                    visited[tx][ty] = true;
-                }
-            }
-        }
-        return -1;
-    }
-}
+		for (int i = 0; i < M; i++) {
+			Arrays.fill(minCrash[i], INF);
+		}
+		System.out.println(dijkstra(0, 0, M - 1, N - 1));
+	}
 
-class Point implements Comparable<Point> {
-    int x;
-    int y;
-    int cnt; // 벽을 부순 개수
+	static int dijkstra(int sx, int sy, int ex, int ey) {
+		PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2) -> Integer.compare(n1.crash, n2.crash));
+		pq.offer(new Node(sx, sy, 0));
+		minCrash[sx][sy] = 0;
 
-    public Point(int x, int y, int cnt) {
-        this.x = x;
-        this.y = y;
-        this.cnt = cnt;
-    }
+		while (!pq.isEmpty()) {
+			Node now = pq.poll();
+			int x = now.x, y = now.y;
+			if (x == ex && y == ey)
+				break;
 
-    @Override
-    public int compareTo(Point o) {
-        return cnt - o.cnt; // 오름차순 정렬
-    }
+			if (visited[x][y])
+				continue;
+			visited[x][y] = true;
+
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				if (nx < 0 || ny < 0 || nx >= M || ny >= N)
+					continue;
+
+				if (!visited[nx][ny] && minCrash[nx][ny] > minCrash[x][y] + map[nx][ny]) {
+					minCrash[nx][ny] = minCrash[x][y] + map[nx][ny];
+					pq.offer(new Node(nx, ny, minCrash[nx][ny]));
+				}
+			}
+		}
+		return minCrash[ex][ey];
+	}
+
+	static class Node {
+		int x, y, crash;
+
+		public Node(int x, int y, int crash) {
+			this.x = x;
+			this.y = y;
+			this.crash = crash;
+		}
+	}
 }
