403c9b3b16c6b2f2b67663bacdec420f9ae23a1d
hyeonjin <78083320+2jin8@users.noreply.github.com>
2024-11-18 09:38:23 +0900

    [Silver III] Title: N과 M (3), Time: 336 ms, Memory: 112016 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,33 +1,35 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+	static int N, M;
+	static int[] nums;
+	static StringBuilder sb = new StringBuilder();
 
-    static int n, m;
-    static int[] result; // 결과 저장 배열
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // 1~n까지 m개 고르기
-        n = sc.nextInt();
-        m = sc.nextInt();
+	// 중복 순열
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		nums = new int[M]; // 순열 기록
+		dfs(0);
+		System.out.println(sb);
+	}
 
-        result = new int[m];
-        dfs(0);
-        System.out.println(sb);
-    }
+	static void dfs(int depth) {
+		if (depth == M) {
+			for (int num : nums) {
+				sb.append(num).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
 
-    public static void dfs(int cnt) {
-        if (cnt == m) {
-            for (int r : result) {
-                sb.append(r).append(" ");
-            }
-            sb.append("\n");
-            return;
-        }
-
-        for (int i = 1; i <= n; i++) {
-            result[cnt] = i;
-            dfs(cnt + 1);
-        }
-    }
+		for (int i = 1; i <= N; i++) {
+			nums[depth] = i;
+			dfs(depth + 1);
+		}
+	}
 }
