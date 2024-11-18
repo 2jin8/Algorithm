1bf0624d615cf90ad6b3fa8c787b14058b9ffac2
hyeonjin <78083320+2jin8@users.noreply.github.com>
2024-11-18 23:27:11 +0900

    [Silver III] Title: 숫자 야구, Time: 68 ms, Memory: 11768 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.java
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -1,76 +1,92 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int answer = 0;
-    static Ask[] asks;
-    static boolean[] used = new boolean[10];
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        asks = new Ask[n];
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int number = Integer.parseInt(st.nextToken());
-            int strike = Integer.parseInt(st.nextToken());
-            int ball = Integer.parseInt(st.nextToken());
-            asks[i] = new Ask(number, strike, ball);
-        }
-        makeNumber(0, 0);
 
-        System.out.println(answer);
-    }
+	static int N, M = 3, K = 9, answerCnt;
+	static int[] nums;
+	static boolean[] used;
+	static Answer[] answers;
 
-    static void makeNumber(int depth, int total) {
-        if (depth == 3) {
-            // 주어진 질문과 비교했을 때 스트라이크, 볼의 개수가 모두 일치하는지 확인하기
-            if (check(String.valueOf(total))) answer++;
-            return;
-        }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		N = Integer.parseInt(br.readLine());
+		answers = new Answer[N];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			String questions = st.nextToken();
+			int strike = Integer.parseInt(st.nextToken());
+			int ball = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i <= 9; i++) {
-            if (!used[i]) { // 다른 숫자 세 개로 구성된 세자리수
-                used[i] = true;
-                makeNumber(depth + 1, total * 10 + i);
-                used[i] = false;
-            }
-        }
-    }
+			answers[i] = new Answer(questions, strike, ball);
+		}
 
-    static boolean check(String num) {
-        for (Ask ask : asks) {
-            int strike = 0, ball = 0;
-            String checkNum = String.valueOf(ask.number);
-            // 한 자리씩 확인하기
-            for (int i = 0; i < 3; i++) {
-                char c = num.charAt(i);
-                if (checkNum.charAt(i) == c) { // 스트라이크 확인
-                    strike++;
-                } else { // 볼 확인
-                    if (checkNum.contains(String.valueOf(c))) {
-                        ball++;
-                    }
-                }
-            }
-            if (strike != ask.strike || ball != ask.ball) {
-                return false;
-            }
-        }
-        return true;
-    }
+		nums = new int[M];
+		used = new boolean[K + 1];
+		dfs(0);
+		System.out.println(answerCnt);
+	}
 
+	static int checkQuestion() {
+		for (int i = 0; i < N; i++) {
+			Answer answer = answers[i];
+			boolean[] isStrike = new boolean[M];
+			int strike = 0, ball = 0;
 
-    static class Ask {
-        int number;
-        int strike;
-        int ball;
+			// strike 계산
+			for (int j = 0; j < M; j++) {
+				// 숫자가 포함되고 순서가 일치함 > 스트라이크
+				if (answer.questions.charAt(j) - '0' == nums[j]) {
+					strike++;
+					isStrike[j] = true;
+				}
+			}
 
-        Ask(int number, int strike, int ball) {
-            this.number = number;
-            this.strike = strike;
-            this.ball = ball;
-        }
-    }
+			// ball 계산
+			for (int j = 0; j < M; j++) {
+				int now = answer.questions.charAt(j) - '0';
+				for (int k = 0; k < M; k++) {
+					// 스트라이크한 번호가 아님 & 숫자가 포함됨 > 볼
+					if (!isStrike[j] && now == nums[k]) {
+						ball++;
+					}
+				}
+			}
+
+			// 하나라도 만족하지 않으면 가능성이 있는 답이 아님
+			if (strike != answer.strike || ball != answer.ball)
+				return 0;
+		}
+		return 1;
+	}
+
+	static void dfs(int depth) {
+		if (depth == M) {
+			answerCnt += checkQuestion();
+			return;
+		}
+
+		for (int i = 1; i <= K; i++) {
+			if (used[i])
+				continue;
+
+			used[i] = true;
+			nums[depth] = i;
+			dfs(depth + 1);
+			used[i] = false;
+		}
+	}
+
+	static class Answer {
+		String questions;
+		int strike, ball;
+
+		public Answer(String questions, int strike, int ball) {
+			this.questions = questions;
+			this.strike = strike;
+			this.ball = ball;
+		}
+	}
 }
