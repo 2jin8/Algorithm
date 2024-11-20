e6198acfb54a50422b3e287642a10c327f6e6d5e
hyeonjin <78083320+2jin8@users.noreply.github.com>
2024-11-20 22:29:57 +0900

    [Silver IV] Title: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데, Time: 176 ms, Memory: 16264 KB -BaekjoonHub

--- 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.java
+++ 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.java
@@ -1,32 +1,44 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        boolean[][] mixed = new boolean[n + 1][n + 1];
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            mixed[a][b] = true;
-            mixed[b][a] = true;
-        }
 
-        int total = 0;
-        for (int i = 1; i <= n - 2; i++) {
-            for (int j = i + 1; j <= n - 1; j++) {
-                if (mixed[i][j]) continue;
-                for (int k = j + 1; k <= n; k++) {
-                    if (mixed[i][k] || mixed[j][k]) continue;
-                    total++;
-                }
-            }
-        }
-        System.out.println(total);
-    }
+	static int N, M, cnt;
+	static int[] icecream;
+	static boolean[][] tasteInfo;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		icecream = new int[N + 1];
+		tasteInfo = new boolean[N + 1][N + 1];
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			tasteInfo[a][b] = true;
+			tasteInfo[b][a] = true;
+		}
+		dfs(0, 1);
+		System.out.println(cnt);
+	}
+
+	// 순서 중요 X > 조합
+	static void dfs(int depth, int start) {
+		if (depth == 3) {
+			int a = icecream[0], b = icecream[1], c = icecream[2];
+			if (!tasteInfo[a][b] && !tasteInfo[a][c] && !tasteInfo[b][c])
+				cnt++;
+			return;
+		}
+
+		for (int i = start; i <= N; i++) {
+			icecream[depth] = i;
+			dfs(depth + 1, i + 1);
+		}
+	}
 }
