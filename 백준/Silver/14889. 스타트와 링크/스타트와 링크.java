7a498640ca63bfee97ab5c9e12d46cccf777ab98
hyeonjin <78083320+2jin8@users.noreply.github.com>
2024-11-20 22:58:59 +0900

    [Silver I] Title: 스타트와 링크, Time: 220 ms, Memory: 35732 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -3,15 +3,13 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static int N, ans;
+	static int N, minDiff;
 	static int[][] arr;
 	static boolean[] used;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-
 		StringTokenizer st = null;
 		arr = new int[N][N];
 		used = new boolean[N];
@@ -21,43 +19,41 @@ public class Main {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
-		// n C n/2
-		ans = Integer.MAX_VALUE;
+		minDiff = Integer.MAX_VALUE;
 		dfs(0, 0);
-		System.out.println(ans);
+		System.out.println(minDiff);
 	}
 
+	static void getMinDiff() {
+		int team1Sum = 0, team2Sum = 0;
+		int[] team1 = new int[N / 2];
+		int[] team2 = new int[N / 2];
+		int idx1 = 0, idx2 = 0;
+		for (int i = 0; i < N; i++) {
+			// used[i]가 true면 team1, false면 team2
+			if (used[i]) team1[idx1++] = i;
+			else team2[idx2++] = i;
+		}
+
+		for (int i = 0; i < N / 2 - 1; i++) {
+			for (int j = i + 1; j < N / 2; j++) {
+				team1Sum += arr[team1[i]][team1[j]] + arr[team1[j]][team1[i]];
+				team2Sum += arr[team2[i]][team2[j]] + arr[team2[j]][team2[i]];
+			}
+		}
+		minDiff = Math.min(minDiff, Math.abs(team1Sum - team2Sum));
+	}
+
+	// 팀 구성하기
 	static void dfs(int depth, int start) {
 		if (depth == N / 2) {
-			int startScore = 0, linkScore = 0;
-			// 스타트 팀의 능력치 구하기 (used[i]가 true)
-			for (int i = 0; i < N - 1; i++) {
-				if (!used[i]) continue;
-				for (int j = i + 1; j < N; j++) {
-					if (!used[j]) continue;
-					startScore += arr[i][j] + arr[j][i];
-				}
-			}
-
-			// 링크 팀의 능력치 구하기 (used[i]가 false)
-			for (int i = 0; i < N - 1; i++) {
-				if (used[i]) continue;
-				for (int j = i + 1; j < N; j++) {
-					if (used[j]) continue;
-					linkScore += arr[i][j] + arr[j][i];
-				}
-			}
-			ans = Math.min(ans, Math.abs(startScore - linkScore));
+			getMinDiff();
 			return;
 		}
 
 		for (int i = start; i < N; i++) {
-			if (used[i])
-				continue;
-
 			used[i] = true;
-			dfs(depth + 1, i);
+			dfs(depth + 1, i + 1);
 			used[i] = false;
 		}
 	}
