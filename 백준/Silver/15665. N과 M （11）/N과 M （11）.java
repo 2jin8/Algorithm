2418a1ddc6976e9b3b979d1104824c79eafb9c0f
hyeonjin <78083320+2jin8@users.noreply.github.com>
2024-12-11 10:18:13 +0900

    [Silver II] Title: N과 M (11), Time: 808 ms, Memory: 127468 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N, M;
+	static int[] arr, record;
+	static StringBuilder sb = new StringBuilder();
+	static HashSet<String> answer = new LinkedHashSet<>(); 
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+
+		record = new int[M];
+		dfs(0);
+		
+		sb.setLength(0);
+		for (String a : answer) {
+			sb.append(a).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	// 중복 순열
+	static void dfs(int depth) {
+		if (depth == M) {
+			sb.setLength(0); 
+			for (int r : record) {
+				sb.append(r).append(" ");
+			}
+			answer.add(sb.toString());
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			record[depth] = arr[i];
+			dfs(depth + 1);
+		}
+	}
+}
