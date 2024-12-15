c5a3db9db75d3c108fcc6ccecf91e2b24449b5a6
hyeonjin <78083320+2jin8@users.noreply.github.com>
2024-12-16 00:22:42 +0900

    [Silver I] Title: IOIOI, Time: 160 ms, Memory: 26280 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -7,18 +7,19 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int M = Integer.parseInt(br.readLine());
-		String S = br.readLine();
-		String check = "IOI";
-		for (int i = 0; i < N - 1; i++) {
-			check += "OI";
-		}
-		
+		char[] chars = br.readLine().toCharArray();
+
 		int ans = 0;
-		int len = check.length();
-		for (int i = 0; i + len <= M; i++) {
-			if (S.substring(i, i + len).equals(check)) {
+		int[] check = new int[M]; // OI가 나온 개수를 저장하는 배열
+        // 처음이 OI로 시작하면 의미없으므로 i는 1부터 시작
+		for (int i = 1; i < M - 1; i++) {
+			if (chars[i] == 'O' && chars[i + 1] == 'I')
+				check[i + 1] = check[i - 1] + 1;
+
+			// OI가 N번 이상 나왔고 시작 문자가 I라면 포함된 것
+			// OI가 2번 > IOIOI (길이 5) => (i + 1) - (1 + 2 * N) - 1 = i - 2 * N + 1
+			if (check[i + 1] >= N && chars[i - 2 * N + 1] == 'I')
 				ans++;
-			}
 		}
 		System.out.println(ans);
 	}
