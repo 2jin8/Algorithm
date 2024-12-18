39ab05286147fc006c44e4d59d2fdff3bbba4940
hyeonjin <78083320+2jin8@users.noreply.github.com>
2024-12-18 18:18:50 +0900

    [Silver II] Title: 이동하기, Time: 368 ms, Memory: 76104 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -1,34 +1,30 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        int[][] board = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (i == 0 && j == 0) continue;
+	static int N, M;
+	static int[][] dp;
 
-                if (i == 0) { // 첫 번째 행은 ≫ 방향으로만 이동 가능
-                    board[i][j] += board[i][j - 1];
-                } else if (j == 0) { // 첫 번째 열은 ∨ 방향으로만 이동 가능
-                    board[i][j] += board[i - 1][j];
-                } else {
-                    board[i][j] += Math.max(board[i - 1][j - 1], Math.max(board[i][j - 1], board[i - 1][j]));
-                }
-            }
-        }
-        System.out.println(board[n - 1][m - 1]);
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		dp = new int[N + 1][M + 1];
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= M; j++) {
+				dp[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= M; j++) {
+				dp[i][j] += Math.max(dp[i - 1][j - 1], Math.max(dp[i - 1][j], dp[i][j - 1]));
+			}
+		}
+		System.out.println(dp[N][M]);
+	}
 }
