657f5cf53dcc1f30cfeb6f26a34f284cdaaf440d
hyeonjin <78083320+2jin8@users.noreply.github.com>
2024-12-19 14:42:56 +0900

    [Gold V] Title: 동전 2, Time: 80 ms, Memory: 11768 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,34 +12,26 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
-		
-		HashSet<Integer> coinSet = new HashSet<>();
+
+		int[] coins = new int[n];
 		for (int i = 0; i < n; i++) {
-			coinSet.add(Integer.parseInt(br.readLine()));
-		}
-		
-		int idx = 0;
-		int[] coins = new int[coinSet.size()];
-		for (int coin : coinSet) {
-			coins[idx++] = coin;
+			coins[i] = Integer.parseInt(br.readLine());
 		}
 		Arrays.sort(coins);
-		
+
 		int[] dp = new int[k + 1];
 		Arrays.fill(dp, INF);
 
-		idx = 0;
-		for (int i = coins[0]; i <= k; i++) {
-			// 현재 금액의 동전을 가지고 있는 경우
-			if (idx < coins.length && i == coins[idx]) {
-				dp[i] = 1;
-				idx++;
-			} else {
-				for (int j = 0; j < idx; j++) {
-					dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+		for (int i = 0; i < n; i++) {
+			int coin = coins[i];
+			for (int j = coin; j <= k; j++) {
+				if (coin == j) {
+					dp[j] = 1;
+				} else {
+					dp[j] = Math.min(dp[j], dp[j - coin] + 1);
 				}
 			}
 		}
-		System.out.println(dp[k] == INF? -1 : dp[k]);
+		System.out.println(dp[k] == INF ? -1 : dp[k]);
 	}
 }
