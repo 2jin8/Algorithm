46c288f679d79ee6b1f288082d027831a3bddf90
hyeonjin <78083320+2jin8@users.noreply.github.com>
2024-12-29 15:50:52 +0900

    [Silver II] Title: 나무 자르기, Time: 476 ms, Memory: 119368 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,41 +1,53 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static long M;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        M = Long.parseLong(st.nextToken());
-        int[] arr = new int[N];
-        st = new StringTokenizer(br.readLine(), " ");
-        int max = 0;
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            max = Math.max(max, arr[i]);
-        }
-        System.out.println(binarySearch(arr, 1, max));;
-    }
 
-    public static int binarySearch(int[] arr, int start, int end) {
-        int result = 0;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            long sum = 0;
-            for (int i = 0; i < N; i++) {
-                // 나무의 높이가 자르는 높이보다 큰 경우
-                if (arr[i] > mid) sum += arr[i] - mid;
-            }
+	static int N, M; // 적어도 M미터를 가져가야 함
+	static int[] trees;
 
-            if (sum < M) { // 가져가야 할 나무가 부족한 경우, 자르는 높이 낮추기
-                end = mid - 1;
-            } else { // 가져가야 할 나무가 충분하거나 같은 경우, 자르는 높이 높이기
-                result = mid;
-                start = mid + 1;
-            }
-        }
-        return result;
-    }
+	// 절단기에 설정할 수 있는 높이의 최댓값을 구해야 함
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		trees = new int[N];
+		st = new StringTokenizer(br.readLine());
+		int maxTree = 0;
+		for (int i = 0; i < N; i++) {
+			trees[i] = Integer.parseInt(st.nextToken());
+			maxTree = Math.max(maxTree, trees[i]);
+		}
+		System.out.println(findMaxCuttingHeight(maxTree));
+	}
+
+	static int findMaxCuttingHeight(int maxTree) {
+		int left = 1, right = maxTree;
+		int ans = 0;
+
+		while (left <= right) {
+			int mid = (left + right) / 2;
+
+			// M미터 이상의 나무를 가져갈 수 있으면 높이를 증가시키기
+			// 우측으로 이동
+			if (check(mid)) {
+				ans = mid;
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		return ans;
+	}
+
+	static boolean check(int height) {
+		long total = 0;
+		for (int tree : trees) {
+			if (tree > height)
+				total += tree - height;
+		}
+		return total >= M;
+	}
 }
