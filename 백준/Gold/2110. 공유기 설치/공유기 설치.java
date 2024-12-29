536b23518b9ca1b2f4ce646b6e8e3f4d0772bc91
hyeonjin <78083320+2jin8@users.noreply.github.com>
2024-12-29 16:39:17 +0900

    [Gold IV] Title: 공유기 설치, Time: 284 ms, Memory: 28736 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,49 +1,57 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, C;
-    static int[] home;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        home = new int[N];
-        for (int i = 0; i < N; i++) {
-            home[i] = Integer.parseInt(br.readLine());
-        }
-        Arrays.sort(home);
-        System.out.println(biSearch());
-    }
 
-    public static int biSearch() {
-        int left = 1, right = home[N - 1] - home[0];
-        int ans = 0;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            int cnt = getCount(mid);
-            if (cnt >= C) { // 설치한 공유기가 C보다 많거나 같은 경우
-                ans = mid;
-                left = mid + 1; // 공유기 사이의 거리를 넓혀서 설치하기(왼쪽으로 범위 좁히기)
-            } else { // 설치한 공유기가 C보다 적은 경우
-                right = mid - 1; // 공유기 사이의 거리를 좁혀서 설치하기(오른쪽으로 범위 좁히기)
-            }
-        }
-        return ans;
-    }
+	static int N, C;
+	static int[] houses;
 
-    public static int getCount(int diff) {
-        int prev = home[0]; // 제일 첫 번째 집에 설치
-        int cnt = 1; // 설치한 공유기의 개수
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		houses = new int[N];
+		for (int i = 0; i < N; i++) {
+			houses[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(houses);
 
-        for (int i = 1; i < N; i++) {
-            int now = home[i];
-            if (now - prev >= diff) { // 두 집 사이의 거리가 diff보다 크다면 공유기 설치 가능
-                prev = now;
-                cnt++;
-            }
-        }
-        return cnt;
-    }
+		System.out.println(findMaxDistance(houses[N - 1]));
+	}
+
+	static int findMaxDistance(int max) {
+		int left = 1, right = max;
+		int ans = 0;
+
+		while (left <= right) {
+			int mid = (left + right) / 2;
+
+			// 공유기를 C개 이상 설치할 수 있는 경우
+			// 인접한 두 공유기 사이의 거리를 늘려보기
+			// => 우측으로
+			if (check(mid)) {
+				ans = mid;
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		return ans;
+	}
+
+	static boolean check(int distance) {
+		int total = 1;
+		long before = houses[0];
+		for (int i = 1; i < N; i++) {
+			long house = houses[i];
+			if (house >= before + distance) {
+				before = house;
+				total++;
+			}
+		}
+		return total >= C;
+	}
 }
