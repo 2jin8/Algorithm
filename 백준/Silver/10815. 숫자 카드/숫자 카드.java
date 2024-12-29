07c1ac91b2ce54b013763dd8c9227568f6d17c38
hyeonjin <78083320+2jin8@users.noreply.github.com>
2024-12-29 16:16:15 +0900

    [Silver V] Title: 숫자 카드, Time: 828 ms, Memory: 137380 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,23 +1,30 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.StringTokenizer;
 
+// v2: Set
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        HashSet<Integer> set = new HashSet<>();
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < N; i++) {
-            set.add(Integer.parseInt(st.nextToken()));
-        }
 
-        int M = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine(), " ");
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < M; i++) {
-            boolean contains = set.contains(Integer.parseInt(st.nextToken()));
-            sb.append(contains ? 1 : 0).append(" ");
-        }
-        System.out.println(sb);
-    }
+	static int N, M;
+	static HashSet<Integer> cards = new HashSet<>();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			cards.add(Integer.parseInt(st.nextToken()));
+		}
+
+		M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < M; i++) {
+			int card = Integer.parseInt(st.nextToken());
+			sb.append(cards.contains(card) ? 1 : 0).append(" ");
+		}
+		System.out.println(sb);
+	}
 }
