8d7071ff2d9a91a8ab6e35cf928df8eee1b2c010
hyeonjin <78083320+2jin8@users.noreply.github.com>
2024-12-30 11:37:03 +0900

    [Silver II] Title: 예산, Time: 144 ms, Memory: 15112 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,40 +1,54 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static int[] arr;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int end = 0;
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            end = Math.max(end, arr[i]);
-        }
-        M = Integer.parseInt(br.readLine());
-        System.out.println(bSearch(0, end));
-    }
 
-    public static int bSearch(int start, int end) {
-        int result = 0;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            int money = 0;
-            for (int i = 0; i < N; i++) {
-                if (mid >= arr[i]) money += arr[i];
-                else money += mid;
-            }
-            if (money <= M) { // 오른쪽으로 범위 좁히기
-                result = mid;
-                start = mid + 1;
-            } else { // 왼쪽으로 범위 좁히기
-                end = mid - 1;
-            }
-        }
-        return result;
-    }
+	static int N, M;
+	static int[] budgets;
 
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		budgets = new int[N];
+		int maxBudget = 0;
+		for (int i = 0; i < N; i++) {
+			budgets[i] = Integer.parseInt(st.nextToken());
+			maxBudget = Math.max(maxBudget, budgets[i]);
+		}
+		M = Integer.parseInt(br.readLine()); // 총 예산
+		System.out.println(findMaxBudget(maxBudget));
+	}
+
+	static int findMaxBudget(int max) {
+		int left = 1, right = max;
+		int ans = 0;
+
+		while (left <= right) {
+			int mid = (left + right) / 2;
+
+			// 예산이 남는다면 예산을 늘려보기
+			// => 우측으로
+			if (check(mid)) {
+				ans = mid;
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		return ans;
+	}
+
+	static boolean check(int maxBudget) {
+		int total = 0;
+		for (int budget : budgets) {
+			// 상한액을 넘으면 상한액만큼만
+			if (budget > maxBudget)
+				total += maxBudget;
+			else
+				total += budget;
+		}
+		return total <= M; // 예산이 남으면 true, 부족하면 false
+	}
 }
