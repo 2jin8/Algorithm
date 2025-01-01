9fcbcd10a96327aa393e240b1742efead24d06b6
hyeonjin <78083320+2jin8@users.noreply.github.com>
2025-01-01 22:33:56 +0900

    [Silver IV] Title: 빙고, Time: 104 ms, Memory: 14060 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.java
+++ 백준/Silver/2578. 빙고/빙고.java
@@ -1,88 +1,85 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
-    static final int N = 5;
-    static boolean[][] check = new boolean[N][N];
-    static int[] nums = new int[N * N];
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        HashMap<Integer, String> hashMap = new HashMap<>();
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < N; j++) {
-                String point = i + "," + j;
-                hashMap.put(Integer.parseInt(st.nextToken()), point);
-            }
-        }
 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < N; j++) {
-                nums[i * N + j] = Integer.parseInt(st.nextToken());
-            }
-        }
+	static int N = 5;
+	static boolean[][] bingo;
+	static Pos[] pos;
 
-        for (int i = 0; i < 5; i++) {
-            StringTokenizer point = new StringTokenizer(hashMap.get(nums[i]), ",");
-            int x = Integer.parseInt(point.nextToken());
-            int y = Integer.parseInt(point.nextToken());
-            check[x][y] = true;
-        }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		pos = new Pos[N * N + 1];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				int num = Integer.parseInt(st.nextToken());
+				pos[num] = new Pos(i, j);
+			}
+		}
 
-        for (int i = 5; i < N * N; i++) {
-            StringTokenizer point = new StringTokenizer(hashMap.get(nums[i]), ",");
-            int x = Integer.parseInt(point.nextToken());
-            int y = Integer.parseInt(point.nextToken());
-            check[x][y] = true;
+		boolean complete = false;
+		bingo = new boolean[N][N];
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			if (complete) continue;
+			for (int j = 0; j < N; j++) {
+				int num = Integer.parseInt(st.nextToken());
+				Pos p = pos[num];
+				bingo[p.x][p.y] = true;
+				cnt++;
 
-            if (checkRow() + checkCol() + checkLeft()+checkRight() >= 3) {
-                System.out.println(i + 1);
-                return;
-            }
-        }
-    }
+				if (isBingo()) {
+					complete = true;
+					break;
+				}
+			}
+		}
+		System.out.println(cnt);
+	}
 
-    public static int checkRow() {
-        int line = 0;
-        for (int i = 0; i < N; i++) {
-            int tmp = 0;
-            for (int j = 0; j < N; j++) {
-                if (!check[i][j]) break;
-                tmp++;
-            }
-            if (tmp == N) line++;
-        }
-        return line;
-    }
+	static boolean isBingo() {
+		int cnt = 0;
+		// 가로, 세로 확인하기
+		for (int i = 0; i < N; i++) {
+			int row = 0, col = 0;
+			for (int j = 0; j < N; j++) {
+				// 수가 지워졌으면 ++
+				if (bingo[i][j]) row++;
 
-    public static int checkCol() {
-        int line = 0;
-        for (int j = 0; j < N; j++) {
-            int tmp = 0;
-            for (int i = 0; i < N; i++) {
-                if (!check[i][j]) break;
-                tmp++;
-            }
-            if (tmp == N) line++;
-        }
-        return line;
-    }
+				if (bingo[j][i]) col++;
+			}
 
-    public static int checkRight() {
-        for (int i = 0; i < N; i++) {
-            if (!check[i][N - 1 - i]) return 0;
-        }
-        return 1;
-    }
+			if (row == 5) cnt++;
+			if (col == 5) cnt++;
+		}
 
-    public static int checkLeft() {
-        for (int i = 0; i < N; i++) {
-            if (!check[i][i]) return 0;
-        }
-        return 1;
-    }
+		// 가로, 세로로 3줄이 그어졌으면 true
+		if (cnt >= 3) return true;
+
+		// 대각선 확인하기
+		int left = 0, right = 0;
+		for (int i = 0; i < N; i++) {
+			// 수가 지워졌으면 ++
+			if (bingo[i][i]) left++;
+			if (bingo[i][N - 1 - i]) right++;
+		}
+
+		if (left == 5) cnt++;
+		if (right == 5) cnt++;
+
+		return cnt >= 3;
+	}
+
+	static class Pos {
+		int x, y;
+
+		public Pos(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 }
