9ec23a601e5b30b004321d98a26c888f756e494e
hyeonjin <78083320+2jin8@users.noreply.github.com>
2025-01-20 00:57:51 +0900

    [Gold II] Title: 문제집, Time: 460 ms, Memory: 44348 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.java
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -33,61 +31,25 @@ public class Main {
 			graph[a].add(b);
 		}
 
-		Queue<Integer> queue = new ArrayDeque<>();
-		PriorityQueue<Problem> pq = new PriorityQueue<>((p1, p2) -> Integer.compare(p1.num, p2.num));
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
 		for (int i = 1; i <= N; i++) {
 			// 진입 차수가 0인 문제 번호 넣기
 			if (degree[i] == 0) {
-				queue.offer(i);
-				pq.offer(new Problem(0, i));
+				pq.offer(i);
 			}
 		}
 
-		solved[0] = true;
 		StringBuilder sb = new StringBuilder();
-		while (!queue.isEmpty() && !pq.isEmpty()) {
-			// 이미 뺀 것은 넘어가기
-			int p1 = queue.peek();
-			while (!queue.isEmpty() && solved[p1]) {
-				queue.poll();
-				p1 = queue.peek();
-			}
-			Problem p2 = pq.peek();
-			while (!pq.isEmpty() && solved[p2.num]) {
-				pq.poll();
-				p2 = pq.peek();
-			}
+		while (!pq.isEmpty()) {
+			int now = pq.poll();
+			sb.append(now).append(" ");
 			
-			int prev;
-			// 큐의 첫 번째 값보다 우선순위 큐의 값이 쉬운 문제 & 이전에 풀어야 하는 문제를 이미 다 푼 경우
-			if (p1 >= p2.num && solved[p2.prev]) {
-				pq.poll();
-				prev = p2.num;
-			} 
-			// 그렇지 않은 경우
-			else {
-				queue.poll();
-				prev = p1;
-			}
-			
-			solved[prev] = true;
-			sb.append(prev).append(" ");
-			for (int next : graph[prev]) {
+			for (int next : graph[now]) {
 				if (--degree[next] == 0) { // 진입차수 1개씩 줄이기 & 0이되면 큐에 넣기
-					queue.offer(next);
-					pq.offer(new Problem(p2.num, next));
+					pq.offer(next);
 				}
 			}
 		}
 		System.out.println(sb);
-	}
-	
-	static class Problem {
-		int prev, num;
-		
-		public Problem(int prev, int num) {
-			this.prev = prev;
-			this.num = num;
-		}
 	}
 }
