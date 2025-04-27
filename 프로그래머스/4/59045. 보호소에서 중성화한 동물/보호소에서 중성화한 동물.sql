1934f8e69d508d3e896bfb9e6efdd814dcbd2d1b
hyeonjin <78083320+2jin8@users.noreply.github.com>
2025-04-27 19:30:17 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,12 @@
+# 보호소에 들어올 당시에는 중성화되지 않음
+# 보호소를 나갈 때는 중성화됨
+# 아이디, 생물 종, 이름 조회
+# 아이디 순으로 조회
+
+SELECT A.ANIMAL_ID, A.ANIMAL_TYPE, A.NAME
+FROM ANIMAL_INS A
+JOIN ANIMAL_OUTS B
+ON A.ANIMAL_ID = B.ANIMAL_ID
+WHERE A.SEX_UPON_INTAKE LIKE 'Intact%' 
+AND (B.SEX_UPON_OUTCOME LIKE 'Spayed%' OR B.SEX_UPON_OUTCOME LIKE 'Neutered%')
+ORDER BY ANIMAL_ID;
