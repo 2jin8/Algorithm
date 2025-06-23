2d62066ed81fd8d7483ab5f54020f3b6adc2489a
hyeonjin <78083320+2jin8@users.noreply.github.com>
2025-06-23 13:25:16 +0900

    [Gold V] Title: 숫자고르기, Time: 108 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -1,57 +1,56 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class Main {
 
-	static int N;
-	static int[] arr;
-	static boolean[] done, visited;
-	static ArrayList<Integer> numberList = new ArrayList<>();
+    static int N;
+    static int[] arr;
+    static boolean[] done, checked;
+    static ArrayList<Integer> answer = new ArrayList<>();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N + 1];
+        done = new boolean[N + 1];
+        checked = new boolean[N + 1];
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            if (i == arr[i]) {
+                done[i] = true;
+                answer.add(i);
+            }
+        }
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		arr = new int[N + 1];
-		done = new boolean[N + 1];
-		visited = new boolean[N + 1];
-		for (int i = 1; i <= N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-			if (arr[i] == i) {
-				done[i] = true;
-				numberList.add(i);
-			}
-		}
+        for (int i = 1; i <= N; i++) {
+            // 이미 탐색 완료한 경우
+            if (done[i]) continue;
 
-		for (int i = 1; i <= N; i++) {
-			if (!done[i]) { // 아직 탐색하지 않은 경우
-				dfs(i);
-			}
-		}
+            dfs(i);
+        }
 
-		StringBuilder sb = new StringBuilder();
-		sb.append(numberList.size()).append("\n");
-		Collections.sort(numberList);
-		for (int number : numberList) {
-			sb.append(number).append("\n");
-		}
-		System.out.println(sb);
-	}
+        StringBuilder sb = new StringBuilder();
+        sb.append(answer.size()).append("\n");
+        answer.stream().sorted()
+                .forEach(a -> sb.append(a).append("\n"));
+        System.out.println(sb);
+    }
 
-	static void dfs(int x) {
-		// 이미 탐색을 완료했으면 탐색 종료
-		if (done[x])
-			return;
-		// 탐색을 완료하지 않았는데 이미 방문했으면 사이클에 포함됨
-		if (visited[x]) {
-			numberList.add(x);
-			done[x] = true;
-		}
+    static void dfs(int idx) {
+        // 이미 탐색한 경우
+        if (done[idx]) return;
 
-		visited[x] = true;
-		dfs(arr[x]); // 연결된 정수로 이동하기
-		visited[x] = false;
-		done[x] = true; // 사이클 포함 여부를 알았으므로 탐색 완료 처리
-	}
+        // 탐색이 완료되지 않았지만 이미 방문한 경우 => 사이클 발생
+        if (checked[idx]) {
+            // 집합에 넣기 & 탐색 완료 처리
+            answer.add(idx);
+            done[idx] = true;
+        }
+
+        checked[idx] = true;
+        dfs(arr[idx]);
+        checked[idx] = false;
+
+        done[idx] = true;
+    }
 }
