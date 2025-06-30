3c1572793001d26640cb22b4aa16cdbc856cc836
hyeonjin <78083320+2jin8@users.noreply.github.com>
2025-06-30 11:12:25 +0900

    [Gold IV] Title: 공유기 설치, Time: 268 ms, Memory: 29220 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -5,53 +5,56 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N, C;
-	static int[] houses;
+    // 가장 인접한 두 공유기 사이의 거리를 최대
+    // 그러니까 공유기 사이의 거리를 이분탐색으로 해야 할 듯
+    // 거리 정함 > 몇 개 공유기 설치해야 하는지 확인
+    // 공유기 설치 개수에 따라 이분탐색 범위 좁히기
+    static int N, C;
+    static int[] houses;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        // C개의 공유기를 N개의 집에 적절히 설치
+        N = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        houses = new int[N];
+        for (int i = 0; i < N; i++) {
+            houses[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(houses);
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		houses = new int[N];
-		for (int i = 0; i < N; i++) {
-			houses[i] = Integer.parseInt(br.readLine());
-		}
-		Arrays.sort(houses);
+        // 가장 인접한 두 공유기 사이의 최대 거리 설정 (즉, 공유기 사이의 최소 거리의 최댓값 찾기)
+        System.out.println(getMaxDistanceBetweenHouse());
+    }
 
-		System.out.println(findMaxDistance(houses[N - 1]));
-	}
+    static int getMaxDistanceBetweenHouse() {
+        int left = 1, right = houses[N - 1] - houses[0]; // 공유기 사이 거리는 최소 1
 
-	static int findMaxDistance(int max) {
-		int left = 1, right = max;
-		int ans = 0;
+        int maxDistance = 0;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            // 설치하는 공유기 수 구하기
+            // 설치 가능한 공유기 수가 C보다 적은 경우, 설치 간격 좁히기
+            if (getInstallCount(mid) < C) {
+                right = mid - 1;
+            } else { // 설치 간격 넓히기
+                maxDistance = mid;
+                left = mid + 1;
+            }
+        }
+        return maxDistance;
+    }
 
-		while (left <= right) {
-			int mid = (left + right) / 2;
-
-			// 공유기를 C개 이상 설치할 수 있는 경우
-			// 인접한 두 공유기 사이의 거리를 늘려보기
-			// => 우측으로
-			if (check(mid)) {
-				ans = mid;
-				left = mid + 1;
-			} else {
-				right = mid - 1;
-			}
-		}
-		return ans;
-	}
-
-	static boolean check(int distance) {
-		int total = 1;
-		long before = houses[0];
-		for (int i = 1; i < N; i++) {
-			long house = houses[i];
-			if (house >= before + distance) {
-				before = house;
-				total++;
-			}
-		}
-		return total >= C;
-	}
+    static int getInstallCount(int distance) {
+        // 우선 첫 번째 집에 무조건 설치
+        int prevHouse = houses[0], count = 1;
+        for (int i = 1; i < N; i++) {
+            // 집 사이의 거리가 distance 이상이여야 공유기 설치 가능
+            if (houses[i] - prevHouse >= distance) {
+                count++;
+                prevHouse = houses[i];
+            }
+        }
+        return count;
+    }
 }
