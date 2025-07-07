cb0378d56f8a74621d173ab6520ababf88e948c6
hyeonjin <78083320+2jin8@users.noreply.github.com>
2025-07-07 14:33:14 +0900

    [level 1] Title: 인기있는 아이스크림, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/133024. 인기있는 아이스크림/인기있는 아이스크림.sql
+++ 프로그래머스/1/133024. 인기있는 아이스크림/인기있는 아이스크림.sql
@@ -1,3 +1,3 @@
-# 총 주문량 기준 내림차순, 같다면 출하 번호 기준 오름차순 정렬
+# 총주문량 기준 내림차순 정렬, 총주문량이 같다면 출하 번호 기준 오름차순 정렬
 SELECT FLAVOR FROM FIRST_HALF
-    ORDER BY TOTAL_ORDER DESC, SHIPMENT_ID;
+ORDER BY TOTAL_ORDER DESC, SHIPMENT_ID;
