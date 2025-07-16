15a80874fdaaa6f77af4f7728876a83edcdc87f5
hyeonjin <78083320+2jin8@users.noreply.github.com>
2025-07-17 00:04:12 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 59.79 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,18 +1,16 @@
 import java.util.*;
 
 public class Solution {
-    
     public int[] solution(int []arr) {
         Stack<Integer> stack = new Stack<>();
-        for (int num : arr) {
-            // 스택이 비어있거나 제일 위의 값과 현재 원소가 같지 않다면 큐에 넣기
-            if (stack.isEmpty() || (!stack.isEmpty() && stack.peek() != num)) {
-                stack.push(num);
-            }
+        for (int a : arr) {
+            // 스택이 비어있거나 제일 마지막에 들어간 값과 다르면 넣기
+            if (stack.isEmpty() || stack.peek() != a)
+                stack.push(a);
         }
         
         int[] answer = new int[stack.size()];
-        for (int i = answer.length - 1; i >= 0; i--) {
+        for (int i=answer.length-1; i>=0; i--) {
             answer[i] = stack.pop();
         }
         return answer;
