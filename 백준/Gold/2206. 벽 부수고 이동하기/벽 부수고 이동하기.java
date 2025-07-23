951c013a7ca9cad12f2cf2354ad6917a84c26ac2
hyeonjin <78083320+2jin8@users.noreply.github.com>
2025-07-23 15:11:59 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 656 ms, Memory: 150320 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,79 +1,77 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
-    static int[][] map;
-    static boolean[][][] visited;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // 0: 이동 O, 1: 이동 X
-        // (1, 1)에서 (N, M)까지 최단 경로로 이동
-        // 벽을 한 개까지 부술 수 있음 ⇒ 상태가 도중에 변경될 수 있음
-        // 변할 수 있는 상태: 벽을 부순 경우 vs 벽을 부수지 않은 경우 → 총 2가지,,
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
-        visited = new boolean[n][m][2]; // [][][0]: 벽을 부수지 않은 경우, [][][1]: 벽을 부순 경우
-        for (int i = 0; i < n; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < m; j++) {
-                map[i][j] = line.charAt(j) - '0';
-            }
-        }
+	static int N, M;
+	static int[][] map;
+	static boolean[][][] visited; // 벽을 부수면 이동할 수 있는 곳이 달라짐 > 벽을 부쉈는지에 따라 구분해서 확인해야 함
 
-        System.out.println(bfs());
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-    static int bfs() {
-        Queue<Pos> queue = new LinkedList<>();
-        queue.offer(new Pos(0, 0, 1, false));
-        visited[0][0][0] = true;
+		map = new int[N + 1][M + 1];
+		visited = new boolean[N + 1][M + 1][2]; // [][][0]: 벽 부수기 X, [][][1]: 벽 부수개 O
+		for (int i = 1; i <= N; i++) {
+			String line = br.readLine();
+			for (int j = 1; j <= M; j++) {
+				map[i][j] = line.charAt(j - 1) - '0';
+			}
+		}
+		System.out.println(bfs());
+	}
 
-        int[][] move = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        while (!queue.isEmpty()) {
-            Pos pos = queue.poll();
-            if (pos.x == n - 1 && pos.y == m - 1) {
-                return pos.time;
-            }
+	static int bfs() {
+		Queue<Pos> queue = new LinkedList<>();
+		queue.offer(new Pos(1, 1, 1, 0));
+		visited[1][1][0] = true;
 
-            for (int i = 0; i < 4; i++) {
-                int nx = pos.x + move[i][0];
-                int ny = pos.y + move[i][1];
-                if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
-                    continue;
-                }
+		int[] dx = { 1, -1, 0, 0 };
+		int[] dy = { 0, 0, 1, -1 };
+		while (!queue.isEmpty()) {
+			Pos now = queue.poll();
+			if (now.x == N && now.y == M)
+				return now.dist;
 
-                if (pos.crashed) { // 이미 벽을 부순 경우
-                    if (map[nx][ny] == 0 && !visited[nx][ny][1]) { // 벽이 아닌 경우만 이동 가능
-                        queue.offer(new Pos(nx, ny, pos.time + 1, true));
-                        visited[nx][ny][1] = true;
-                    }
-                } else { // 벽을 부수지 않은 경우
-                    if (map[nx][ny] == 0 && !visited[nx][ny][0]) { // 이동할 칸이 벽이 아니면 그냥 이동
-                        queue.offer(new Pos(nx, ny, pos.time + 1, false));
-                        visited[nx][ny][0] = true;
-                    } else if (map[nx][ny] == 1 && !visited[nx][ny][1]) { // 이동한 칸이 벽이면 부숴보기
-                        queue.offer(new Pos(nx, ny, pos.time + 1, true));
-                        visited[nx][ny][1] = true;
-                    }
-                }
-            }
-        }
-        return -1;
-    }
+			// 네 방향 탐색
+			for (int i = 0; i < 4; i++) {
+				int nx = now.x + dx[i];
+				int ny = now.y + dy[i];
+				// 범위 벗어나면 탐색 X
+				if (nx <= 0 || ny <= 0 || nx > N || ny > M) {
+					continue;
+				}
 
-    static class Pos {
-        int x, y;
-        int time; // 소요 시간
-        boolean crashed; // 벽 부쉈는지 여부
+				// 이동하려는 곳이 벽 & 아직 벽을 부수지 않음 & 해당 지점을 방문하지 않음
+				if (map[nx][ny] == 1 && now.crash == 0 && !visited[nx][ny][1]) {
+					visited[nx][ny][1] = true;
+					queue.offer(new Pos(nx, ny, now.dist + 1, 1));
+				}
+				// 이동하려는 곳이 벽이 아님 & 해당 지점을 방문하지 않음
+				else if (map[nx][ny] == 0 && !visited[nx][ny][now.crash]) {
+					visited[nx][ny][now.crash] = true;
+					queue.offer(new Pos(nx, ny, now.dist + 1, now.crash));
+				}
+			}
+		}
+		return -1;
+	}
 
-        public Pos(int x, int y, int time, boolean crashed) {
-            this.x = x;
-            this.y = y;
-            this.time = time;
-            this.crashed = crashed;
-        }
-    }
+	static class Pos {
+		int x, y;
+		int dist;
+		int crash; // 0: 부수기 X, 1: 부수기 O
+
+		public Pos(int x, int y, int dist, int crash) {
+			this.x = x;
+			this.y = y;
+			this.dist = dist;
+			this.crash = crash;
+		}
+	}
 }
