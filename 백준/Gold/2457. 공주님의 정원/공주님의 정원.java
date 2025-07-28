a15160b0ffbf38f74ce5280dbb7cc8f4e870ce18
hyeonjin <78083320+2jin8@users.noreply.github.com>
2025-07-28 16:32:28 +0900

    [Gold III] Title: 공주님의 정원, Time: 536 ms, Memory: 52160 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
@@ -1,57 +1,68 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+import java.util.concurrent.Flow;
 
 public class Main {
+
+    static int N;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        Flower[] flowers = new Flower[n];
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(br.readLine());
+        Flower[] flowers = new Flower[N];
+        StringTokenizer st = null;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
             int startM = Integer.parseInt(st.nextToken());
             int startD = Integer.parseInt(st.nextToken());
+            int start = startM * 100 + startD;
+
             int endM = Integer.parseInt(st.nextToken());
             int endD = Integer.parseInt(st.nextToken());
-            int start = startM * 100 + startD;
             int end = endM * 100 + endD;
             flowers[i] = new Flower(start, end);
         }
         Arrays.sort(flowers, (f1, f2) -> {
             if (f1.start == f2.start)
-                return f2.end - f1.end; // 피는 날이 같다면 지는 날을 기준으로 내림차순 정렬
-            return f1.start - f2.start; // 피는 날을 기준으로 오름차순 정렬
+                return Integer.compare(f1.end, f2.end);
+            return Integer.compare(f1.start, f2.start);
         });
 
-        int startDay = 301, endDay = 1201;
-        int idx = 0, totalFlower = 0, maxEnd = 0;
-        while (startDay < endDay) { // 12월 1일 이후는 살펴볼 필요가 없음
-            boolean findFlower = false;
-
-            for (int i = idx; i < n; i++) {
-                // 현재 선택한 꽃이 지는 날보다 다음에 선택할 꽃이 피는 날이 늦는 경우
-                if (startDay < flowers[i].start) break; // 매일 꽃이 피어있을 수 없음
+        int start = 301, end = 1201;
+        int flowerCount = 0, idx = 0, maxEnd = 0;
+        while (start < end) {
+            boolean isFind = false;
+            for (int i = idx; i < N; i++) {
+                // 시작일보다 늦게 피면 의미 없음
+                Flower flower = flowers[i];
+                if (flower.start > start)
+                    break;
 
                 // 가장 늦게까지 피어있는 꽃 찾기
-                if (flowers[i].end > maxEnd) {
-                    maxEnd = flowers[i].end;
-                    idx = i + 1; // 현재 선택한 꽃 이후부터 탐색하도록
-                    findFlower = true;
+                if (flower.end > maxEnd) {
+                    maxEnd = flower.end;
+                    idx = i + 1; // 현재 꽃 다음부터 찾도록 인덱스 기록
+                    isFind = true;
                 }
             }
 
-            if (findFlower) { // 선택할 꽃을 찾았다면 값 갱신하기
-                startDay = maxEnd;
-                totalFlower++;
-            } else break; // 그렇지 않다면 종료하기
+            // 선택할 꽃을 찾은 경우
+            if (isFind) {
+                start = maxEnd;
+                flowerCount++;
+            } else { // 선택할 꽃을 찾기 못하면 종료
+                break;
+            }
         }
-        // 11월 30일까지 꽃이 피어있지 못하는 경우(지는 날이 12월 1일이어야 11월 30일까지 꽃이 피어있음)
-        if (maxEnd < endDay) System.out.println(0);
-        else System.out.println(totalFlower);
+        // 제일 늦게까지 피는 꽃이 12월 1일까지 피어있지 않으면 0 출력
+        System.out.println(maxEnd < end ? 0 : flowerCount);
     }
 
     static class Flower {
-        int start, end; // 피는 시간, 지는 시간
+        int start, end;
 
         public Flower(int start, int end) {
             this.start = start;
