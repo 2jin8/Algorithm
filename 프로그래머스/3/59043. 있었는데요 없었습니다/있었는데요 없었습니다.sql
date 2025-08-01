72fee74125dda3b947ebd505ac3df1485039af5a
hyeonjin <78083320+2jin8@users.noreply.github.com>
2025-08-01 10:51:22 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -0,0 +1,7 @@
+# 보호 시작일보다 입양일이 더 빠른 동물의 아이디와 이름을 조회하는 SQL
+# 보호 시작일이 빠른 순으로 조회
+SELECT A.ANIMAL_ID, A.NAME FROM ANIMAL_INS A
+JOIN ANIMAL_OUTS B
+ON A.ANIMAL_ID = B.ANIMAL_ID
+WHERE A.DATETIME > B.DATETIME # 보호 시작일: A.DATETIME, 입양일: B.DATETIME
+ORDER BY A.DATETIME;
