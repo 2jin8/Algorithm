64bc6996d67ee12edd2e1c32ff1755633672eb7f
hyeonjin <78083320+2jin8@users.noreply.github.com>
2025-08-05 12:47:50 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 536 ms, Memory: 129836 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
@@ -1,66 +1,59 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-	static int N, M;
-	static int[] orders, inDegree; // orders: 수강할 수 있는 학기, inDegree: 진입 차수
-	static ArrayList<Integer>[] graph;
+    static int N, M;
+    static int[] inDegree, time;
+    static ArrayList<Integer>[] graph;
+    // 최소 몇 학기에 이수할 수 있는지 == 내가 진입차수가 0이 되는게 언제인지
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 진입차수 세기
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        time = new int[N + 1];
+        inDegree = new int[N + 1];
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            inDegree[b]++;
+        }
 
-		orders = new int[N + 1];
-		inDegree = new int[N + 1];
-		graph = new ArrayList[N + 1];
-		for (int i = 1; i <= N; i++) {
-			graph[i] = new ArrayList<>();
-		}
+        // 진입차수가 0인 과목 큐에 넣기
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 1; i <= N; i++) {
+            if (inDegree[i] == 0) {
+                queue.offer(i);
+                time[i] = 1;
+            }
+        }
 
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			inDegree[b]++; // b의 진입차수(=선수과목 수) 증가
-			graph[a].add(b); // 선수과목 정보 저장
-		}
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
 
-		Queue<Integer> queue = new ArrayDeque<>();
-		for (int i = 1; i <= N; i++) {
-			// 선수과목이 없으면 큐에 넣기
-			if (inDegree[i] == 0) {
-				queue.offer(i);
-			}
-		}
+            for (int next : graph[now]) {
+                // 현재 과목과 연결된 간선 끊고, 진입차수가 0이 된다면 큐에 넣기
+                if (--inDegree[next] == 0) {
+                    queue.offer(next);
+                    time[next] = time[now] + 1;
+                }
+            }
+        }
 
-		int order = 1;
-		while (!queue.isEmpty()) {
-			int size = queue.size();
-			for (int i = 0; i < size; i++) {
-				int now = queue.poll();
-				orders[now] = order;
-
-				// 현재 과목과 연결된 과목 끊기 (선수과목 수강했으므로)
-				for (int next : graph[now]) {
-					if (--inDegree[next] == 0) {
-						queue.offer(next);
-					}
-				}
-			}
-			order++;
-		}
-
-		StringBuilder sb = new StringBuilder();
-		for (int i = 1; i <= N; i++) {
-			sb.append(orders[i]).append(" ");
-		}
-		System.out.println(sb);
-	}
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(time[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
 }
