234571bfd85ec1c09a998691d06dbd89700830fd
hyeonjin <78083320+2jin8@users.noreply.github.com>
2025-08-11 10:56:56 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2328 ms, Memory: 336548 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -5,81 +5,78 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N, M;
-	static int[] parents;
+    static int N, M;
+    static int[] parents;
+    static Edge[] edges;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-		Edge[] edges = new Edge[M];
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			edges[i] = new Edge(a, b, c);
-		}
+        // 연결된 간선 정보 저장
+        edges = new Edge[M];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            edges[i] = new Edge(a, b, c);
+        }
 
-		Arrays.sort(edges);
+        // 거리 기준 간선 정렬
+        Arrays.sort(edges, (e1, e2) -> Integer.compare(e1.dist, e2.dist));
 
-		make();
-		int weight = 0, group = N;
-		if (group == 2) { // 이미 두 개의 마을로 분할된 경우
-			System.out.println(0);
-			return;
-		}
-		
-		for (Edge edge : edges) {
-			// 대표자가 다르다면 그룹의 수 1개 감소 & 가중치 더하기
-			if (union(edge.a, edge.b)) {
-				weight += edge.w;
-				// 마을을 두 개로 분리하면 종료하기
-				if (--group == 2)
-					break;
-			}
-		}
-		System.out.println(weight);
-	}
+        // 자기 자신을 부모로 초기 설정
+        parents = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parents[i] = i;
+        }
 
-	static void make() {
-		parents = new int[N + 1];
-		for (int i = 1; i <= N; i++) {
-			parents[i] = i;
-		}
-	}
+        int edgeCount = 0, answer = 0, maxDist = 0;
+        for (int i = 0; i < M; i++) {
+            Edge edge = edges[i];
+            // 사이클이 발생하지 않으면 간선 연결하기
+            if (union(edge.a, edge.b)) {
+                answer += edge.dist;
+                maxDist = Math.max(maxDist, edge.dist);
 
-	static int findRoot(int a) {
-		if (a == parents[a])
-			return a;
+                // N-1개 간선 모두 연결하면 종료
+                if (++edgeCount == N - 1) {
+                    break;
+                }
+            }
+        }
+        // 두 개의 마을로 분할하기 위해 MST에서 가장 큰 거리를 가지는 간선 제거
+        System.out.println(answer- maxDist); 
+    }
 
-		return parents[a] = findRoot(parents[a]);
-	}
+    static int findRoot(int a) {
+        if (parents[a] == a)
+            return a;
 
-	static boolean union(int a, int b) {
-		int aRoot = findRoot(a);
-		int bRoot = findRoot(b);
-		if (aRoot == bRoot)
-			return false;
+        return parents[a] = findRoot(parents[a]);
+    }
 
-		parents[bRoot] = aRoot;
-		return true;
-	}
+    static boolean union(int a, int b) {
+        int aRoot = findRoot(a);
+        int bRoot = findRoot(b);
 
-	static class Edge implements Comparable<Edge> {
-		int a, b, w;
+        if (aRoot == bRoot)
+            return false;
 
-		public Edge(int a, int b, int w) {
-			this.a = a;
-			this.b = b;
-			this.w = w;
-		}
+        parents[bRoot] = aRoot;
+        return true;
+    }
 
-		@Override
-		public int compareTo(Edge o) {
-			return this.w - o.w; // 가중치 기준 오름차순 정렬
-		}
-	}
+    static class Edge {
+        int a, b, dist;
+
+        public Edge(int a, int b, int dist) {
+            this.a = a;
+            this.b = b;
+            this.dist = dist;
+        }
+    }
 }
