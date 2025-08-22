cd4741657ae7f42cc8a06ccbd9efa42e0860c538
hyeonjin <78083320+2jin8@users.noreply.github.com>
2025-08-23 01:10:38 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 144 ms, Memory: 17028 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
@@ -1,106 +1,110 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-// 가진 열쇠의 개수가 같더라도 
-// 가진 열쇠가 다르면 방문해도 됨
-// 비트마스킹으로 획득한 열쇠의 정보를 저장하기!
 public class Main {
 
-	static int N, M, KEY = 6, K = 64;
-	static char[][] map;
-	static boolean[][][] visited;
-	static int[] dx = { 1, -1, 0, 0 }, dy = { 0, 0, 1, -1 };
+    static int N, M;
+    static Pos start;
+    static ArrayList<Pos> endList = new ArrayList<>();
+    static char[][] map;
+    static int[][][] visited;
+    static int[] keyType = {1, 2, 4, 8, 16, 32}; // a: 1, b:2, ..., f:32
+    static int[] dx = {1, -1, 0, 0}, dy = {0, 0, 1, -1};
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		map = new char[N][M];
-		visited = new boolean[N][M][K];
-		int sx = -1, sy = -1;
-		for (int i = 0; i < N; i++) {
-			String line = br.readLine();
-			for (int j = 0; j < M; j++) {
-				map[i][j] = line.charAt(j);
-				if (map[i][j] == '0') {
-					sx = i;
-					sy = j;
-				}
-			}
-		}
-		System.out.println(bfs(sx, sy));
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new char[N][M];
+        visited = new int[N][M][64]; // 비트마스킹으로 현재 가진 열쇠 표현
 
-	static int bfs(int sx, int sy) {
-		Queue<Point> queue = new ArrayDeque<>();
-		queue.offer(new Point(sx, sy, 0, 0));
-		visited[sx][sy][0] = true;
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = line.charAt(j);
+                if (map[i][j] == '0') { // 현재 위치
+                    start = new Pos(i, j);
+                } else if (map[i][j] == '1') { // 출구
+                    endList.add(new Pos(i, j));
+                }
+            }
+        }
+        System.out.println(bfs());
+    }
 
-		while (!queue.isEmpty()) {
-			Point now = queue.poll();
-			int x = now.x, y = now.y, dist = now.dist;
-			if (map[x][y] == '1') { // 출구에 도착
-				return dist;
-			}
+    static int bfs() {
+        Queue<Pos> queue = new ArrayDeque<>();
+        queue.offer(new Pos(start.x, start.y, 0));
+        visited[start.x][start.y][0] = 1;
 
-			for (int i = 0; i < 4; i++) {
-				int nx = x + dx[i];
-				int ny = y + dy[i];
-				if (nx < 0 || ny < 0 || nx >= N || ny >= M)
-					continue;
+        while (!queue.isEmpty()) {
+            Pos now = queue.poll();
+            int x = now.x, y = now.y, key = now.key;
+            if (isEnd(x, y)) {
+                return visited[x][y][key] - 1;
+            }
 
-				if (map[nx][ny] == '#' || visited[nx][ny][now.key])
-					continue;
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                // 범위 벗어나면 X
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M)
+                    continue;
 
-				char next = map[nx][ny];
-				// 열쇠가 있는 경우
-				if (next >= 'a' && next <= 'f') {
-					int key = next - 'a';
-					// 해당 열쇠를 이미 획득한 경우, 그냥 방문하기
-					if ((now.key & (1 << key)) > 0) {
-						queue.offer(new Point(nx, ny, now.key, dist + 1));
-						visited[nx][ny][now.key] = true;
-					}
-					// 해당 열쇠를 획득한 적이 없는 경우
-					else {
-						int newKey = now.key | (1 << key); // 열쇠 획득
-						if (!visited[nx][ny][newKey]) {
-							queue.offer(new Point(nx, ny, newKey, dist + 1));
-							visited[nx][ny][newKey] = true;
-						}
-					}
-				}
-				// 문이 있는 경우
-				else if (next >= 'A' && next <= 'F') {
-					int door = next - 'A';
-					// 문을 열 수 있는 열쇠가 있는 경우
-					if ((now.key & (1 << door)) > 0) {
-						queue.offer(new Point(nx, ny, now.key, dist + 1));
-						visited[nx][ny][now.key] = true;
-					}
-				}
-				// 빈 칸 or 시작 위치 or 출구인 경우
-				else {
-					queue.offer(new Point(nx, ny, now.key, dist + 1));
-					visited[nx][ny][now.key] = true;
-				}
-			}
-		}
-		return -1;
-	}
+                // 벽이거나 이미 방문했으면 X
+                if (map[nx][ny] == '#' || visited[nx][ny][key] != 0)
+                    continue;
 
-	static class Point {
-		int x, y, key, dist; // key: 획득한 key 정보, dist: 이동 횟수
+                // 열쇠 집고 이동
+                if (map[nx][ny] >= 'a' && map[nx][ny] <= 'f') {
+                    int newKey = key | keyType[map[nx][ny] - 'a'];
+                    if (visited[nx][ny][newKey] == 0) {
+                        visited[nx][ny][newKey] = visited[x][y][key] + 1;
+                        queue.offer(new Pos(nx, ny, newKey));
+                    }
+                }
+                // 문 -> 열쇠 확인
+                else if (map[nx][ny] >= 'A' && map[nx][ny] <= 'F') {
+                    if ((now.key & keyType[map[nx][ny] - 'A']) > 0) {
+                        queue.offer(new Pos(nx, ny, key));
+                        visited[nx][ny][key] = visited[x][y][key] + 1;
+                    }
+                }
+                // 나머지 칸은 그냥 이동
+                else {
+                    queue.offer(new Pos(nx, ny, key));
+                    visited[nx][ny][key] = visited[x][y][key] + 1;
+                }
+            }
+        }
+        return -1;
+    }
 
-		public Point(int x, int y, int key, int dist) {
-			this.x = x;
-			this.y = y;
-			this.key = key;
-			this.dist = dist;
-		}
-	}
+    // 출구는 적어도 한 개 == 여러 개 존재
+    static boolean isEnd(int x, int y) {
+        for (Pos pos : endList) {
+            if (pos.x == x && pos.y == y)
+                return  true;
+        }
+        return false;
+    }
+
+    static class Pos {
+        int x, y, key;
+
+        public Pos(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Pos(int x, int y, int key) {
+            this(x, y);
+            this.key = key;
+        }
+    }
 }
