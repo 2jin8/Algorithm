3769e99fc05c9401e20e6227c235a301a6737367
hyeonjin <78083320+2jin8@users.noreply.github.com>
2025-09-10 22:58:11 +0900

    [Gold IV] Title: 알고스팟, Time: 128 ms, Memory: 14972 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -6,70 +6,74 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N, M, INF = 10000;
-	static int[][] map, minCrash;
-	static boolean[][] visited;
-	static int[] dx = { 1, -1, 0, 0 }, dy = { 0, 0, 1, -1 };
+    static int N, M;
+    static int[][] map, minDist;
+    static boolean[][] visited;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		map = new int[M][N];
-		minCrash = new int[M][N];
-		visited = new boolean[M][N];
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        minDist = new int[N][M];
+        visited = new boolean[N][M];
 
-		for (int i = 0; i < M; i++) {
-			char[] chars = br.readLine().toCharArray();
-			for (int j = 0; j < N; j++) {
-				map[i][j] = chars[j] - '0';
-			}
-		}
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = line.charAt(j) - '0';
+            }
+            Arrays.fill(minDist[i], Integer.MAX_VALUE);
+        }
 
-		for (int i = 0; i < M; i++) {
-			Arrays.fill(minCrash[i], INF);
-		}
-		System.out.println(dijkstra(0, 0, M - 1, N - 1));
-	}
+        System.out.println(dijkstra(0, 0));
+    }
 
-	static int dijkstra(int sx, int sy, int ex, int ey) {
-		PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2) -> Integer.compare(n1.crash, n2.crash));
-		pq.offer(new Node(sx, sy, 0));
-		minCrash[sx][sy] = 0;
+    static int[] dx = {1, -1, 0, 0}, dy = {0, 0, 1, -1};
 
-		while (!pq.isEmpty()) {
-			Node now = pq.poll();
-			int x = now.x, y = now.y;
-			if (x == ex && y == ey)
-				break;
+    static int dijkstra(int x, int y) {
+        // 벽을 부순 개수를 기준으로 우선순위 큐 정렬
+        PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2) -> Integer.compare(n1.crash, n2.crash));
+        pq.offer(new Node(x, y, 0));
+        minDist[x][y] = 0;
 
-			if (visited[x][y])
-				continue;
-			visited[x][y] = true;
+        int crash = 0;
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
+            // 도착 지점에 도달한 경우
+            if (now.x == N - 1 && now.y == M - 1) {
+                crash = now.crash;
+                break;
+            }
 
-			for (int i = 0; i < 4; i++) {
-				int nx = x + dx[i];
-				int ny = y + dy[i];
-				if (nx < 0 || ny < 0 || nx >= M || ny >= N)
-					continue;
+            if (visited[now.x][now.y]) continue;
+            visited[now.x][now.y] = true;
 
-				if (!visited[nx][ny] && minCrash[nx][ny] > minCrash[x][y] + map[nx][ny]) {
-					minCrash[nx][ny] = minCrash[x][y] + map[nx][ny];
-					pq.offer(new Node(nx, ny, minCrash[nx][ny]));
-				}
-			}
-		}
-		return minCrash[ex][ey];
-	}
+            // 주변 네 칸으로 이동 가능
+            for (int i = 0; i < 4; i++) {
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny])
+                    continue;
 
-	static class Node {
-		int x, y, crash;
+                // 더 짧은 최단거리(부순 벽의 개수)가 존재하는 경우 업데이트 후 PQ에 넣기
+                if (minDist[now.x][now.y] + map[nx][ny] < minDist[nx][ny]) {
+                    minDist[nx][ny] = minDist[now.x][now.y] + map[nx][ny];
+                    pq.offer(new Node(nx, ny, minDist[nx][ny]));
+                }
+            }
+        }
+        return crash;
+    }
 
-		public Node(int x, int y, int crash) {
-			this.x = x;
-			this.y = y;
-			this.crash = crash;
-		}
-	}
+    static class Node {
+        int x, y, crash; // crash: 부순 벽의 개수
+
+        public Node(int x, int y, int crash) {
+            this.x = x;
+            this.y = y;
+            this.crash = crash;
+        }
+    }
 }
