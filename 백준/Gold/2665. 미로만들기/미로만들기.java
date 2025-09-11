31ddc45deac9260dbebc0d618ce9be06d772e412
hyeonjin <78083320+2jin8@users.noreply.github.com>
2025-09-11 22:21:40 +0900

    [Gold IV] Title: 미로만들기, Time: 108 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.java
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -1,75 +1,70 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 
-/*
-    다익스트라 풀이
-*/
 public class Main {
+    static int n;
+    static int[][] map;
+    static boolean[][] visited;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+        visited = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < n; j++) {
+                map[i][j] = str.charAt(j) - '0';
+            }
+        }
+        System.out.println(bfs());
+    }
 
-	static int N, INF = 2500;
-	static int[][] map, minDist;
-	static boolean[][] visited;
-	static int[] dx = { 1, -1, 0, 0 }, dy = { 0, 0, 1, -1 };
+    public static int bfs() {
+        PriorityQueue<Point> pq = new PriorityQueue<>();
+        pq.offer(new Point(0, 0, 0));
+        visited[0][0] = true;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		map = new int[N][N];
-		visited = new boolean[N][N];
-		for (int i = 0; i < N; i++) {
-			char[] chars = br.readLine().toCharArray();
-			for (int j = 0; j < N; j++) {
-				// 거리 계산을 위해 흰 방을 0, 검은 방을 1로 바꾸기
-				if (chars[j] == '0') map[i][j] = 1;
-				else map[i][j] = 0;
-			}
-		}
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+        while (!pq.isEmpty()) {
+            Point point = pq.poll(); // 이동 횟수가 가장 적은 Point 나옴
+            if (point.x == n - 1 && point.y == n - 1) // 끝방에 도착한 경우
+                return point.cnt; // 방을 바꾼 횟수 return
 
-		minDist = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			Arrays.fill(minDist[i], INF);
-		}
-		System.out.println(dijkstra(0, 0, N - 1, N - 1));
-	}
+            for (int i = 0; i < 4; i++) {
+                int tx = point.x + dx[i];
+                int ty = point.y + dy[i];
+                if (tx < 0 || ty < 0 || tx >= n || ty >= n)
+                    continue;
 
-	static int dijkstra(int sx, int sy, int ex, int ey) {
-		PriorityQueue<Room> pq = new PriorityQueue<>((r1, r2) -> Integer.compare(r1.d, r2.d));
-		pq.offer(new Room(sx, sy, 0));
-		minDist[sx][sy] = 0;
+                if (visited[tx][ty]) continue;
 
-		while (!pq.isEmpty()) {
-			Room now = pq.poll();
-			if (now.x == ex && now.y == ey)
-				break;
+                if (map[tx][ty] == 0) { // 검은 방이면 흰 방으로 바꾸고 이동(cnt + 1)
+                    pq.offer(new Point(tx, ty, point.cnt + 1));
+                } else { // 흰 방이면 그냥 이동(cnt)
+                    pq.offer(new Point(tx, ty, point.cnt));
+                }
+                visited[tx][ty] = true;
+            }
+        }
+        return -1;
+    }
+}
 
-			if (visited[now.x][now.y])
-				continue;
-			visited[now.x][now.y] = true;
+class Point implements Comparable<Point> {
+    int x;
+    int y;
+    int cnt; // 검은 방을 흰 방으로 바꾼 수
 
-			for (int i = 0; i < 4; i++) {
-				int nx = now.x + dx[i];
-				int ny = now.y + dy[i];
-				if (nx < 0 || ny < 0 || nx >= N || ny >= N)
-					continue;
+    public Point(int x, int y, int cnt) {
+        this.x = x;
+        this.y = y;
+        this.cnt = cnt;
+    }
 
-				if (!visited[nx][ny] && minDist[nx][ny] > minDist[now.x][now.y] + map[nx][ny]) {
-					minDist[nx][ny] = minDist[now.x][now.y] + map[nx][ny];
-					pq.offer(new Room(nx, ny, minDist[nx][ny]));
-				}
-			}
-		}
-		return minDist[ex][ey];
-	}
-
-	static class Room {
-		int x, y, d; // d: 지나가는 검은 방의 수
-
-		public Room(int x, int y, int d) {
-			this.x = x;
-			this.y = y;
-			this.d = d;
-		}
-	}
+    @Override
+    public int compareTo(Point o) {
+        return cnt - o.cnt; // 변경 횟수를 기준으로 오름차순 정렬
+    }
 }
