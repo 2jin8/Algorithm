f9047c97553b960106927461a2d6d004c0d9b343
hyeonjin <78083320+2jin8@users.noreply.github.com>
2025-09-20 16:06:18 +0900

    [Gold V] Title: 내려가기, Time: 396 ms, Memory: 54644 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -1,41 +1,46 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N;
-	static int[][] arr, minDP, maxDP;
+    static int N;
+    static int[][] arr, minDp, maxDp;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		arr = new int[N + 1][3];
-		minDP = new int[N + 1][3];
-		maxDP = new int[N + 1][3];
-		for (int i = 1; i <= N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < 3; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][3];
+        StringTokenizer st = null;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-		for (int i = 1; i <= N; i++) {
-			minDP[i][0] = Math.min(minDP[i - 1][0], minDP[i - 1][1]) + arr[i][0];
-			minDP[i][1] = Math.min(minDP[i - 1][0], Math.min(minDP[i - 1][1], minDP[i - 1][2])) + arr[i][1];
-			minDP[i][2] = Math.min(minDP[i - 1][1], minDP[i - 1][2]) + arr[i][2];
+        minDp = new int[N][3];
+        maxDp = new int[N][3];
+        minDp[0] = arr[0].clone();
+        maxDp[0] = arr[0].clone();
 
-			maxDP[i][0] = Math.max(maxDP[i - 1][0], maxDP[i - 1][1]) + arr[i][0];
-			maxDP[i][1] = Math.max(maxDP[i - 1][0], Math.max(maxDP[i - 1][1], maxDP[i - 1][2])) + arr[i][1];
-			maxDP[i][2] = Math.max(maxDP[i - 1][1], maxDP[i - 1][2]) + arr[i][2];
-		}
+        for (int i = 1; i < N; i++) {
+            maxDp[i][0] = arr[i][0] + Math.max(maxDp[i - 1][0], maxDp[i - 1][1]);
+            minDp[i][0] = arr[i][0] + Math.min(minDp[i - 1][0], minDp[i - 1][1]);
 
-		int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
-		for (int i = 0; i < 3; i++) {
-			min = Math.min(min, minDP[N][i]);
-			max = Math.max(max, maxDP[N][i]);
-		}
-		System.out.println(max + " " + min);
-	}
+
+            maxDp[i][1] = arr[i][1] + Math.max(maxDp[i - 1][0], Math.max(maxDp[i - 1][1], maxDp[i - 1][2]));
+            minDp[i][1] = arr[i][1] + Math.min(minDp[i - 1][0], Math.min(minDp[i - 1][1], minDp[i - 1][2]));
+
+            maxDp[i][2] = arr[i][2] + Math.max(maxDp[i - 1][1], maxDp[i - 1][2]);
+            minDp[i][2] = arr[i][2] + Math.min(minDp[i - 1][1], minDp[i - 1][2]);
+        }
+
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for (int i = 0; i < 3; i++) {
+            min = Math.min(min, minDp[N - 1][i]);
+            max = Math.max(max, maxDp[N - 1][i]);
+        }
+        System.out.println(max + " " + min);
+    }
 }
