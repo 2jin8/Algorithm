c256bd022bd9f0079edbc93d0e994e0c1d826130
hyeonjin <78083320+2jin8@users.noreply.github.com>
2025-09-28 22:52:23 +0900

    [Gold IV] Title: 단어 수학, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -1,33 +1,32 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
 
-	static int[] alphabet = new int[26];
+    static Integer[] count = new Integer[26]; // A ~ Z
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Arrays.fill(count, 0); // Integer는 null 로 초기화 됨!
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            int value = 1;
+            for (int j = str.length() - 1; j >= 0; j--) {
+                char c = str.charAt(j);
+                count[c - 'A'] += value;
+                value *= 10;
+            }
+        }
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		for (int i = 0; i < N; i++) {
-			char[] words = br.readLine().toCharArray();
-			int mul = 1;
-			// 자리에 따른 값 매기기
-			for (int j = words.length - 1; j >= 0; j--) {
-				alphabet[words[j] - 'A'] += mul;
-				mul *= 10;
-			}
-		}
+        Arrays.sort(count, Collections.reverseOrder()); // 내림차순 정렬
+        int mul = 9, total = 0;
+        for (Integer c : count) {
+            if (c == 0) break;
 
-		Arrays.sort(alphabet);
-		int sum = 0, mul = 9;
-		// 값이 큰 것부터 확인하기
-		for (int i = alphabet.length - 1; i >= 0; i--) {
-			if (alphabet[i] == 0)
-				break;
-
-			sum += alphabet[i] * mul--;
-		}
-		System.out.println(sum);
-	}
+            total += mul-- * c; // 가장 많이 차지하는 수부터 큰 수 할당
+        }
+        System.out.println(total);
+    }
 }
