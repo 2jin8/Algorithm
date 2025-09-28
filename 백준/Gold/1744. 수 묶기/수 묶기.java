2833aad6a59f2ebf125c660abf5d4f38343a5c6f
hyeonjin <78083320+2jin8@users.noreply.github.com>
2025-09-28 19:09:02 +0900

    [Gold IV] Title: 수 묶기, Time: 108 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.java
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -1,42 +1,50 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n =Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> negativePq = new PriorityQueue<>();
-        PriorityQueue<Integer> positivePq = new PriorityQueue<>(Collections.reverseOrder());
-        for (int i = 0; i < n; i++) {
+        int N = Integer.parseInt(br.readLine());
+        // negPq: 작은 수 기준 정렬, posPq: 큰 수 기준 정렬
+        PriorityQueue<Integer> negPq = new PriorityQueue<>();
+        PriorityQueue<Integer> posPQ = new PriorityQueue<>((i1, i2) -> Integer.compare(i2, i1));
+
+        for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(br.readLine());
-            if (num <= 0) {
-                negativePq.offer(num);
+            if (num > 0) {
+                posPQ.offer(num);
             } else {
-                positivePq.offer(num);
+                negPq.offer(num);
+            }
+        }
+        
+        // 양수는 양수끼리, 음수는 음수끼리 곱해야 큰 수를 만들 수 있음
+        // 0은 양수랑은 더하고 음수랑은 곱해야 큰 수를 만들 수 있음
+        int total = 0;
+        while (!posPQ.isEmpty()) {
+            int p1 = posPQ.poll();
+            // 하나 더 꺼낼 수 있으면 두 값을 곱하는게 더 큼
+            if (!posPQ.isEmpty()) {
+                int p2 = posPQ.poll();
+                // 둘 중 하나가 1이면 더하는 값이 더 큼
+                if (p1 == 1 || p2 == 1) total += p1 + p2;
+                else total += p1 * p2;
+            } else {
+                total += p1;
             }
         }
 
-        int total = 0;
-        while (positivePq.size() > 1) { // 두 개씩 뽑기
-            int a = positivePq.poll();
-            int b = positivePq.poll();
-            total += Math.max(a + b, a * b);
-        }
+        while (!negPq.isEmpty()) {
+            int n1 = negPq.poll();
+            if (!negPq.isEmpty()) {
+                int n2 = negPq.poll();
 
-        while (negativePq.size() > 1) { // 두 개씩 뽑기
-            int a = negativePq.poll();
-            int b = negativePq.poll();
-            total += Math.max(a + b, a * b);
-        }
-
-        if (!positivePq.isEmpty() && !negativePq.isEmpty()) { // 양수 큐와 음수 큐에 값이 아직 존재하는 경우
-            int a = positivePq.poll();
-            int b = negativePq.poll();
-            total += Math.max(a + b, a * b);
-        } else if (!positivePq.isEmpty()) { // 양수 큐에만 값이 존재하는 경우
-            total += positivePq.poll();
-        } else if (!negativePq.isEmpty()) { // 음수 큐에만 값이 존재하는 경우
-            total += negativePq.poll();
+                total += n1 * n2;
+            } else {
+                total += n1;
+            }
         }
         System.out.println(total);
     }
