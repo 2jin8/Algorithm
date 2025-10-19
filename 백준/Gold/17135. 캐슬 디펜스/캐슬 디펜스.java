725d3e851f3b98ffaebf5d2accfd27acf8df293e
hyeonjin <78083320+2jin8@users.noreply.github.com>
2025-10-19 22:51:21 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 272 ms, Memory: 44132 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
@@ -1,132 +1,123 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N, M, D, enemy, ans;
-	static int[] pos = new int[3]; // 궁수의 위치를 저장할 배열
-	static int[][] map;
+    static int N, M, D, maxKill, totalEnemy;
+    static int[][] map, newMap;
+    static int[] posC = new int[3]; // 궁수의 열의 위치 (행은 N - 1로 고정)
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	static int[] dx = { 0, -1, 0 }, dy = { -1, 0, 1 }; // 좌, 상, 우
+        // 풀이 로직
+        // 1. 궁수의 위치 정하기 (3자리만 정하면 됨 => 완탐)
+        // 2. 모든 적이 게임판에서 없어질 때까지 시뮬레이션 돌리기
+        // 3. 없앤 적의 개수 갱신
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        D = Integer.parseInt(st.nextToken()); // 궁수의 공격 거리 제한
+        map = new int[N][M];
+        newMap = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                totalEnemy += map[i][j]; // 적의 수 계산
+            }
+        }
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		D = Integer.parseInt(st.nextToken()); // 궁수의 공격 거리 제한
-		map = new int[N + 1][M];
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				if (map[i][j] == 1)
-					enemy++;
-			}
-		}
-		dfs(0, 0);
-		System.out.println(ans);
-	}
+        setPosition(0, 0);
+        System.out.println(maxKill);
+    }
 
-	static void dfs(int depth, int start) {
-		// 3명의 궁수 위치를 결정한 경우
-		if (depth == 3) {
-			ans = Math.max(ans, bfs());
-			return;
-		}
+    // 순서 중요 X > 조합
+    static void setPosition(int depth, int start) {
+        if (depth == 3) {
+            maxKill = Math.max(maxKill, simulation());
+            return;
+        }
 
-		for (int i = start; i < M; i++) {
-			pos[depth] = i;
-			dfs(depth + 1, i + 1);
-		}
-	}
+        // 궁수의 위치 정하기
+        for (int i = start; i < M; i++) {
+            posC[depth] = i;
+            setPosition(depth + 1, i + 1);
+        }
+    }
 
-	static int[][] copyArr() {
-		int[][] arr = new int[N + 1][M];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				arr[i][j] = map[i][j];
-			}
-		}
-		return arr;
-	}
+    static int simulation() {
+        int kill = 0, enemy = totalEnemy;
+        // 기존 배열 새로운 배열로 복사 (1번 실행하고 끝낼 것 아니므로.. 주의!)
+        for (int i = 0; i < N; i++) {
+            newMap[i] = map[i].clone();
+        }
 
-	static int bfs() {
-		int removeCnt = 0;
-		int tmpEnemy = enemy; // ^^.. 초기화 조심하자
+        while (enemy != 0) {
+            int[][] tmpMap = new int[N][M];
+            for (int i = 0; i < N; i++) {
+                tmpMap[i] = newMap[i].clone();
+            }
 
-		Queue<Pos> queue = new ArrayDeque<>();
-		int[][] tmpMap = copyArr();  // 초기화 조심하자
-		while (tmpEnemy != 0) {
-			Pos[] removeArr = new Pos[3];
-			for (int i = 0; i < 3; i++) {
-				boolean[][] visited = new boolean[N + 1][M];
-				queue.clear();
+            // 각 궁사별로 가장 가까운 적 구하기
+            for (int c : posC) {
+                Enemy killEnemy = getMinDistEnemy(N, c);
+                if (killEnemy.d == -1) continue; // 죽일 적을 못 찾은 경우
 
-				int x = N, y = pos[i];
-				queue.offer(new Pos(x, y));
-				visited[x][y] = true;
+                int killCount = tmpMap[killEnemy.x][killEnemy.y]; // 같은 적을 공격할 수도 있으므로 tmpMap의 값으로 확인하기
+                enemy -= killCount;
+                kill += killCount;
+                tmpMap[killEnemy.x][killEnemy.y] = 0; // 적 제거
+            }
 
-				while (!queue.isEmpty()) {
-					Pos now = queue.poll();
-					// 적이 있는 위치라면 기록하고 탐색 종료하기
-					if (tmpMap[now.x][now.y] == 1) {
-						removeArr[i] = now;
-						break;
-					}
+            // 마지막 줄에 살아있던 적의 수 빼주기
+            for (int last : tmpMap[N - 1]) {
+                enemy -= last;
+            }
 
-					for (int j = 0; j < 3; j++) {
-						int nx = now.x + dx[j]; // 변수 잘 보기
-						int ny = now.y + dy[j];
-						if (nx < 0 || ny < 0 || nx >= N || ny >= M)
-							continue;
+            // 기존 배열에 새로운 배열 붙여넣기 (이때, 한 줄씩 아래로 밀 것)
+            for (int i = N - 2; i >= 0; i--) {
+                newMap[i + 1] = tmpMap[i].clone();
+            }
+            Arrays.fill(newMap[0], 0);
+        }
+        return kill;
+    }
 
-						int d = Math.abs(x - nx) + Math.abs(y - ny);
-						if (visited[nx][ny] || d > D)
-							continue;
+    static Enemy getMinDistEnemy(int r, int c) {
+        int x = -1, y = -1, d = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (newMap[i][j] == 0) continue;
 
-						queue.offer(new Pos(nx, ny));
-						visited[nx][ny] = true;
-					}
-				}
-			}
+                // 가장 가까운 거리에 있는 적 찾기
+                int dist = Math.abs(i - r) + Math.abs(j - c);
+                if (dist > D) continue;
 
-			for (Pos p : removeArr) {
-				if (p == null || tmpMap[p.x][p.y] == 0)
-					continue;
+                if (dist < d) {
+                    d = dist;
+                    x = i;
+                    y = j;
+                } else if (dist == d) { // 거리가 같은 적이 여러 명이면 가장 왼쪽에 있는 적 공격
+                    if (j < y) { // j가 더 왼쪽에 있으면 갱신
+                        x = i;
+                        y = j;
+                    }
+                }
+            }
+        }
+        if (d == Integer.MAX_VALUE) d = -1; // 죽일 적을 못 찾는 경우
+        return new Enemy(x, y, d);
+    }
 
-				// 공격받은 적은 게임에서 제외됨
-				tmpMap[p.x][p.y] = 0;
-				tmpEnemy--; // 남아있는 적의 수 감소
-				removeCnt++; // 제거할 수 있는 적의 수 증가
-			}
-			// 적이 성이 있는 칸으로 이동하면 게임에서 제외됨
-			for (int i = 0; i < M; i++) {
-				if (tmpMap[N - 1][i] == 1)
-					tmpEnemy--;
-			}
+    static class Enemy {
+        int x, y, d;
 
-            // 위에서부터 덮어씌우면 기존 값이 날라가니 아래부터 덮어씌워야 함 (제발 조심하자)
-			// 궁수의 공격이 끝나면 적은 아래로 한 칸 이동함
-			for (int i = N - 1; i > 0; i--) {
-				tmpMap[i] = tmpMap[i - 1].clone();
-			}
-			Arrays.fill(tmpMap[0], 0);
-		}
-		return removeCnt;
-	}
-
-	static class Pos {
-		int x, y;
-
-		public Pos(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
+        public Enemy(int x, int y, int d) {
+            this.x = x;
+            this.y = y;
+            this.d = d;
+        }
+    }
 }
